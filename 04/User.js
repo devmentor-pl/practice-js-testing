c8f10d0f35e88cc7aa7c3de62f6d9da152d1b7f3
Martin Gawlyta <martin.guidegmail.com>
2020-10-03 19:22:51 +0200

    04 test 2 and 3

--- 04/User.js
+++ 04/User.js
@@ -1,12 +1,24 @@
 export default class User {
     constructor(object) {
-        this.email = object.email;
-        this.password = object.password;
-    }
+
+            this.checkIfCorrectMail();
+
+            this.email = object.email;
+            this.password = object.password;
+
+
+    };
+
     getEmail() {
         return this.email;
     }
     getPassword() {
         return this.password;
     }
+    checkIfCorrectMail() {
+        if (!this.email.includes('@')) {
+            throw new TypeError('E-mail must contain @')
+        };
+    }
+    
 };
