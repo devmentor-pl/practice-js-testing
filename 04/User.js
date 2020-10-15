e9dd55ad26e9db3f7f8673322f04075724c00066
Dawid <dwilk92@o2.pl>
2020-10-15 16:07:27 +0200

    zad 4

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,11 @@
 export default class User {
+    constructor( email = 'koder@devmentor.pl',
+     password = 'pw123456')
+     getu (){
+        if (email!=='koder@devmentor.pl' ){
+            throw new Error('pownino byc cos')
 
+            
+        }
+     }
 }
