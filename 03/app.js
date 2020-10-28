f099729fca585ae7687db77bff4f93c626681e11
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-10-28 20:49:03 +0100

    Finish task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,19 @@
 export default function randomNumber(min, max) {
-  return 2;
+  if (min === undefined) {
+    throw new Error("Min argument not provided.");
+  }
+
+  if (max === undefined) {
+    throw new Error("Max argument not provided.");
+  }
+
+  if (typeof min !== "number") {
+    throw new TypeError("Min argument must be a number.");
+  }
+
+  if (typeof max !== "number") {
+    throw new TypeError("Max argument must be a number.");
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -7,4 +7,50 @@ describe("Random Number", () => {
     expect(number).toBeGreaterThanOrEqual(1);
     expect(number).toBeLessThanOrEqual(10);
   });
+
+  it("should return random number between 1 and 10 when max < min", () => {
+    const number = randomNumber(10, 1);
+
+    expect(number).toBeGreaterThanOrEqual(1);
+    expect(number).toBeLessThanOrEqual(10);
+  });
+
+  it("should return 0 when min and max are both equal to 0", () => {
+    const number = randomNumber(0, 0);
+
+    expect(number).toBe(0);
+  });
+
+  it("should return random number between -5 and 1", () => {
+    const number = randomNumber(-5, 1);
+
+    expect(number).toBeGreaterThanOrEqual(-5);
+    expect(number).toBeLessThanOrEqual(1);
+  });
+
+  it("should return 0 when min and max are both equal to 0", () => {
+    const number = randomNumber(0, 0);
+
+    expect(number).toBe(0);
+  });
+
+  it("should throw an error when min parameter was not provided", () => {
+    expect(() => randomNumber()).toThrow("Min argument not provided.");
+  });
+
+  it("should throw an error when max parameter was not provided", () => {
+    expect(() => randomNumber(0)).toThrow("Max argument not provided.");
+  });
+
+  it("should throw an error when min parameter is not a number", () => {
+    expect(() => randomNumber("1", 10)).toThrow(
+      "Min argument must be a number."
+    );
+  });
+
+  it("should throw an error when max parameter is not a number", () => {
+    expect(() => randomNumber(1, "10")).toThrow(
+      "Max argument must be a number."
+    );
+  });
 });
