c35cb91ab4289e7cedbe4309a5cd2e64475f5ac9
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-10-28 19:15:04 +0100

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,59 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector(".error--click");
+  const enterEl = document.querySelector(".error--enter");
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+  handleAlertClick();
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
+  initEventWithError(clickEl, "click", new RangeError("Błąd zakresu!"));
+  initEventWithError(enterEl, "mouseenter", new TypeError("Błąd typu!"));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+  element.style.top = Math.random() * 600 + "px";
+  element.style.left = Math.random() * 800 + "px";
 
-    if(error) {
-        throw error;
-    }
+  if (error) {
+    throw error;
+  }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  element.addEventListener(eventName, function () {
+    try {
+      setRandomPosition(this, error);
+    } catch (e) {
+      showAlert(e.message);
+    }
+  });
+}
+
+function showAlert(message) {
+  const alertEl = document.querySelector(".alert");
+
+  if (alertEl) {
+    alertEl.classList.remove("alert--hidden");
+
+    const alertMessageEl = document.querySelector(".alert__message");
+
+    if (alertMessageEl) {
+      alertMessageEl.innerText = message;
+    }
+  }
+}
+
+function handleAlertClick() {
+  const alertEl = document.querySelector(".alert");
+
+  if (alertEl) {
+    alertEl.addEventListener("click", (e) => {
+      if (e.target.classList.contains("alert")) {
+        e.target.classList.add("alert--hidden");
+      }
+    });
+  }
 }
