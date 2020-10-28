6584b84676ab18ed4c359dafe41f5b5972558bfe
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-10-28 19:58:59 +0100

    Implement random number to pass the test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,10 @@
-import randomNumber from './app';
+import randomNumber from "./app";
+
+describe("Random Number", () => {
+  it("should return random number between 1 and 10", () => {
+    const number = randomNumber(1, 10);
+
+    expect(number).toBeGreaterThanOrEqual(1);
+    expect(number).toBeLessThanOrEqual(10);
+  });
+});
