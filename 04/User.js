2c47d7dc8a25bd2e860ca1e1268c0db2bfc5cf4a
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-10-28 21:10:48 +0100

    Finish task 4

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,38 @@
 export default class User {
+  constructor({ email, password }) {
+    if (!this._isEmailValid(email)) {
+      throw new TypeError("Wrong email argument.");
+    }
 
+    if (!this._isPasswordValid(password)) {
+      throw new TypeError("Password must be at least 8 characters length.");
+    }
+
+    this.email = email;
+    this.password = password;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  login() {
+    return this.email.includes("devmentor.pl");
+  }
+
+  _isEmailValid(email) {
+    const emailRe = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return emailRe.test(email);
+  }
+
+  _isPasswordValid(password) {
+    const minPasswordLength = 8;
+
+    return password.trim().length >= minPasswordLength;
+  }
 }
