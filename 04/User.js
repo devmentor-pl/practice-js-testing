cce3201bba553c9d4f08d0133976b1657af013a8
Martyna Kiljan <kmartynak@icloud.com>
2020-10-29 23:47:13 +0100

    .

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,44 @@
 export default class User {
 
+    constructor(user) {
+
+        this.email = user.email;
+        this.password = user.password;
+        this.checkCorrectEmail()
+        this.chectCorrectPassword()
 }
+
+getEmail() {
+    return this.email;
+}
+getPassword() {
+    return this.password;
+}
+
+
+
+checkCorrectEmail() {
+   if(!this.email.includes('@')) {
+    throw new TypeError ('email is not valid')
+} else if(this.email.includes('devmentor')) {
+    return true;
+}
+}
+
+chectCorrectPassword() {
+    if(!this.password.includes('pw') && this.password.length === 8) {
+    throw new TypeError ('password is not correct')
+    } 
+}
+
+
+
+login() {
+    if(this.login.includes('devmentor.pl')) {
+        return true;
+    } else if (!this.login.includes('devmentor.pl'))
+    return false;
+  }
+
+}
+
