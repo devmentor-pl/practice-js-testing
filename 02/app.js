4c2110cda2db72a6a91a6342631fd3c4e723c11b
AnnaBuller <aniabela@wp.pl>
2020-11-10 18:18:44 +0100

    solve 02 task to consult

--- 02/app.js
+++ 02/app.js
@@ -9,20 +9,35 @@ function init() {
 
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
 }
 
 function setRandomPosition(element, error = null) {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
+    if (error) {
+        throw error
     }
 }
 
 function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
+        try {
+            setRandomPosition(this, error);
+        } catch (err) {
+            popAlert(err)
+        }
     })
+}
+
+function popAlert(err) {
+    const alertBox = document.querySelector('.alert');
+    const alertMsg = alertBox.querySelector('.alert__message');
+    alertMsg.innerText = err.message;
+    alertBox.classList.remove('alert--hidden');
+    alertBox.addEventListener('click', e => {
+        if (e.target === e.currentTarget) {
+            alertBox.classList.add('alert--hidden');
+        }
+        }) 
 }
