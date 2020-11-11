0b29cccd5fa91e80a2cac451a154646740155fa6
AnnaBuller <aniabela@wp.pl>
2020-11-11 13:36:07 +0100

    red 1st test fixed

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,8 +1,5 @@
 import randomNumber from './app';
 
 it('throw exception when no arguments are passed', () => {
-  function getRandomNum() {
-    new randomNumber();
-  }
-  expect(getRandomNum).toThrow()
+  expect(randomNumber).toThrow()
 }) 
