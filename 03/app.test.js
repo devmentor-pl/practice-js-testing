0ff623b0f40e3ab070cf581350e297c5953b8b89
AnnaBuller <aniabela@wp.pl>
2020-11-11 12:59:19 +0100

    create red 1st test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,8 @@
 import randomNumber from './app';
+
+it('throw exception when no arguments are passed', () => {
+  function getRandomNum() {
+    new randomNumber();
+  }
+  expect(getRandomNum).toThrow()
+}) 
