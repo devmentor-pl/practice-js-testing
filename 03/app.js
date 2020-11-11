58554e1058d85d625ebd42e7c8f7dc519ce5423d
AnnaBuller <aniabela@wp.pl>
2020-11-11 13:55:52 +0100

    refactor function

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,9 @@
 export default function randomNumber(min, max) {
-  if (typeof min === 'undefined' && typeof max === 'undefined') {
+  if (!areArgumentsPassed(min, max)) {
     throw new Error('No arguments were passed.')
   }
+}
+
+function areArgumentsPassed(...args) {
+  return [...args].every(arg => typeof arg !== 'undefined')
 }
