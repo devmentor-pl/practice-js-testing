b989e73b6811af6c8f1369aee44282f8e3081f84
AnnaBuller <aniabela@wp.pl>
2020-11-11 18:06:01 +0100

    add implementation

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,22 @@
 export default class User {
+  constructor({ email, password }) {
+    if (!email.includes('@')) {
+      throw new Error('Email address lacks \'@\'.')
+    }
+    if (password.length <= 7) {
+      throw new Error('Password should have at least 7 characters')
+    }
+    this.email = email;
+    this.password = password;
+  }
 
+  getEmail() {
+    return this.email
+  }
+  getPassword() {
+    return this.password
+  }
+  login() {
+    return this.email.includes('devmentor.pl') ? true : false
+  }
 }
