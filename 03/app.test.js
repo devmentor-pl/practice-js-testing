507b2e2ad943360417adfd026f3e7d9e7a5d8401
AnnaBuller <aniabela@wp.pl>
2020-11-11 15:22:40 +0100

    group tests

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,30 +1,33 @@
 import randomNumber from './app';
 
-it('throw exception when no arguments are passed', () => {
+describe('arguments', () => {
+  it('throw exception when no arguments are passed', () => {
   function getRandomNum() {
     randomNumber()
   }
   expect(getRandomNum).toThrow()
-}) 
+  }) 
 
-it('throw exception when arg1 is greater than arg2', () => {
-  function getRandomNum() {
-    randomNumber(3, 1)
-  }
-  expect(getRandomNum).toThrow('First argument must be lesser than the second one.')
+  it('throw exception when arg1 is greater than arg2', () => {
+    function getRandomNum() {
+      randomNumber(3, 1)
+    }
+    expect(getRandomNum).toThrow('First argument must be lesser than the second one.')
+  })
+
+  it('throw exception when arg1 is not a number', () => {
+    function getRandomNum() {
+      randomNumber('notANumber', 1)
+    }
+    expect(getRandomNum).toThrow('Argument must be a number.')
+  })
+
+  it('throw exception when arg2 is not a number', () => {
+    function getRandomNum() {
+      randomNumber(1, 'notANumber')
+    }
+    expect(getRandomNum).toThrow('Argument must be a number.')
+  })
 })
 
-it('throw exception when arg1 is not a number', () => {
-  function getRandomNum() {
-    randomNumber('notANumber', 1)
-  }
-  expect(getRandomNum).toThrow('Argument must be a number.')
-})
-
-it('throw exception when arg2 is not a number', () => {
-  function getRandomNum() {
-    randomNumber(1, 'notANumber')
-  }
-  expect(getRandomNum).toThrow('Argument must be a number.')
-})
 
