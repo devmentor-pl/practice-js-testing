f8f2ecff5bb85681417a15ce5c37009b152377a9
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-25 16:40:15 +0100

    Create 2nd test

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,4 @@
-export default function randomNumber(min, max) {
+export default function randomNumber(min = 0, max = 0) {
     const number = Math.floor(Math.random() * (max - min) + min);
     return number;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -5,3 +5,8 @@ it('should return 0 if min = 0 and max = 0', () => {
     const max = 0;
     expect(randomNumber(min, max)).toBe(0);
 })
+
+it('should throw an error if parameter min is not a number', () => {
+    const min = 'X';
+    expect(randomNumber(min, undefined)).toThrow();
+})
