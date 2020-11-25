16683d5a2f83c6e3f6d3443ab5bf3ded6b033c91
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-25 13:32:42 +0100

    Fix task 01

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,27 @@
 function getProductList() {
-    return [
-        {name: 'JavaScript: podstawy', type: 'book', count: 3, price: 67.19, discount: 0.21},
-        {name: 'React: podstawy', type: 'book', count: 4, price: 79.17, discount: 0.27}
+    return [{
+            name: 'JavaScript: podstawy',
+            type: 'book',
+            count: 3,
+            price: 67.19,
+            discount: 0.21
+        },
+        {
+            name: 'React: podstawy',
+            type: 'book',
+            count: 4,
+            price: 79.17,
+            discount: 0.27
+        }
     ]
 }
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
+    //Dzięki debuggerowi i dodaniu do watch zmiennej acc mogłem zauważyć, że wskazuje ona na cały obiekt, a nie jego poszczególne właściwości.
+    return productList.reduce((product, acc) => {
+        return ((acc.price * acc.count) * (1 - acc.discount) + (product.price * product.count) * (1 - product.discount)).toFixed(2);
     });
 }
 
-const totalPrice = getTotalPrice( getProductList() );
+const totalPrice = getTotalPrice(getProductList());
 console.log(totalPrice); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
