3a1842495ac26d8e27f9fcc9c0b97f8d34f08845
Piotr Niemyjski <piotr.niemyjski@wp.pl>
2020-11-26 21:03:24 +0100

    task#3: after 1st test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,8 @@
 import randomNumber from './app';
+
+it('throw exception when min is not a number', () => {
+    function callRandomNumber() {
+        randomNumber('3', 4)
+    }
+        expect(callRandomNumber).toThrow()
+})
