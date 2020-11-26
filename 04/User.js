e7d5ee0c2ceb153a416adfbfb0a80b4fd2382cc0
Piotr Niemyjski <piotr.niemyjski@wp.pl>
2020-11-26 22:54:50 +0100

    task#4: done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,40 @@
 export default class User {
+    constructor({email, password}) {
+        this.email = email
+        this.password = password
 
+        this.checkEmail()
+        this.checkPassword()
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getPassword() {
+        return this.password
+    }
+
+    checkEmail() {
+        if(!(this.email.includes('@'))) {
+            throw new Error('incorrect email')
+        }
+    }
+
+    checkPassword() {
+        const pattern = /[a-z]/i
+
+        if(!this.password.match(pattern)) {
+            throw new Error('password should consist of at least one letter')
+        }
+    }
+
+    login() {
+        const pattern = /@devmentor.pl/i
+
+        if(this.email.match(pattern)) {
+            return true
+        }
+        return false
+    }
 }
