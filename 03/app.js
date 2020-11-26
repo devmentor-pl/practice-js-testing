7a1913bb818ae2c9d3ecec65ccf1fc0560f2a1b3
Piotr Niemyjski <piotr.niemyjski@wp.pl>
2020-11-26 21:19:30 +0100

    task#3: after 4th implementation

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,9 @@
 export default function randomNumber(min, max) {
-    isMinAndMaxOK(min, max)
+    if(isMinAndMaxOK(min, max)) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min)) + min
+    }
 }
 
 function isMinAndMaxOK(min, max) {
@@ -7,5 +11,5 @@ function isMinAndMaxOK(min, max) {
         throw new Error('min and max must be a number')
     } else if(max <= min) {
         throw new Error('max must be grater than min')
-    }
+    } else return true
 }
