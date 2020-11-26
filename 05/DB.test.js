b90a1c829a717d748e869be0ffffdf88e6511c9e
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-26 09:27:06 +0100

    Task 5 needs consulting

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,81 @@
 import DB from './DB';
+
+describe('insert', () => {
+
+
+    // Nie działa zgodnie z zamierzeniem
+    /*  it('Should thrwo an error when try to duplicate ID',
+         async () => {
+
+             const database = new DB();
+
+             const result = await database.insert({
+                 a: 'X',
+                 id: 1
+             }, {
+                 b: 'Y',
+                 id: 1
+             }, {
+                 c: 'Z',
+                 id: 1
+             })
+
+
+             expect(result).toThrow();
+         }) */
+
+
+
+
+    it('Should insert first data with id = 1', async () => {
+        const database = new DB();
+
+        const result = await database.insert({
+            x: 'X'
+        })
+
+        expect(result).toStrictEqual({
+            x: 'X',
+            id: 1
+        })
+    })
+
+    it('Should insert data with id = 2', async () => {
+        const database = new DB([{
+            x: 'X',
+            id: 1
+        }]);
+
+        const result = await database.insert({
+            y: 'Y',
+            id: 2
+        });
+
+        expect(result).toStrictEqual({
+            y: 'Y',
+            id: 2
+        })
+    })
+
+
+})
+
+describe('remove', () => {
+
+    // Nie działa
+    /*  it('Should throw if try to remove item with id that not exists', async () => {
+
+         const database = new DB([{
+             x: 'X',
+             id: 1
+         }]);
+
+         const promise = database.remove(2)
+
+         return promise.then(result => {
+             expect(result).toBe('Item not exist!')
+         })
+
+     }) */
+
+})
