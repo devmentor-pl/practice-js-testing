97cdcae7de24dc8fa01d7e6f531c118e5d33d23f
Piotr Niemyjski <piotr.niemyjski@wp.pl>
2020-11-26 21:10:28 +0100

    task#3: after 3rd test

--- 03/app.test.js
+++ 03/app.test.js
@@ -13,3 +13,11 @@ it('throw exception when max is not a number', () => {
     }
         expect(callRandomNumber).toThrow()
 })
+
+it('throw exception when max <= min', () => {
+    function callRandomNumber() {
+        randomNumber(4, 3)
+        randomNumber(4, 4)
+    }
+        expect(callRandomNumber).toThrow()
+})
