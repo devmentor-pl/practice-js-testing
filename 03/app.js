27df0cfebd1fa9cd3cd8e4762ff5414d2718dfd2
Piotr Niemyjski <piotr.niemyjski@wp.pl>
2020-11-26 21:16:21 +0100

    task#3: after 3rd implementation

--- 03/app.js
+++ 03/app.js
@@ -5,5 +5,7 @@ export default function randomNumber(min, max) {
 function isMinAndMaxOK(min, max) {
     if(typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('min and max must be a number')
+    } else if(max <= min) {
+        throw new Error('max must be grater than min')
     }
 }
