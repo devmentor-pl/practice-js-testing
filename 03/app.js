52712422f555cd66f8efb525c149871aed5963b2
Ewa Skórska <ewaskorska94@gmail.com>
2021-01-17 21:32:03 +0100

    Add initial state

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,3 @@
 export default function randomNumber(min, max) {
-
+    if(typeof(min) != 'number' || typeof(max) != 'number') return null;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,9 @@
 import randomNumber from './app';
+
+it('return null if parameter isn\'t a number', () => {
+    const notNumber = '01';
+    expect(randomNumber(notNumber, 0)).toBe(null);
+    expect(randomNumber(0, notNumber)).toBe(null);
+    expect(randomNumber(notNumber, notNumber)).toBe(null);
+});
+
