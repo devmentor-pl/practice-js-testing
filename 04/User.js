fe97556aea5ad77f3476f33103fd9562d8e12cde
Ewa Skórska <ewaskorska94@gmail.com>
2021-01-18 00:09:35 +0100

    Add TDD implementation User

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,42 @@
-export default class User {
+const PASSWORD_LENGTH = 3;
 
+export default class User {
+    constructor( {email, password} ) {
+        this._validateEmail(email);
+        this._validatePassword(password);
+        this.email = email;
+        this.password = password;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+    
+    login() {
+        const domain = /@devmentor\.pl$/;
+        const matched = this.email.match(domain);
+        return matched !== null && matched.length === 1;
+    }
+
+    _isEmailValid(email) {
+        return email.includes("@");
+    }
+
+    _validateEmail(email) {
+        if(!this._isEmailValid(email)) 
+            throw new Error("Incorrect email!");
+    }
+
+    _isPasswordValid(password) {
+        return password.length > PASSWORD_LENGTH;
+    }
+
+    _validatePassword(password) {
+        if(!this._isPasswordValid(password))
+            throw new Error("Password should be at least 4 characters long");
+    }
 }
