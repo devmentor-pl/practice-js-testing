598130204e631eae1d10f9b06698528b0c8d7853
gregor232 <ktotok@gmail.com>
2021-01-21 21:36:38 +0100

    tasks 01, 02 done

--- 01/app.js
+++ 01/app.js
@@ -6,10 +6,11 @@ function getProductList() {
 }
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
+    return productList.reduce( (product, acc) => {        
+        return ((acc.price * acc.count) - ((acc.price * acc.count) * acc.discount)) + ((product.price * product.count) - ((product.price * product.count) * product.discount));
     });
 }
 
 const totalPrice = getTotalPrice( getProductList() );
 console.log(totalPrice); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
+
--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', init);
+const clickEl = document.querySelector('.error--click');
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
     const enterEl = document.querySelector('.error--enter');
 
     setRandomPosition(clickEl);
@@ -10,14 +10,15 @@ function init() {
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 
-}
+ }
 
 function setRandomPosition(element, error = null) {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
 
     if(error) {
-        throw error;
+        displaySection(error);
+        
     }
 }
 
@@ -25,4 +26,21 @@ function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
         setRandomPosition(this, error);
     })
+}
+
+function displaySection(error) {
+        const sectionAlert = document.querySelector(".alert--hidden");
+        const pAlert = document.querySelector(".alert__message");
+        const alertCon = document.querySelector(".alert__container");
+        const bodyEl = document.querySelector("body");
+
+        sectionAlert.style.display = 'block';
+        pAlert.innerText = error.message;
+
+        bodyEl.addEventListener("click", (e) => {
+            sectionAlert.style.display = 'none';
+            if (e.target === alertCon || e.target ===  pAlert || e.target ===  clickEl) {
+                sectionAlert.style.display = 'block';
+            }
+        }) 
 }
