59ff60fe25ac2c5a4c268fb062741f9322714b58
gregor232 <ktotok@gmail.com>
2021-01-22 20:52:56 +0100

    first test done

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,14 @@
 import randomNumber from './app';
+
+/*describe('randomNumber', () => {
+    describe('no parameters, passes', () => {
+        const result = randomNumber();
+
+        expect(result).toBeDefined();
+    })
+}) */
+
+test('randomNumber', () => {
+
+    expect(typeof randomNumber('1', '2')).toBe('number')
+})
