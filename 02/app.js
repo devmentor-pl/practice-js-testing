6e82e6bca1cb69a47697bd7b2dacc737a7884fd4
gregor232 <ktotok@gmail.com>
2021-01-22 15:31:44 +0100

    task 02 updated

--- 02/app.js
+++ 02/app.js
@@ -6,28 +6,41 @@ function init() {
 
     setRandomPosition(clickEl);
     setRandomPosition(enterEl);
-
+    
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
+    
+    
 
  }
 
 function setRandomPosition(element, error = null) {
+
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
-
-    if(error) {
-        displaySection(error);
-        
+    
+    
+    if (error) {
+        throw error
     }
+
 }
+
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
-}
 
+        element.addEventListener(eventName, function() {
+            
+            try {
+                setRandomPosition(this, error)
+            }
+            catch (error) {
+                displaySection(error)
+            }
+        })
+    
+
+}
 function displaySection(error) {
         const sectionAlert = document.querySelector(".alert--hidden");
         const pAlert = document.querySelector(".alert__message");
