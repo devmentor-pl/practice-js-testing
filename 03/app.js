f53577bbc4d2397e2d58edbc5cc2dee5f21a4001
gregor232 <ktotok@gmail.com>
2021-01-22 23:36:00 +0100

    second test done

--- 03/app.js
+++ 03/app.js
@@ -1,8 +1,18 @@
-export default function randomNumber(min, max) {
-    if (typeof min !== 'number' && typeof max !== 'number' ) {
-        return alert('Parametry muszą być liczbami')
-    }
+/*export default function randomNumber(min, max) {
+    //if (typeof min !== 'number' && typeof max !== 'number' ) {
+    //    return alert('Parametry muszą być liczbami')
+    //}
     return Math.random() * (max - min) + min;
-}
+} */
 
-randomNumber(1,2);
+//randomNumber(1,2);
+
+export default class RanNum {
+    constructor () {
+        this.min = 2,
+        this.max = 1
+    }
+    randomNumber() {
+        return Math.random() * (this.max - this.min) + this.min;
+    }
+}
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,4 +1,4 @@
-import randomNumber from './app';
+import RanNum from './app';
 
 
 
@@ -7,7 +7,14 @@ import randomNumber from './app';
     expect(typeof randomNumber('1', '2')).toBe('number')
 }) */
 
-test('randomNumber without parameters', () => {
+/*test('randomNumber without parameters', () => {
 
-    expect(randomNumber()).toBeFalsy()
-})
+    expect(randomNumber()).toBeDefined()
+}) */
+// wyczerpaly mi sie pomysly, taka implementacja jest chyba wystarczajaca
+// czy po sprawdzeniu tego pierwszego bledu powinienem od razu implementowac ten if
+
+test('randomNumber parameters', () => {
+    const ranNum = new RanNum;
+    expect(ranNum.max > ranNum.min).toBe(true)
+}) 
