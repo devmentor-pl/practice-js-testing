d3094b0578ed46fbad7c8fc2f6c055bb976fa7e5
gregor232 <ktotok@gmail.com>
2021-01-26 11:26:23 +0100

    05 second test passes

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -8,13 +8,15 @@ describe('data', () => {
             expect(resolve).toBe(1)
             
         }) */
-        return expect(db.insert({a: 3, b: 4})).resolves.toBe({"a": 3, "b": 4, "id": 1})
+        return expect(db.insert({a: 3, b: 4})).resolves.toStrictEqual({a: 3, b: 4, id: 1})
         
     })
     it('if data.id is a number', () => {
         const db = new DB();
         
-        return expect(db.insert({a: 3, b: 4, id: "id"})).resolves.toBe({"a": 3, "b": 4, "id": "id"})
-        
-    })
+        //return expect(db.insert({a: 3, b: 4, id: "id"})).resolves.toBe({"a": 3, "b": 4, "id": "id"})
+
+
+        return expect(db.insert({a: 3, b: 4, id: "id"})).rejects.toBe('ID can be only number!')
+    }) 
 })
