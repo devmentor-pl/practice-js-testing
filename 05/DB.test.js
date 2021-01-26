d6840f8994167f4d8b9c3415dbb3a5c8a9742e42
gregor232 <ktotok@gmail.com>
2021-01-26 11:26:57 +0100

    05 second test passes v2

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -3,11 +3,7 @@ import DB from './DB';
 describe('data', () => {
     it('if data.id is 1', () => {
         const db = new DB();
-        /*return db.insert({a: 3, b: 4})
-        .then((resolve) => {
-            expect(resolve).toBe(1)
-            
-        }) */
+        
         return expect(db.insert({a: 3, b: 4})).resolves.toStrictEqual({a: 3, b: 4, id: 1})
         
     })
