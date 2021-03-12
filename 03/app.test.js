cfaa4623bb8f3c129e6cfb9dca007f06740de976
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2021-03-12 14:24:18 +0100

    Add 5 tests

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,24 @@
 import randomNumber from './app';
+
+it('return 0 when min is not set', () => {
+    expect(randomNumber()).toBe(0);
+});
+
+it('return 0 when max is not set', () => {
+    expect(randomNumber()).toBe(0);
+})
+
+it('throw exception when min, max, both is not a number', () => {
+    const notNumber = 'a';
+    expect(() => randomNumber(notNumber, undefined)).toThrow();
+    expect(() => randomNumber(undefined, notNumber)).toThrow();
+    expect(() => randomNumber(notNumber, notNumber)).toThrow();
+});
+
+it('return 1 when min and max has value 1', () => {
+    expect(randomNumber(1, 1)).toBe(1);
+});
+
+it('throw exception when min is bigger than max', () => {
+    expect(() => randomNumber(10, 20)).toThrow;
+});
