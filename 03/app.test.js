c56eb5ddad682438fc8dbc3cedbe9812c4e89c46
Karolina Jedrajczyk <kalka@MacBook-Pro-Karolina.local>
2021-03-22 19:37:50 +0100

    message

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,12 @@
+
+
 import randomNumber from './app';
+
+test('random number 1 to 1 ', () => {
+    const random = randomNumber(1,1)
+   expect(random).toBe(1);
+});
+
+test('nieliczba', () => {
+expect(() => randomNumber('a',3)).toThrow();
+})
--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,27 @@
 export default class User {
+    constructor({ email, password }) {
+
+
+        this.email = email;
+        this.password = password;
+
+
+        if (!this.email.includes('@')) {
+            throw new Error('Invalid Email')
+        }
+
+        if (this.password.length < 5) {
+            throw new Error('The password must contain min. 5 characters')
+        }
+    }
+
+
+    login() {
+        if (this.email.includes("devmentor.pl")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
