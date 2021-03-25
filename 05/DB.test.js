643e3383fc5386e0e41c654dbee01e31e869ffdc
Karolina Jedrajczyk <kalka@pc19.home>
2021-03-25 10:09:47 +0100

    message

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,13 @@
 import DB from './DB';
+
+describe('insert method', () => {
+    test('return data when ID is provided', async() => {
+        expect.assertions(1);
+        let db = new DB();
+        let id = 5;
+        let promise =  db.insert();
+        return promise.then( result => {
+            expect(result).toBe(id);
+        })
+    })
+})
