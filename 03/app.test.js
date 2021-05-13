41873bad77eb833c0ae1c0cf0204be0792233f89
Agnieszka Polińska <agapoli@gmail.com>
2021-05-13 09:18:12 +0200

    Add test which check if first argument is smaller than second one

--- 03/app.test.js
+++ 03/app.test.js
@@ -9,3 +9,7 @@ it('throws error if argument is not a number', () => {
 it('returns x if both arguments are equal x', () => {
     expect(randomNumber(1,1)).toBe(1);
 })
+
+it('throws error if min is bigger than max', () => {
+    expect(() => randomNumber(3,1)).toThrow('The first argument should be smaller than second one');
+})
