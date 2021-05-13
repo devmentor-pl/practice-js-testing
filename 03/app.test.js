45fc9d288ac5baf63f777840d85f3a2b2bd02dfe
Agnieszka Polińska <agapoli@gmail.com>
2021-05-13 09:00:49 +0200

    Add test to check if random number is from min-max interval

--- 03/app.test.js
+++ 03/app.test.js
@@ -5,3 +5,7 @@ it('throws error if argument is not a number', () => {
     expect(() => randomNumber(0,'0')).toThrow('Both arguments should be a number');
     expect(() => randomNumber('0','0')).toThrow('Both arguments should be a number');
 })
+
+it('returns x if both arguments are equal x', () => {
+    expect(randomNumber(1,1).toBe(1));
+})
