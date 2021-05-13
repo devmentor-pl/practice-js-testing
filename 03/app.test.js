3cbd46ac9e11fe286a92876d4133ad477b83fa17
Agnieszka Polińska <agapoli@gmail.com>
2021-05-13 08:47:04 +0200

    invoke randomNumber() as a callback

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,7 +1,7 @@
 import randomNumber from './app';
 
 it('throws error if argument is not a number', () => {
-    expect(randomNumber('0',0)).toThrow('not each argument is number');
-    expect(randomNumber(0,'0')).toThrow('not each argument is number');
-    expect(randomNumber('0','0')).toThrow('Both arguments should be a number');
+    expect(() => randomNumber('0',0)).toThrow('Both arguments should be a number');
+    expect(() => randomNumber(0,'0')).toThrow('Both arguments should be a number');
+    expect(() => randomNumber('0','0')).toThrow('Both arguments should be a number');
 })
