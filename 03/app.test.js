af562bd7c9930dbec142b20e9df369544ad71f13
Agnieszka Polińska <agapoli@gmail.com>
2021-05-13 09:52:55 +0200

    Add test with check if random number is from interval

--- 03/app.test.js
+++ 03/app.test.js
@@ -13,3 +13,10 @@ it('returns x if both arguments are equal x', () => {
 it('throws error if min is bigger than max', () => {
     expect(() => randomNumber(3,1)).toThrow('The first argument should be smaller than second one');
 })
+
+it('checks if random number id from interval', () => {
+    const min = 1;
+    const max = 3;
+    expect(randomNumber(min,max)).toBeGreaterThan(min);
+    expect(randomNumber(min,max)).toBeLessThan(max);
+})
