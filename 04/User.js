33a201c3477ba7263c843f679b696485e5d3773e
Agnieszka Polińska <agapoli@gmail.com>
2021-05-13 11:53:20 +0200

    Implement User class

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,37 @@
 export default class User {
+    constructor({email, password}) {
+        this._validateEmail(email);
+        this._validatePassword(password);
+        this.email = email;
+        this.password = password;
+    }
+
+    _validateEmail(email) {
+        //https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const validatedEmail = re.test(String(email).toLowerCase());
+
+        if (!validatedEmail) {
+            throw new Error('The email is incorrect');
+        }
+    }
+
+    _validatePassword(password) {
+        if (password.length < 5) {
+            throw new Error('The password should have minimum 6 characters');
+        }
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        return this.email.endsWith('devmentor.pl');
+    }
 
 }
