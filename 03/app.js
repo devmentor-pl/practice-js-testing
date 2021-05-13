de316e97c7bb96ce59ef7d3ecfdbfe144b001e8d
Agnieszka Polińska <agapoli@gmail.com>
2021-05-13 09:12:35 +0200

    Implement logic of randomNumber()

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,7 @@ export default function randomNumber(min, max) {
     if (typeof min !== "number" || typeof max !== "number") {
         throw new Error('Both arguments should be a number');
     }
+
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumber;
 }
