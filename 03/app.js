98d6b1f2beddf99c6b0d8111d5b53869c3210974
Agnieszka Polińska <agapoli@gmail.com>
2021-05-13 09:20:01 +0200

    Implement 'if' which check if max is bigger than min

--- 03/app.js
+++ 03/app.js
@@ -3,6 +3,10 @@ export default function randomNumber(min, max) {
         throw new Error('Both arguments should be a number');
     }
 
+    if (min > max) {
+        throw new Error('The first argument should be smaller than second one');
+    }
+
     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     return randomNumber;
 }
