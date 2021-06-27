1c5fbf4294b24a186a1405268824d222b757b700
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-06-27 16:14:05 +0200

    Task 03 red
    Task 03 red

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
-export default function randomNumber(min, max) {
+// export default 
+function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    number = Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+for (let i = 1; i < 21; i++) {
+    console.log(i, randomNumber(1, 100))
 }
