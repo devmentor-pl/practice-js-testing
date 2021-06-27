c6e3c52b34b6c99fd6bd2b274ec4102d82ec0234
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-06-27 16:15:19 +0200

    Task 03 green
    Task 03 green

--- 03/app.js
+++ 03/app.js
@@ -3,6 +3,7 @@ function randomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     number = Math.floor(Math.random() * (max - min + 1)) + min;
+    return number;
 }
 
 for (let i = 1; i < 21; i++) {
