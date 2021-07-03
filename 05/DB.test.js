3396b5d381133796f8850bae9da06ff73f4d311f
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-03 10:58:07 +0200

    Task 05
    Task 05

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,14 @@
 import DB from './DB';
+
+describe('data', () => {
+
+    it('if data.id is 1', () => {
+        const db = new DB();
+        return expect(db.insert({a: 3, b: 4})).resolves.toStrictEqual({"a": 3, "b": 4, "id": 1});
+    });
+
+    it('if data.id is a number', () => {
+        const db = new DB();
+        return expect(db.insert({a: 3, b: 4, id: "id"})).resolves.toStrictEqual({"a": 3, "b": 4, "id": "id"});
+    });
+});
