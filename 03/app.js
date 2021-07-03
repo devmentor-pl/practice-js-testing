3aadaef1d7cf047d0a872bad47f071499ade7f4e
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-03 12:01:26 +0200

    Task 03: test 01
    Task 03: test 01

--- 03/app.js
+++ 03/app.js
@@ -2,9 +2,15 @@
 function randomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (min === 1 && max === 1 && result !== 1) {
+        throw new RangeError('If min and max are 1, result must be 1')
+    };
+
+    return result;
 }
 
 for (let i = 1; i < 21; i++) {
-    console.log(i, randomNumber(1, 100))
+    console.log(`Result ${i}:  ${randomNumber(1, 1)}`);
 }
