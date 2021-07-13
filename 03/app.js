9763288498d48fed4e57e68e0ea283864c3cdac9
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-13 10:34:16 +0200

    Task 03: first test
    Task 03: first test

--- 03/app.js
+++ 03/app.js
@@ -2,38 +2,15 @@ export default function randomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
 
-    if (min > max) {
-        max = min;
-    }
-    if ((typeof min !== 'number' || isNaN(min)) && (typeof max === 'number' && !isNaN(max))) {
-        min = max;
-    }
-    if ((typeof max !== 'number' || isNaN(max)) && (typeof min === 'number' && !isNaN(min))) {
-        max = min;
-    }
-    if ((typeof min !== 'number' || isNaN(min)) && (typeof max !== 'number' || isNaN(max))) {
-        min = 1;
-        max = 1;
-    }
+    console.log(min)
+    console.log(max)
 
     let result = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    console.log(result)
 
-    if (min === max && result !== min) {
-        throw new RangeError('If \"min\" and \"max\" are equal, \"result\" must be equal \"min\" and \"max\"');
-    };
-    if (min > max) {
-        throw new RangeError('\"min\" cannot be bigger than \"max\"!');
-    };
-    if (typeof min !== 'number' || isNaN(min) || typeof max !== 'number' || isNaN(max)) {
-        throw new RangeError('\"min\" and \"max\" must be numbers!');
-    };
-    if (result < min || result > max) {
-        throw new RangeError('\"result\" cannot be smaller than \"min\" and \"result\" cannot be bigger than \"max\"!');
-    };
     return result;
 }
 
-console.log('Testing randomNumbers');
-for (let i = 1; i < 21; i++) {
-    console.log(`Result ${i}:  ${randomNumber(3, 4)}`);
-}
+randomNumber(10, 1);
+
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,6 @@
 import randomNumber from './app';
+
+it('randomNumber is a number', () => {
+    expect(typeof randomNumber(10, 1)).toBe('number');
+})
+
