5729e596a2c25662ba49c3f9b44826714c5b5268
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-13 11:32:03 +0200

    Task 03: second test refactor
    Task 03: second test refactor

--- 03/app.js
+++ 03/app.js
@@ -5,8 +5,14 @@ export default function randomNumber(min, max) {
     console.log(min)
     console.log(max)
 
+    // both parameters must be numbers
     if (typeof min !== 'number' || isNaN(min) || typeof max !== 'number' || isNaN(max)) {
         console.log('Both parameters must be numbers!')
+    }
+
+    // parameter min must be less or equal as parameter max
+    if (min > max) {
+        min = max;
     }
 
     let result = Math.floor(Math.random() * (max - min + 1)) + min;
--- 03/app.test.js
+++ 03/app.test.js
@@ -4,8 +4,13 @@ it('randomNumber is a number', () => {
     expect(typeof randomNumber(10, 1)).toBe('number');
 })
 
-it('parameter min must be smaller or equal as parameter max', () => {
-    randomNumber.min = 10;
+it('parameter min must be less or equal as parameter max', () => {
+
+    randomNumber.min = 11;
     randomNumber.max = 10;
+    if (randomNumber.min > randomNumber.max) {
+        randomNumber.min = randomNumber.max;
+    }
+
     expect(randomNumber.min <= randomNumber.max).toBe(true);
 })
