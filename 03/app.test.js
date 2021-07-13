7e436c5ee858de2d22295a56499b35492f706714
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-13 11:19:51 +0200

    Task 03: second test red
    Task 03: second test red

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,8 @@ it('randomNumber is a number', () => {
     expect(typeof randomNumber(10, 1)).toBe('number');
 })
 
+it('parameter min must be smaller or equal as parameter max', () => {
+    min = 10;
+    max = 1;
+    expect(min <= max).toBe(true);
+})
