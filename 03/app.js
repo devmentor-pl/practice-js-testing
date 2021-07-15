9b652027ad12a3e81340b2dd547374b4fc1e8e91
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-15 11:03:13 +0200

    Task 03: test 01 red
    Task 03: test 01 red

--- 03/app.js
+++ 03/app.js
@@ -1,36 +1,11 @@
 export default function randomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-
-    console.log(min)
-    console.log(max)
-
-    // both parameters must be numbers
-    if (typeof min !== 'number' || isNaN(min) || typeof max !== 'number' || isNaN(max)) {
-        console.log('Both parameters must be numbers!')
-    }
-
-    // parameter min must be less or equal as parameter max
-    if (min > max) {
-        min = max;
-    }
-
-    let result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    // result must be greater or equal as parameter min
-    if (result < min) {
-        result = min;
-    }
-
-    // result must be less or equal as parameter max
-    if (result > max) {
-        result = max;
-    }
-
-    console.log(result)
+    
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     return result;
 }
 
-randomNumber(10, 1);
+randomNumber(1, 10);
 
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,71 +1,8 @@
 import randomNumber from './app';
 
 it('randomNumber is a number', () => {
-    expect(typeof randomNumber(10, 1)).toBe('number');
-})
 
-it('parameter min must be less or equal as parameter max', () => {
-
-    randomNumber.min = 11;
-    randomNumber.max = 10;
-    if (randomNumber.min > randomNumber.max) {
-        randomNumber.min = randomNumber.max;
-    }
-
-    expect(randomNumber.min <= randomNumber.max).toBe(true);
-})
-
-it('result must be greater or equal as parameter min', () => {
-
-    randomNumber.min = 5;
-    randomNumber.max = 10;
-    randomNumber.result = 4;
-    if (randomNumber.result < randomNumber.min) {
-        randomNumber.result = randomNumber.min;
-    }
-
-    expect(randomNumber.result >= randomNumber.min).toBe(true);
-})
-
-// test with new object randomNumber2
-it('result must be greater or equal as parameter min', () => {
-
-    const randomNumber2 = new randomNumber;
-
-    randomNumber2.min = 5;
-    randomNumber2.max = 10;
-    randomNumber2.result = 2;
-    if (randomNumber2.result < randomNumber2.min) {
-        randomNumber2.result = randomNumber2.min;
-    }
-
-    expect(randomNumber2.result >= randomNumber2.min).toBe(true);
-})
-
-it('result must be less or equal as parameter max', () => {
-
-    randomNumber.min = 5;
-    randomNumber.max = 10;
-    randomNumber.result = 12;
-    if (randomNumber.result > randomNumber.max) {
-        randomNumber.result = randomNumber.max;
-    }
-
-    expect(randomNumber.result <= randomNumber.max).toBe(true);
-})
-
-// test with new object randomNumber2
-it('result must be less or equal as parameter max', () => {
-
-    const randomNumber2 = new randomNumber;
-
-    randomNumber2.min = 5;
-    randomNumber2.max = 10;
-    randomNumber2.result = 22;
-    if (randomNumber2.result > randomNumber2.max) {
-        randomNumber2.result = randomNumber2.max;
-    }
-
-    expect(randomNumber2.result <= randomNumber2.max).toBe(true);
+    expect(typeof randomNumber(min, max)).toBe('number');
+    
 })
 
