0d8b3744a9a96e6229a7898f73170c0bc28df121
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-15 11:14:01 +0200

    Task 03: test 01 refactor
    Task 03: test 01 refactor

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,11 @@
 export default function randomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
+
+    // both parameters must be numbers
+    if (typeof min !== 'number' || isNaN(min) || typeof max !== 'number' || isNaN(max)) {
+        alert('Both parameters must be numbers!');
+    }
     
     let result = Math.floor(Math.random() * (max - min + 1)) + min;
 
