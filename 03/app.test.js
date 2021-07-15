3ab84645cf62c3fce2d370d2a6d68c0ff882017e
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-07-15 11:04:22 +0200

    Task 03: test 01 green
    Task 03: test 01 green

--- 03/app.test.js
+++ 03/app.test.js
@@ -2,7 +2,10 @@ import randomNumber from './app';
 
 it('randomNumber is a number', () => {
 
+    const min = 1;
+    const max = 10;
+
     expect(typeof randomNumber(min, max)).toBe('number');
-    
+
 })
 
