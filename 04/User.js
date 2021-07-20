1b3dd404e45f3de404b6de7326c7c90aa811f411
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-07-20 17:22:27 +0200

    Task 04

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,30 @@
 export default class User {
+    constructor({email, password}) {
+        if(!email.includes('@')) {
+            throw new Error('Email is incorrect!');
+        }
 
+        if(password.length <= 3) {
+            throw new Error('Password is incorrect!');
+        }
+
+        this.email = email;
+        this.password = password;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        if (this.email.includes('devmentor.pl')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
