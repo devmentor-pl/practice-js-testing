954c64cae17d612597442a56b50a35ebf5315814
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-07-20 16:44:54 +0200

    Task 03 - 2 test

--- 03/app.js
+++ 03/app.js
@@ -3,4 +3,7 @@ export default function randomNumber(min, max) {
     if(min === 1 && max === 1) {
         return 1;
     }
+
+    const randomNum = Math.floor(Math.random() * (max - min)) + min
+    return randomNum;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('return 1 when min, max is 1', () => {
     expect(randomNumber(1, 1)).toBe(1);
 });
+
+it('return 0 when min, max is not set', () => {
+    expect(randomNumber()).toBe(0); 
+})
