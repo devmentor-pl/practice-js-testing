a9dd7ffa79efda52258b00bd138bf7704759e146
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-07-24 23:02:54 +0200

    task4

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,49 @@
+// To zadanie polega na zaimplementowaniu funkcjonalności na podstawie testów, które są przygotowane w pliku User.test.js. Zadanie zostanie wykonane jeśli wszystkie testy będą przechodzić.
+
+// PS. Testy możesz uruchomić przy pomocy komenty npm run test-04
+
+
+
+// Jeśli spojrzysz do pliku package.json to zobaczysz, że uruchomienie testów dla tego zadania będzie możliwe przy pomocy komendy: npm run test-03.
+
+// PS. Pamiętaj, aby zainstalować niezbędne paczki poprzez npm i.
+
 export default class User {
 
+    constructor({ email, password }) {
+
+        this.email = email;
+        this.password = password;
+
+        this.checkData()
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+
+    login() {
+        if (this.email.includes('devmentor.pl')) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    checkData() {
+        if (!this.email.includes('@')) {
+            throw new Error('email is incorrect!');
+        }
+
+        if (this.password.length <= 5) {
+            throw new Error('password is too short!');
+        }
+
+    }
 }
