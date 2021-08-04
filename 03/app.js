2e8737cbc91cde6a032df06c497e1889a373eae4
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-08-04 12:10:39 +0200

    Second test - ready

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,6 @@
 export default function randomNumber(min, max) {
+    if(typeof min !== 'number') {
+        throw new Error('Arg need to be a number!');
+    }
     return Math.round(Math.random() * (max - min)) + min;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,10 +1,12 @@
 import randomNumber from './app';
 
 it('returns 1 from the range 1 - 1', () => {
-    expect(randomNumber(1,1)).toBe(1);
+    expect(randomNumber(1, 1)).toBe(1);
 })
 
 it('throws error if arg1 is NaN', () => {
-    expect(randomNumber('1',1).toThrow())
-
+    function testNumber(){
+        randomNumber('1', 1);
+    }
+    expect(testNumber).toThrow();
 })
