2af2987a2ab538e020b4bfc26f164ca558e5905b
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-08-04 11:38:53 +0200

    Second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,8 @@ import randomNumber from './app';
 it('returns 1 from the range 1 - 1', () => {
     expect(randomNumber(1,1)).toBe(1);
 })
+
+it('throws error if arg1 is NaN', () => {
+    expect(randomNumber('1',1).toThrow())
+
+})
