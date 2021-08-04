13ae6cb744ae3acefd544c153212907f6421e94a
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-08-04 12:13:54 +0200

    Third test - ready

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,5 @@
 export default function randomNumber(min, max) {
-    if(typeof min !== 'number') {
+    if(typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('Arg need to be a number!');
     }
     return Math.round(Math.random() * (max - min)) + min;
