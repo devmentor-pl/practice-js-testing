6f06dc992b5d6036f13c32939bcb55cd1bd271f2
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-10 14:46:44 +0100

    03 modified test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,6 +1,5 @@
 import randomNumber from "./app";
 
 it("return error if only one argument is set", () => {
-    const random = randomNumber(1);
-    expect(random).toThrow(new Error("One of arguments is missing"));
+    expect(randomNumber(1)).toThrow(new Error("One of arguments is missing"));
 });
