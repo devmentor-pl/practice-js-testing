63fa7a98973747c9dacee71333ec4606fff1a4ae
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-10 15:48:49 +0100

    03 issues with error tests

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,13 @@
 import randomNumber from "./app";
 
-it("return error if only one argument is set", () => {
-    expect(randomNumber(1)).toThrow(new Error("One of arguments is missing"));
+it("return error if min is not a number", () => {
+    expect(randomNumber("string", 1)).toThrow();
+});
+
+it("return 1 if min is 1 and max is 1", () => {
+    expect(randomNumber(1, 1)).toBe(1);
+});
+
+it("return error if max is larger than min", () => {
+    expect(randomNumber(8, 2)).toThrow();
 });
