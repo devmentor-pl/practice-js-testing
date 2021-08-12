f06d6309558f6bf2e5a84d78889d938d4a757980
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-12 11:10:47 +0100

    04 not solved

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,57 @@
 export default class User {
+    /*constructor({ email, password }) {
+        //this.email;
+        //this.password;
+        this.getEmail(email);
+        this.getPassword(password);
+    }
 
+    getEmail(email) {
+        if (!email.includes("@")) {
+            throw new Error("Not an email");
+        }
+        this.email = email;
+    }
+
+    getPassword(password) {
+        if (password.length < 5) {
+            throw new Error("Password too short");
+        }
+        this.password = password;
+    }
+
+    login() {
+        if (this.email.includes("devmentor.pl")) {
+            return true;
+        } else return false;
+    }*/
+
+    constructor(obj) {
+        this.email = this.getEmail(obj);
+        this.password = this.getPassword(obj);
+        //this.email;
+        //this.password;
+    }
+
+    getEmail(obj) {
+        const email = obj.email;
+        if (!email.includes("@")) {
+            throw new Error("Not an email");
+        }
+        return email;
+    }
+
+    getPassword(obj) {
+        const password = obj.password;
+        if (password.length < 5) {
+            throw new Error("Password too short");
+        }
+        return password;
+    }
+
+    login() {
+        if (this.email.includes("devmentor.pl")) {
+            return true;
+        } else return false;
+    }
 }
