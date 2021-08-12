eae68f681b6211f6bef0136d357d5f22336c250f
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-12 16:27:25 +0100

    04 solved

--- 04/User.js
+++ 04/User.js
@@ -1,52 +1,30 @@
 export default class User {
-    /*constructor({ email, password }) {
-        //this.email;
-        //this.password;
-        this.getEmail(email);
-        this.getPassword(password);
-    }
-
-    getEmail(email) {
-        if (!email.includes("@")) {
-            throw new Error("Not an email");
-        }
+    constructor({ email, password }) {
         this.email = email;
-    }
-
-    getPassword(password) {
-        if (password.length < 5) {
-            throw new Error("Password too short");
-        }
         this.password = password;
+
+        this.validateEmail();
+        this.validatePassword();
     }
 
-    login() {
-        if (this.email.includes("devmentor.pl")) {
-            return true;
-        } else return false;
-    }*/
-
-    constructor(obj) {
-        this.email = this.getEmail(obj);
-        this.password = this.getPassword(obj);
-        //this.email;
-        //this.password;
+    getEmail() {
+        return this.email;
     }
 
-    getEmail(obj) {
-        const email = obj.email;
-        if (!email.includes("@")) {
+    getPassword() {
+        return this.password;
+    }
+
+    validateEmail() {
+        if (!this.email.includes("@")) {
             throw new Error("Not an email");
         }
-        return email;
     }
 
-    getPassword(obj) {
-        const password = obj.password;
-        if (password.length < 5) {
+    validatePassword() {
+        if (this.password.length < 5) {
             throw new Error("Password too short");
         }
-        return password;
     }
 
     login() {
