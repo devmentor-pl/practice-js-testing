52a2b96cb56d25acdd90e5183568921ece87f6c6
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-03 12:44:54 +0200

    Task 03 Second test's implementation

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,7 @@ export default function randomNumber(min, max) {
     if (typeof min !== 'number') {
         throw new Error('min must be a number')
     }
+    if (typeof max !== 'number') {
+        throw new Error('max must be a number')
+    }
 }
