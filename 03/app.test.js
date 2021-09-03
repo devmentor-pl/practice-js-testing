de11789faf0fb85875320e8902d48be1d90bb6b9
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-03 14:19:51 +0200

    Finish task 03

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,28 +1,32 @@
 import randomNumber from './app';
 
-it('Should throw exception when min not a number', () => {
-    function checkNumber() {
-        randomNumber('1')
-    }
-    expect(checkNumber).toThrow()
+describe('Range', () => {
+    it('Should throw exception when min not a number', () => {
+        function checkNumber() {
+            randomNumber('1')
+        }
+        expect(checkNumber).toThrow()
+    })
+    
+    it('Should throw exception when max is not a number', () => {
+        function checkNumber() {
+            randomNumber(5, '1')
+        }
+        expect(checkNumber).toThrow()
+    })
+    
+    it('Should throw exceptation when valid range', () => {
+        function checkRange() {
+            randomNumber(5, 3)
+        }
+        expect(checkRange).toThrow()
+    })
 })
 
-it('Should throw exception when max is not a number', () => {
-    function checkNumber() {
-        randomNumber(5, '1')
-    }
-    expect(checkNumber).toThrow()
-})
-
-it('Should throw exceptation when valid range', () => {
-    function checkRange() {
-        randomNumber(5, 3)
-    }
-    expect(checkRange).toThrow()
-})
-
-it('Return number greater than 5 and less than 10', () => {
-    const number = randomNumber(5, 10);
-    expect(number).toBeGreaterThan(5);
-    expect(number).toBeLessThanOrEqual(10);
+describe('Drawing number', () => {
+    it('Return number greater than 5 and less than 10', () => {
+        const number = randomNumber(5, 10);
+        expect(number).toBeGreaterThan(5);
+        expect(number).toBeLessThanOrEqual(10);
+    })
 })
