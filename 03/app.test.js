6f3ded32b814127e9d992b83ba01c2dc5675259d
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-03 12:43:06 +0200

    Task 03 Second Test

--- 03/app.test.js
+++ 03/app.test.js
@@ -6,3 +6,10 @@ it('Should throw exception when min not a number', () => {
     }
     expect(checkNumber).toThrow()
 })
+
+it('Should throw exception when max is not a number', () => {
+    function checkNumber() {
+        randomNumber(5, '1') 
+    }
+    expect(checkNumber).toThrow()
+})
