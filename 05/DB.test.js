1c16c31290bd41a1705a51332e8875bb607771cd
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-03 18:00:04 +0200

    Task 05 Tests describe ID number

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,44 @@
 import DB from './DB';
+
+describe('check ID number', () => {
+    it('should inform if ID is not a number', () => {
+        expect.assertions(1);
+        const db = new DB();
+        const data = {
+            id: '3'
+        }
+        const promise = db.insert(data);
+        return promise.catch(err => {
+            expect(err).toBe('ID can be only number!')
+        })
+    })
+
+    it('should inform if ID is not a number', () => {
+        expect.assertions(1);
+        const db = new DB();
+        db._rows = [{
+            name: "name",
+            id: 3
+        }]
+        const data = {
+            id: 3
+        }
+        const promise = db.insert(data);
+        return promise.catch(err => {
+            expect(err).toBe('ID can\'t be duplicated!')
+        })
+    })
+
+    it('should return ID when it is a number', () => {
+        expect.assertions(1);
+        const db = new DB();
+        const data = {
+            id: 3
+        }
+        const promise = db.insert(data);
+        return promise.then(result => {
+            expect(result).toBe(data)
+        })
+    })
+
+})
