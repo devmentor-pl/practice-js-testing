470e58c0dd450be18f4f2e1f10d234ef8c996062
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-03 12:36:28 +0200

    Task 03  First test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,8 @@
 import randomNumber from './app';
+
+it('Should throw exception when min not a number', () => {
+    function checkNumber() {
+        randomNumber('1') 
+    }
+    expect(checkNumber).toThrow()
+})
