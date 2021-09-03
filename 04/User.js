a8dad144e7fcfb2a53139cab1ba9a068ba18356e
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-03 15:28:57 +0200

    Finish task 04

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,35 @@
 export default class User {
+    constructor({
+        email,
+        password
+    }) {
+        this.email = email;
+        this.password = password;
+        this.validateData();
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    validateData() {
+        if (!this.email.includes('@')) {
+            throw new Error('Email is incorect')
+        }
+        if (this.password.length < 8) {
+            throw new Error('Email is incorect')
+        }
+    }
+    login() {
+        if (this.email.endsWith('devmentor.pl')) {
+            return true;
+        } else {
+            return false
+        }
+    }
 
 }
