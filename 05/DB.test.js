59f3c5760d952f1d62c81198ae4ede6f8201c0e9
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-04 17:29:05 +0200

    Finish task 05

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -47,7 +47,7 @@ describe('select', () => {
     it('should return row when give an ID', () => {
         expect.assertions(1);
         const db = new DB();
-        db._rows = [{
+        db._rows = [{ // czy powinnam przekazac id poprzez db.insert???
             name: "name",
             id: 3
         }, {
@@ -75,8 +75,7 @@ describe('select', () => {
     })
 })
 describe('remove', () => {
-    it('should return when ID is found', () => {
-        expect.assertions(1);
+    it('should return when ID is found', async () => {
         const db = new DB();
         db._rows = [{
             name: "name",
@@ -86,11 +85,10 @@ describe('remove', () => {
             id: 5
         }]
         const id = 5;
-        const promise = db.remove(id)
-        return promise.then(result => {
-            expect(result).toBe('Item was remove!') 
-        })
+        const result = await db.remove(id)
+        expect(result).toBe('Item was remove!')
     })
+
     it('should inform when ID not exist', () => {
         expect.assertions(1);
         const db = new DB();
@@ -106,5 +104,58 @@ describe('remove', () => {
         return promise.catch(err => {
             expect(err).toBe('Item not exist!')
         })
+    })
+})
+
+describe('update', () => {
+    it('should inform when ID have to be set', () => {
+        expect.assertions(1);
+        const db = new DB();
+        const data = {
+            name: 3
+        }
+        const promise = db.update(data);
+        return promise.catch(err => {
+            expect(err).toBe('ID have to be set!')
+        })
+    })
+
+    it('should return when ID is found', async () => {
+        expect.assertions(1);
+        const db = new DB();
+        const data = {
+            name: "name",
+            id: 3
+        }
+        await db.insert(data);
+        const newData = {
+            name: "neName",
+            id: 3
+        }
+        const promise = await db.update(newData);
+        if (db._rows.some(item => item.id === newData.id)) {
+            return expect(promise).toBe(newData)
+        }
+    })
+
+    it('should return when ID is found', () => {
+        expect.assertions(1);
+        const db = new DB();
+        const data = {
+            name: "name",
+            id: 3
+        }
+        db.insert(data);
+        db.remove(3);
+        const newData = {
+            name: "newName",
+            id: 3
+        }
+        const promise = db.update(newData);
+        if (!db._rows.some(item => item.id === newData.id)) {
+            return promise.catch(err => {
+                expect(err).toBe('ID not found!')
+            })
+        }
     })
 })
