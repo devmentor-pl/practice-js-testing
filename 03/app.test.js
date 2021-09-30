e158e75e950ea6f1c54f283d7b8f9738695208c7
Jakub Chrobak <jchrobakk@gmail.com>
2021-09-30 17:14:39 +0200

    03 first test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,11 @@
-import randomNumber from './app';
+import randomNumber from "./app";
+
+describe("Random number generator", () => {
+  it("should throw an error if no argument is provided", () => {
+    function useGenerator() {
+      randomNumber();
+    }
+
+    expect(useGenerator).toThrow();
+  });
+});
