943164c227fb331baeeb0a0059c5417d2b06e4a4
Jakub Chrobak <jchrobakk@gmail.com>
2021-09-30 17:29:35 +0200

    03 third test pass

--- 03/app.js
+++ 03/app.js
@@ -1,17 +1,21 @@
 export default function randomNumber(min, max) {
-  if (isArgumentsProvided()) {
+  if (isArgumentsProvided(min, max)) {
     throw new Error("No arguments provided");
   }
 
-  if (isMinGreater()) {
+  if (isMinGreater(min, max)) {
     throw new Error("Min must be greater than max");
   }
+
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return randomNumber;
 }
 
-function isArgumentsProvided() {
+function isArgumentsProvided(min, max) {
   return min === undefined || max === undefined ? true : false;
 }
 
-function isMinGreater() {
+function isMinGreater(min, max) {
   return min > max ? true : false;
 }
