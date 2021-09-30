eaa5d203a5d1bd24cc22b69882794a9134d6f00d
Jakub Chrobak <jchrobakk@gmail.com>
2021-09-30 17:18:25 +0200

    03 second test pass

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,8 @@ export default function randomNumber(min, max) {
   if (min === undefined || max === undefined) {
     throw new Error("No arguments provided");
   }
+
+  if (min > max) {
+    throw new Error("Min must be greater than max");
+  }
 }
