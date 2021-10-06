ee3895d6e22ba218c85baf62129aef04ef96c5e2
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-10-06 14:11:35 +0200

    third test written

--- 03/app.js
+++ 03/app.js
@@ -4,6 +4,7 @@ export default function randomNumber(min, max) {
         return min;
     }
 
+
     if(typeof min !== 'number' || typeof max !=='number') {
         throw new Error('Property have to be a number');
     }
--- 03/app.test.js
+++ 03/app.test.js
@@ -4,6 +4,7 @@ it('return 1 when both randmized numbers are 1' , () => {
     expect(randomNumber(1,1)).toBe(1);
 });
 
+
 it('throw error if parametr is not a number' , () => {
 
     function createNumber() {
@@ -14,3 +15,15 @@ it('throw error if parametr is not a number' , () => {
 
     expect(createNumber).toThrow();
 });
+
+
+it('throw error if max > min' , () => {
+
+    function compareNumber() {
+        const min = 4;
+        const max = 3;
+        randomNumber(min,max);
+    }
+
+    expect(compareNumber).toThrow();
+})
