c7b27b4aad6da3af86e127e72bb0333a89a2a652
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-10-06 13:40:16 +0200

    second test written

--- 03/app.test.js
+++ 03/app.test.js
@@ -2,4 +2,15 @@ import randomNumber from './app';
 
 it('return 1 when both randmized numbers are 1' , () => {
     expect(randomNumber(1,1)).toBe(1);
-})
+});
+
+it('throw error if parametr is not a number' , () => {
+
+    function createNumber() {
+        const min = 'abc'
+        const max = 5;
+        randomNumber(min , max);
+    }
+
+    expect(createNumber).toThrow();
+});
