c7093b661edf825d55be7ed3484cad9a2850e0d5
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-10-21 02:20:37 +0200

    feat: add test when one of args is NaN

--- 03/app.test.js
+++ 03/app.test.js
@@ -9,10 +9,19 @@ it('throw exception when first arg is not a number', () => {
     expect(drawNumber).toThrow();
 });
 
-it('throw excpetion when second arg is not a number', () => {
+it('throw exception when second arg is not a number', () => {
     function drawNumber() {
         const min = 2;
         const max = undefined;
+        randomNumber(min, max);
+    }
+    expect(drawNumber).toThrow();
+});
+
+it('throw exception when one of arg is NaN', () => {
+    function drawNumber() {
+        const min = NaN;
+        const max = 1;
         randomNumber(min, max);
     }
     expect(drawNumber).toThrow();
