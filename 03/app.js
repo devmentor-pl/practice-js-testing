d3104c9642a4df23cb33e5036c875f6d12678004
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-10-21 02:12:55 +0200

    feat: add instruction checking type of 2nd argument

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,9 @@ export default function randomNumber(min, max) {
     if (!isVariableType(min, 'number')) {
         throw new Error('Argument have to be a number');
     }
+    if (!isVariableType(max, 'number')) {
+        throw new Error('Argument have to be a number');
+    }
 }
 
 function isVariableType(variable, type) {
