37b533403cafb1bbbacc5e08daf89b3396b1137a
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-10-21 02:11:32 +0200

    feat: add test for type of 2nd argument

--- 03/app.test.js
+++ 03/app.test.js
@@ -8,3 +8,12 @@ it('throw exception when first arg is not a number', () => {
     }
     expect(drawNumber).toThrow();
 });
+
+it('throw excpetion when second arg is not a number', () => {
+    function drawNumber() {
+        const min = 2;
+        const max = undefined;
+        randomNumber(min, max);
+    }
+    expect(drawNumber).toThrow();
+})
