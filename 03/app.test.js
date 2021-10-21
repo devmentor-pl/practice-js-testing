c05c4b8bb2cc339157cb33208d9ca2d5040b6f24
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-10-21 02:02:38 +0200

    feat: add test for type of argument

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,10 @@
 import randomNumber from './app';
+
+it('throw exception when first arg is not a number', () => {
+    function drawNumber() {
+        const min = 'x';
+        const max = 6;
+        randomNumber(min, max);
+    }
+    expect(drawNumber).toThrow();
+});
