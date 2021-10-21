f5705913c136ebc33a66adc84047a60f8547937e
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-10-21 18:27:47 +0200

    feat: add instruction checking if min < max

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,10 @@ export default function randomNumber(min, max) {
     if (!isVariableValid(min) || !isVariableValid(max)) {
         throw new Error('Both arguments have to be a number');
     }
+
+    if (min > max) {
+        throw new RangeError('max has to be greater than min!');
+    }
 }
 
 function isVariableValid(variable, type = 'number') {
