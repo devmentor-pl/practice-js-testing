3999820ab35b2aab212c1725b3181a7c8cf83017
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-11-19 14:25:49 +0100

    Green, first test done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,5 @@
 export default function randomNumber(min, max) {
-    
+    if (typeof min !== 'string' || typeof max !== 'string') {
+        throw new Error ('Property have to be a number');
+    } 
 }
