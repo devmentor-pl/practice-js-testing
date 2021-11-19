bd5807a19c1bc658a598d84d6485877be14e2f50
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-11-19 12:36:14 +0100

    Second task

--- 02/app.js
+++ 02/app.js
@@ -4,21 +4,41 @@ function init() {
     const clickEl = document.querySelector('.error--click');
     const enterEl = document.querySelector('.error--enter');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+        setRandomPosition(clickEl);
+        setRandomPosition(enterEl);
+        initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+        initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
+    
 
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
-
-    if(error) {
-        throw error;
+    try{
+        element.style.top = Math.random() * 600 + 'px';
+        element.style.left = Math.random() * 800 + 'px';
+    
+        if(error) {
+            throw error;
+        }
     }
+    catch(e) {
+        const alert = document.querySelector('.alert__message');
+        const message = e.message;
+        const window  = alert.parentElement.parentElement;
+        window.style.display = 'block';
+        alert.innerText = message;
+
+        closeAlert(window);
+    }
+}
+
+function closeAlert(window) {
+    window.addEventListener('click', () => {
+
+        const window = document.querySelector('.alert');
+        window.style.display = 'none';
+    })
 }
 
 function initEventWithError(element, eventName, error) {
