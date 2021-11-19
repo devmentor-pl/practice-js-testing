fcd8a5f4bdab97415de5800b8b2863636820b63a
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-11-19 14:50:23 +0100

    Second red test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,10 +1,19 @@
 import randomNumber from './app';
 
 it ('throw error, when prop min or prop max is NaN', () => {
-    function numberValue() {
+    function checkingValues() {
         const min = 2;
         const max = '2';
-        randomNumber (min, max);
+        randomNumber(min, max);
     }
-    expect(numberValue).toThrow();
+    expect(checkingValues).toThrow();
+})
+
+it ('throw error, when prop min is bigger than prop max', () => {
+    function checkingValues() {
+        const min = 5;
+        const max = 3;
+        randomNumber(min, max);
+    }
+    expect(checkingValues).toThrow();
 })
