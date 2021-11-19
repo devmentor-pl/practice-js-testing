7e2fa2cac284f2b9aec5a619b3e2ebe3f28066c2
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-11-19 14:07:12 +0100

    First red test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,9 @@
 import randomNumber from './app';
+it('return 0 while prop min is the same as prop max', () => {
+    function sameNumbers() {
+        const min = 1;
+        const max = 1;
+        randomNumber(min, max);
+    }
+    expect(sameNumbers).toBe(0);
+})
