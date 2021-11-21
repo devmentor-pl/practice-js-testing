c9e24a04ee0e7710f4acdaff50e8fc64fb72d1b9
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-11-21 21:29:07 +0100

    Fourth task

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,34 @@
 export default class User {
+    constructor( {email, password} ) {
+
+        this.email = email;
+        this.password = password;
+
+        if (!this.email.includes('@')) {
+            throw new Error ('Email must be correct');
+        }
+
+        if (this.password.length < 4) {
+            throw new Error ('Password must be longer than three signs');
+        }
+
+    }
+    
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        if(this.email.includes('devmentor.pl')) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
