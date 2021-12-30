abc95778995e259223b7aa4e8bd3de72d33f9dda
Rafał Kazik <kazik.raf@gmail.com>
2021-12-30 20:52:43 +0100

    3rd tast /2nd test

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,10 @@ it("return 1 when min and max are 1", () => {
   const result = randomNumber(1, 1);
   expect(result).toBe(1);
 });
+
+it("error if isNaN", () => {
+  function randomNumberNaN(min, max) {
+    min === NaN || max === NaN;
+  }
+  expect(randomNumber).toThrow();
+});
