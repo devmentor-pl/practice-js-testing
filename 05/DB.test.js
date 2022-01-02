40147e6e4e40041d12a9df0b4a933bec0ca63f86
Rafał Kazik <kazik.raf@gmail.com>
2022-01-02 19:19:30 +0100

    5th task

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,10 @@
-import DB from './DB';
+import DB from "./DB";
+
+describe("DB", () => {
+  it("ID should be a number", async () => {
+    const database = new DB();
+    const id = await database.insert(data.id);
+
+    expect(typeof id).toBe("number");
+  });
+});
