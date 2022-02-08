d7b666eaefabce51295a7526f8fe62915565eb72
Ewelina <ewelina@ekopacz.pl>
2022-02-08 21:45:13 +0100

    fixed another bug in the first test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,6 +1,5 @@
 import randomNumber from './app';
 
 it('if number min = 1 and number max = 1', () => {
-    const randomNo = new randomNumber();
-    expect(randomNo(1,1)).toBe(1);
-})
+    expect(randomNumber(1,1)).toBe(1);
+});
