889df62742f6c0c9d49fb5b455d2cdffe6b2efcb
Ewelina <ewelina@ekopacz.pl>
2022-02-08 21:58:18 +0100

    added second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('if number min = 1 and number max = 1', () => {
     expect(randomNumber(1,1)).toBe(1);
 });
+
+it('if value is not a number', () => {
+    expect(randomNumber).toThrow();
+});
