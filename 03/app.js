68b67a7f77c0cd5b559b44bbdea19195e65e524e
Ewelina <ewelina@ekopacz.pl>
2022-02-08 22:43:18 +0100

    no code needed, finish task

--- 03/app.js
+++ 03/app.js
@@ -14,7 +14,7 @@ export default function randomNumber(min, max) {
     if(min > max) {
         throw new Error('value min can be greater than value max')
     }
-    
+
     else{
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
