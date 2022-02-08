ff575f8af5d84f905b3f8734622d82fcd8fecd37
Ewelina <ewelina@ekopacz.pl>
2022-02-08 11:57:32 +0100

    add solution

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', init);
 
+const alertSection = document.querySelector('.alert');
+const alertContainer = document.querySelector('.alert__container');
+const messageSection = document.querySelector('.alert__message');
+
 function init() {
     const clickEl = document.querySelector('.error--click');
     const enterEl = document.querySelector('.error--enter');
@@ -12,17 +16,31 @@ function init() {
 
 }
 
-function setRandomPosition(element, error = null) {
+function setRandomPosition(element,error = null) {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
-
-    if(error) {
+    if(error){ // troche tego nie ogarniam, chciałam wyrzucic ten kod aby wyrzucanie bledy było tylko na dole w try ... catch, ale bez tego, to nie dziala 
         throw error;
     }
 }
 
 function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+    try{
+        setRandomPosition(this,error);
+    }
+    catch(e){
+        alertSection.classList.remove('alert--hidden');
+        messageSection.innerText = error;
+    }
+    });
 }
+
+alertSection.addEventListener('click', e => {
+    e.stopPropagation();
+    if(e.target === e.currentTarget){
+    alertSection.classList.add('alert--hidden');
+    }
+});
+
+
