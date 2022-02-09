aa2240051e172e7e5a3f5e0be702c51276f39743
Ewelina <ewelina@ekopacz.pl>
2022-02-09 10:00:48 +0100

    add code to the first test

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,18 @@
 export default class User {
+    constructor({email,password}){
+        this.email = email;
+        this.password = password;
+    }
 
+    getEmail(){
+        if(this.email){
+            return this.email;
+        }
+    }
+
+    getPassword(){
+        if(this.password){
+            return this.password;
+        }
+    }
 }
