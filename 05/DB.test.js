1360036b89e1eb8c346d90906ba0c2f605f447c5
Ewelina <ewelina@ekopacz.pl>
2022-02-09 20:06:41 +0100

    add the last test

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -6,9 +6,20 @@ describe('insert method', () => {
         try {
             await db.insert({id:'b'});
         }
-        catch (e){
-            expect(e).toBe('ID can be only number!');
+        catch (error){
+            expect(error).toBe('ID can be only number!');
         }
     });
+
+    it('if ID is already in a database', async () => {
+        expect.assertions(1);
+        const id1 = {id:1};
+        const id2 = {id:1};
+        const db = new DB(id1);
+
+        await db.insert(id1);
+        const promise = db.insert(id2);
+        return promise.catch(error => { expect(error).toBe('ID can\'t be duplicated!')});
+    })
 
 });
