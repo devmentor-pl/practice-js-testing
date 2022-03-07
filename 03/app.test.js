6d029060b7750e241be0e4b9009dc65ee7dae686
Magdalena Nolde <magdalena@nolde.de>
2022-03-07 17:28:13 +0100

    first test task 3

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,9 @@
 import randomNumber from './app';
+// 
+// jeśli losujesz liczbę z przedzialu od 1 do 1, to funkcja zwróci 1,
+
+it('return 1 when randomized numbers are in the range 1 and 1' , () => {
+
+    expect(randomNumber(1, 1)).toBe(1);
+
+});
