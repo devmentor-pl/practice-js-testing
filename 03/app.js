6b17cf8dce67af7e9fe6defc806bbfc596c8fadc
Magdalena Nolde <magdalena@nolde.de>
2022-03-07 18:57:38 +0100

    second test task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 export default function randomNumber(min, max) {
+    if((typeof min !== 'number') || (typeof max !== 'number')) {
+        throw new Error('Numbers must be passed as a argument');
+    }
     return Math.round(Math.random() * (max - min)) + min;
 } 
+
+
+
+
--- 03/app.test.js
+++ 03/app.test.js
@@ -7,3 +7,12 @@ it('return 1 when randomized numbers are in the range 1 and 1' , () => {
     expect(randomNumber(1, 1)).toBe(1);
 
 });
+
+/* jeśli podasz jako argument "nie liczbę", to zostanie rzucony błąd, */
+
+it('throws an error if one of the arguments is NaN', () => {
+    function checkNumber() {
+        randomNumber('NaN', 5);
+    }
+    expect(checkNumber).toThrow()
+}) 
