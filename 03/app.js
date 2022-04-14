7bdc583e7f5afa42be7233dbf86d46a27b5001bf
Max <maciejdobisz@gmail.com>
2022-04-14 13:56:49 +0100

    Test4 ok

--- 03/app.js
+++ 03/app.js
@@ -4,10 +4,15 @@ export default function randomNumber(min, max) {
     }
 
     if(typeof max !== 'number') {
-        throw new Error('"max argument is not a number');
+        throw new Error('argument "max" is not a number');
     }
 
-    if((min = 1) && (max = 1)) {
+    if(min === 1 && max === 1) {
         return 1;
     }
+
+    if(min > max) {
+        throw new Error('argument "min" greater than argument "max"');
+    }
+    
 }
