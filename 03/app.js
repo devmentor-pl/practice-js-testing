8e100e9a514f2da5ec85410273edbacb331f0e3b
Max <maciejdobisz@gmail.com>
2022-04-14 13:32:14 +0100

    test1

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,5 @@
 export default function randomNumber(min, max) {
- 
+    if(typeof min !== 'number') {
+        throw new Error('"min" argument is not a number');
+    }
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,2 @@
 import randomNumber from './app';
 
-it('throw error if "min" is not a number', () => {
-    expect( () => randomNumber('1', 2)).toThrow();
-});
