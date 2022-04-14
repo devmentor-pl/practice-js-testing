5fe528fe1ac443d18969e037a177f346917cff02
Max <maciejdobisz@gmail.com>
2022-04-14 13:36:27 +0100

    Test2

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('throw error if "min" is not a number', () => {
     expect( () => randomNumber('1', 2)).toThrow();
 });
+
+it('throw error if "max" is not a number', () => {
+    expect( () => randomNumber(2, '1')).toThrow();
+});
