08ffab77d84dc566bdb2f48c8fbb2936fb866d7a
Max <maciejdobisz@gmail.com>
2022-04-14 13:40:13 +0100

    Test3

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,9 +1,13 @@
 import randomNumber from './app';
 
 it('throw error if "min" is not a number', () => {
-    expect( () => randomNumber('1', 2)).toThrow();
+    expect( () => randomNumber('1', 2) ).toThrow();
 });
 
 it('throw error if "max" is not a number', () => {
-    expect( () => randomNumber(2, '1')).toThrow();
+    expect( () => randomNumber(2, '1') ).toThrow();
+});
+
+it('return 1 if both arguments = 1', () => {
+    expect( () => randomNumber(1,1) ).toBe(1);
 });
