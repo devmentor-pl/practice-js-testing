b9e2fd39a15193b2ed47473b32f5adad4b0a0c15
Jakub Parol <kubciop33@gmail.com>
2022-05-02 15:23:42 +0200

    test 1 - red

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,9 @@
 import randomNumber from './app';
+
+it('return 1 when the number to be drawn is between 1 and 1', () => {
+    const min = 1;
+    const max = 1;
+    const result = randomNumber(min, max);
+
+    expect(result).toBe(1);
+});
