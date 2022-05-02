ea74f0019c85e0d6fccca6acc3d4a8ce24cd83c3
Jakub Parol <kubciop33@gmail.com>
2022-05-02 16:48:12 +0200

    test 2 - green

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,7 @@
 export default function randomNumber(min, max) {
     if(min === 1 && max === 1) {
         return 1;
+    } else if(typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('Arguments have to be a number!');
     } else return Math.round(Math.random() * (max - min + 1) + min);
 }
