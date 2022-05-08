64335f8288e274d3c58543f467ddd4c9e3c69ecd
Anna <krolikowska.ann@gmail.com>
2022-05-08 18:19:56 +0200

    03 added test 1

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,6 @@
-import randomNumber from './app';
+import randomNumber from '../app';
+
+it('if you draw a number between 1 and 1, the function returns 1', () => {
+    expect(randomNumber(1,1)).toBe(1)
+    
+})
