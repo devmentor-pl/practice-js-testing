3cf0c59af1b213524764389c246b9b8e31aac659
Anna <krolikowska.ann@gmail.com>
2022-05-16 16:59:20 +0200

    03 added test 2

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,6 +1,16 @@
-import randomNumber from '../app';
+import randomNumber from './app';
 
 it('if you draw a number between 1 and 1, the function returns 1', () => {
     expect(randomNumber(1,1)).toBe(1)
+    
+})
+
+it('if you provide NaN as an argument, an error will be returned', () => {
+    function drawNumber() {
+        const min = NaN
+        const max = NaN
+        randomNumber(min,max)
+    }
+    expect(drawNumber).toThrow()
     
 })
