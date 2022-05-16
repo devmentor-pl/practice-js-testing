e14b0c41219f73716a19842a716b0a2820432371
Anna <krolikowska.ann@gmail.com>
2022-05-16 17:22:45 +0200

    03 added impl 3

--- 03/app.js
+++ 03/app.js
@@ -4,5 +4,9 @@ if(isNaN(min) || isNaN(max)) {
   throw new Error('The argument cannot be NaN')
 }
 
+if(min > max) {
+  throw new Error('The first argument cannot be greater than the second argument')
+}
+
   return  Math.floor(Math.random() * (max - min +1)) + min;
 }
