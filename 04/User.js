56c8d00cc1c85b42ade08bf8bab26eb27e06bffa
Anna <krolikowska.ann@gmail.com>
2022-05-16 21:30:15 +0200

    04 added implementation

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,31 @@
 export default class User {
 
+    constructor({email, password}) {
+        this.email = email;
+        this.password = password;
+
+        if(!this.email.includes('@')) {
+            throw new Error('Email need @ sign!')
+        }
+
+        if(this.password.length < 8) {
+            throw new Error('Password must be at least 8 characters long')
+        }
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getPassword() {
+        return this.password
+    }
+
+    login() {
+        if(this.email.includes('devmentor.pl')) {
+            return true
+        } else { 
+            return false
+        }
+    }   
 }
