21d7e8ca5f0d6d3b74e6333b84bd00f9208994d3
Anna <krolikowska.ann@gmail.com>
2022-05-16 17:00:17 +0200

    03 added impl 2

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,8 @@
 export default function randomNumber(min, max) {
+
+if(isNaN(min) || isNaN(max)) {
+  throw new Error('The argument cannot be NaN')
+}
+
   return  Math.floor(Math.random() * (max - min +1)) + min;
 }
