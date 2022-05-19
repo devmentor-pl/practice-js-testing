b0f464fb5395dc33c8fcf11cd5a9b74ef626e867
Anna <krolikowska.ann@gmail.com>
2022-05-19 22:04:50 +0200

    05 added tests for DB class

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,93 @@
 import DB from './DB';
+
+describe('DB', () => {
+    describe('insert()', () => {
+        it('Should stop function when ID is not a number', async () => {
+        expect.assertions(1)
+            try {
+                 const db = new DB()
+                await db.insert({name: 'Anna', city: 'Cracow', id: 'x'})
+            } catch(err)  {
+                 expect(err).toBe('ID can be only number!')
+            }
+         })
+
+        it('Should stop function when ID is duplicated', async () => {
+            expect.assertions(1)
+            try {
+                const db = new DB()
+                await db.insert({name: 'Anna', city: 'Cracow', id: 1})
+                await db.insert({name: 'Anna', city: 'Cracow', id: 1})
+            } catch(err)  {
+                expect(err).toBe('ID can\'t be duplicated!')
+            }
+        })  
+    })
+
+    describe('select()', () => {
+        it('Should return error when ID is not found', async () => {
+        expect.assertions(1)
+            try {
+                const db = new DB()
+                await db.select(1)
+
+            } catch(err)  {
+                expect(err).toBe('ID not found')
+            }
+        })  
+    })
+
+    
+    describe('remove()', () => {
+        it('Should return error when item not exist', async () => {
+        expect.assertions(1)
+            try {
+                const db = new DB()
+                await db.insert({name: 'Anna', city: 'Cracow', id:1})
+                await db.remove(2)
+
+            } catch(err)  {
+                expect(err).toBe('Item not exist!')
+            }
+        })  
+
+        it('Should removed item', async () => {
+            expect.assertions(1)
+                 try {
+                    const db = new DB()
+                    await db.insert({name: 'Anna', city: 'Cracow', id:1})
+                    await db.remove(1)
+                    await db.select(1)
+    
+                } catch(err)  {
+                    expect(err).toBe('ID not found')
+             }
+            })  
+    })
+
+    describe('update()', () => {
+        it('Should return error when item is not set', async () => {
+            expect.assertions(1)
+                try {
+                    const db = new DB()
+                    await db.insert({name: 'Anna', city: 'Cracow', id:1})
+                    await db.update({name: 'Anna', city: 'Warsaw'})
+    
+                } catch(err)  {
+                    expect(err).toBe('ID have to be set!')
+                }
+            })  
+
+            it('Should return error when ID is not found', async () => {
+                expect.assertions(1)
+                    try {
+                        const db = new DB()
+                        await db.update({name: 'Ala', city: 'Gdynia', id:2})
+        
+                    } catch(err)  {
+                        expect(err).toBe('ID not found!')
+                    }
+                })  
+        })
+})
+
