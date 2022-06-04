70ad63c30afc58f190300bdcb2a956f03e982ab7
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-06-04 18:17:32 +0200

    Finish 04

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,28 @@
 export default class User {
+    constructor(obj) {
+        const {email, password} = obj;
 
+        if(!email.includes('@')) {
+            throw new Error('email is incorrect');
+        }
+
+        if(password.length < 8) {
+            throw new Error('password is incorrect');
+        }
+
+        this.email = email;
+        this.password = password;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        return this.email.includes('devmentor.pl');
+    }
 }
