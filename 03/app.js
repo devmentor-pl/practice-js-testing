5f716a8b78785e82ef0e702ad783c8cb7524996e
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-06-04 18:00:46 +0200

    [red,gree] min not number

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,8 @@
 export default function randomNumber(min, max) {
+
+    if(typeof min !== 'number') {
+        throw new Error('"min" is not a number');
+    }
+
     return Math.random() * (max - min) + min;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -2,4 +2,8 @@ import randomNumber from './app';
 
 it('return 1 if min=1 and max=1', () => {
     expect( randomNumber(1, 1) ).toBe(1);
+});
+
+it('throw erorr if min is not a number', () => {
+    expect( () => randomNumber('1', 2)).toThrow('"min" is not a number');
 })
