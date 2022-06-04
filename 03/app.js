798fbfed2be25fcef5f0e34daca94e0f5b5c1684
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-06-04 18:07:56 +0200

    Finish 03

--- 03/app.js
+++ 03/app.js
@@ -1,8 +1,20 @@
 export default function randomNumber(min, max) {
 
-    if(typeof min !== 'number') {
-        throw new Error('"min" is not a number');
+    if(!isNumber(min)) {
+        throwError('"min" is not a number')
+    }
+
+    if(!isNumber(max)) {
+        throwError('"max" is not a number')
     }
 
     return Math.random() * (max - min) + min;
+}
+
+function isNumber(value) {
+    return typeof value === 'number' && !Number.isNaN(value);
+}
+
+function throwError(message) {
+    throw new Error(message);
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -7,3 +7,7 @@ it('return 1 if min=1 and max=1', () => {
 it('throw erorr if min is not a number', () => {
     expect( () => randomNumber('1', 2)).toThrow('"min" is not a number');
 })
+
+it('throw erorr if max is not a number', () => {
+    expect( () => randomNumber(1, '2')).toThrow('"max" is not a number');
+})
