3de6d44182a2328727c7ab0d40643b0930776fb0
Filip <43039909+Urfisior@users.noreply.github.com>
2022-06-08 12:12:35 +0100

    test 03 add test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,7 +1,7 @@
 import randomNumber from './app';
 
 describe('randomNumber()', () => {
-    it(`if no arguments, then expect error information`, () => {
-        expect(() => { randomNumber() }).toThrow()
+    it(`if no arguments, throw error information`, () => {
+        expect(() => { randomNumber() }).toThrow();
     });
 });
