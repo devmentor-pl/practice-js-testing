81c96d499f2e979b11fb9029b73f88a2b0f7cb83
Filip <43039909+Urfisior@users.noreply.github.com>
2022-06-08 12:24:27 +0100

    test 03 add test

--- 03/app.test.js
+++ 03/app.test.js
@@ -5,6 +5,9 @@ describe('randomNumber()', () => {
         expect(() => { randomNumber() }).toThrow();
     });
     it(`one agrument, throw error information`, () => {
-        expect(()=>{randomNumber(3)}).toThrow()
+        expect(() => { randomNumber(3) }).toThrow()
+    });
+    it(`ones of the arguments is not a number, throw error information`, () => {
+        expect(() => { randomNumber('d', 2) }).toThrow()
     })
 });
