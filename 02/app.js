49b57e18731f5f6655e58b90fb229d19d4e497da
Filip <43039909+Urfisior@users.noreply.github.com>
2022-06-08 13:36:28 +0100

    task 01 & 02 done

--- 02/app.js
+++ 02/app.js
@@ -13,16 +13,28 @@ function init() {
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
-
-    if(error) {
-        throw error;
-    }
-}
+    try {
+        element.style.top = Math.random() * 600 + 'px';
+        element.style.left = Math.random() * 800 + 'px';
+        if (error) throw error;
+    } catch (error) {
+        displayError(`${error.message}`);
+    };
+};
 
 function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
         setRandomPosition(this, error);
     })
 }
+
+function displayError(errorMsg) {
+    const errorContainer = document.querySelector('.alert--hidden');
+    errorContainer.classList.remove('alert--hidden');
+    errorContainer.children[0].firstElementChild.textContent = errorMsg;
+    errorContainer.addEventListener('click', (e) => {
+        if (e.target.classList.contains('alert')) {
+            e.target.classList.add('alert--hidden');
+        };
+    });
+};
