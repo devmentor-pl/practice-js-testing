789500844a6b6712a5e05bfc4ae597ff9d23b574
Filip <43039909+Urfisior@users.noreply.github.com>
2022-06-08 14:46:53 +0100

    task 04 done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,28 @@
 export default class User {
-
-}
+    constructor({email,password}) {
+        this.email = email;
+        this.password = password;
+        this.isEmailCorrect();
+        this.isPasswordCorrect();
+        this.login();
+    };
+    getEmail() {
+        return this.email;
+    };
+    getPassword() {
+        return this.password;
+    };
+    isEmailCorrect() {
+        if (!this.email.includes('@')) throw new Error('incorrect email');
+    };
+    isPasswordCorrect() {
+        if (this.password.length < 8) throw new Error('incorrect password should contains min. 8 characters');
+    };
+    login() {
+        let isLogin = false;
+        if (this.email.includes('devmentor.pl')) {
+            isLogin = true;
+        };
+        return isLogin;
+    };
+};
