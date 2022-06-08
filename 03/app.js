01e70f5c345b4675a9b6a57782405d0f4aba788d
Filip <43039909+Urfisior@users.noreply.github.com>
2022-06-08 12:33:45 +0100

    task 03 update randomNumber func.

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,8 @@
 export default function randomNumber(min, max) {
     if (typeof min === 'undefined' || typeof max === 'undefined') {
         throw new Error('This function should contains two number value [min],[max]');
-    }
-}
+    };
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('This function should contains only numbers');
+    };
+};
