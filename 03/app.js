fedb1490e93ad75378846a0c09ff6c17c765fb61
Filip <43039909+Urfisior@users.noreply.github.com>
2022-06-08 12:58:34 +0100

    task 03 update randomNumber func.

--- 03/app.js
+++ 03/app.js
@@ -5,4 +5,8 @@ export default function randomNumber(min, max) {
     if (typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('This function should contains only numbers');
     };
+    if (min > max) {
+        throw new Error(`First argument can not be greater then second one`);
+    }
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
