fc4c26274598a35d5d181b9cd2ddcf02c187be8c
Camila Montes Celinski <camicelinski@gmail.com>
2022-06-26 23:41:05 +0200

    task-03 test-2 green

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,7 @@
 export default function randomNumber(min, max) {
+    if(typeof min !== 'number' && !Number.isNaN(min)) {
+        throw new Error('"min" must be a number')
+    }
+
     return Math.random() * (max - min) + min
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -5,5 +5,5 @@ it('return 1 if min = 1 and max = 1', () => {
 })
 
 it('throw error if min is not a number', () => {
-    expect( () => randomNumber('a', 1) ).toThrow('"min" must be a number')
+    expect(() => randomNumber('a', 1)).toThrow('"min" must be a number')
 })
