1667f84469ffcd0336117a792ff1fca43b13391c
Camila Montes Celinski <camicelinski@gmail.com>
2022-06-26 23:26:34 +0200

    task-03 test-2 red

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('return 1 if min = 1 and max = 1', () => {
     expect(randomNumber(1, 1)).toBe(1)
 })
+
+it('throw error if min is not a number', () => {
+    expect( () => randomNumber('a', 1) ).toThrow('"min" must be a number')
+})
