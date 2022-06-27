efb17bde4592afa2b527bb0666603054e26d45f2
Camila Montes Celinski <camicelinski@gmail.com>
2022-06-27 14:46:32 +0200

    task-03 test-3 refactor

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,19 @@
 export default function randomNumber(min, max) {
-    if(typeof min !== 'number' && !Number.isNaN(min)) {
-        throw new Error('"min" must be a number')
+    if(!isNumber(min)) {
+        throwError('"min" must be a number')
     }
 
-    if(typeof max !== 'number' && !Number.isNaN(max)) {
-        throw new Error('"max" must be a number')
+    if(!isNumber(max)) {
+        throwError('"max" must be a number')
     }
 
     return Math.random() * (max - min) + min
+}
+
+function isNumber(value) {
+    return typeof value === 'number' && !Number.isNaN(value)
+}
+
+function throwError(message) {
+    throw new Error(message)
 }
