fc64f9af9a5d3e4feed7733cb2eb7e7d86393007
Camila Montes Celinski <camicelinski@gmail.com>
2022-06-27 14:29:43 +0200

    task-03 test-3 red

--- 03/app.test.js
+++ 03/app.test.js
@@ -7,3 +7,7 @@ it('return 1 if min = 1 and max = 1', () => {
 it('throw error if min is not a number', () => {
     expect(() => randomNumber('a', 1)).toThrow('"min" must be a number')
 })
+
+it('throw error if max is not a number', () => {
+    expect(() => randomNumber(1, 'b')).toThrow('"max" must be a number')
+})
