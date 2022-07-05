31ddf40793ae142e78971807650bcbedb6aa5efd
Piotr <piotr.wojcikievvicz@gmail.com>
2022-07-05 17:28:34 +0200

    second implementation

--- 03/app.js
+++ 03/app.js
@@ -1,8 +1,15 @@
 export default function randomNumber(min, max) {
     
+    if (!isNumber(min)) {
+        throw new Error('Min is not a number');
+    }
+
     if (min === max) {        
         return max;
     }    
-       
+    
+    function isNumber(value) { 
+        return (typeof value === 'number');
+    }
 }
 
