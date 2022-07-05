6426847e32bea6d5767f26f6290d18a9bc1a62d4
Piotr <piotr.wojcikievvicz@gmail.com>
2022-07-05 16:55:51 +0200

    add second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,13 @@
 import randomNumber from './app';
 
-it('when min = 1 and  max = 1 return 1', () => {
+it ('when min = 1 and  max = 1 return 1', () => {
     expect(randomNumber(1,1)).toBe(1);
+});
+
+it ('throw exception when min is not number' , () => {    
+    function drawNumber(min, max) {
+        randomNumber(min, max);
+    }
+
+    expect(drawNumber('1', 2)).toThrow();
 });
