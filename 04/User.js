1a08780d941676505466a2c5416451506c283c42
Piotr <piotr.wojcikievvicz@gmail.com>
2022-07-06 20:59:33 +0200

    task 04 completed

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,29 @@
 export default class User {
+    constructor({email, password}) {
+        this.email = email;
+        this.password = password;
 
+        if (!email.includes('@')) {
+            throw new Error('Email is incorrect');
+        }
+
+        if (password.length < 8) {
+            throw new Error('Password is to short');
+        }
+
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        const reg =  /([a-zA-Z]+)@devmentor.pl*$/        
+        if (reg.test(this.email)) return true;
+        return false;
+    }
 }
