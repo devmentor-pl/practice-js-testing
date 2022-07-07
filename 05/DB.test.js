47842a21ae748200182174684e12574516a6819d
Piotr <piotr.wojcikievvicz@gmail.com>
2022-07-07 22:15:56 +0200

    finished all task

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -73,5 +73,101 @@ describe('DB', () => {
         
     });
         
-    
+    describe('remove(id)', () => {
+        it('Should resolve when item existed and was removed', 
+            async () => {          
+                expect.assertions(1);
+                const db = new DB();
+                await db.insert({id : 1});
+                await db.insert({id : 2});                
+                               
+                return db.remove(2).then(result => {
+                    expect(result).toBe('Item was remove!');
+                });                
+        });
+
+        it('Should reject when item not existed', 
+            async () => {          
+                expect.assertions(1);
+                const db = new DB();
+                await db.insert({id : 1});
+                await db.insert({id : 2});                
+                               
+                return db.remove(4).catch(err => {
+                    expect(err).toBe('Item not exist!');
+                });                
+        });
+        
+    });
+
+    describe('update(data)', () => {
+        it('Should reject when new data has no id', 
+            async () => {          
+                expect.assertions(1);
+                const db = new DB();
+                await db.insert({id : 1});
+                await db.insert({id : 2});
+                const data = {a : 1, b : 2};
+                               
+                return db.update(data).catch(err => {
+                    expect(err).toBe('ID have to be set!');
+                });                
+        });
+
+        it('Should reject when id not found', 
+            async () => {          
+                expect.assertions(1);
+                const db = new DB();
+                await db.insert({id : 1});
+                await db.insert({id : 2});
+                const data = {id: 3, a : 'test'};
+                               
+                return db.update(data).catch(err => {
+                    expect(err).toBe('ID not found!');
+                });                
+        });
+        
+        it('Should resolve when data updated', 
+            async () => {          
+                expect.assertions(1);
+                const db = new DB();
+                await db.insert({id : 1});
+                await db.insert({id : 2});
+                const data = {id: 2, a : 'test'};
+                               
+                return db.update(data).then(result => {
+                    expect(result).toBe(data);
+                });                
+        });
+
+    });
+
+    describe('truncate', () => {
+        it('Should resolve when database was cleared', 
+            async () => {          
+                const db = new DB();
+                await db.insert({id : 1});                                
+                return db.truncate().then(result => {
+                    expect(result).toBe(true);
+                });                
+        });
+        
+    });
+
+    describe('getRows()', () => {
+        it('Should resolve when return database', 
+            async () => {          
+            const db = new DB();            
+            const rows = [{ id: 1 }, { id: 2 }];            
+
+            await db.insert(rows[0]);
+            await db.insert(rows[1]);
+                        
+            return db.getRows().then( result => {
+                expect(result).toEqual(rows);
+            })
+            
+        });
+        
+    });
 })
