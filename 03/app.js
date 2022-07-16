b22839447faeac1eea359f0bf04c3aa0ae412058
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-07-16 10:56:14 +0200

    Add the second test and implementation to exercise 03

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,7 @@
 export default function randomNumber(min, max) {
     const drawnNumber = Math.floor(Math.random() * max + min);
+    if(typeof (min && max) !== 'number'){
+        throw new Error('Min and max have to be a number');      
+    }
     return drawnNumber;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,10 @@ it('return 1 when min is 1 and max is 1', () => {
     const drawnNumber = randomNumber(1, 1);
     expect(drawnNumber).toBe(1);
 });
+
+it('should throw exception when insert not a number', () => {
+    function drawNumber() {
+        randomNumber('1', '2');
+    }
+    expect(drawNumber).toThrow();
+});
