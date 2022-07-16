23218e1dc1895d9d49f9ac99881a63c917e1c859
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-07-16 10:38:14 +0200

    Add the first test and implementation of randomNumber function to exercise 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,4 @@
 export default function randomNumber(min, max) {
-
+    const drawnNumber = Math.floor(Math.random() * max + min);
+    return drawnNumber;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,6 @@
 import randomNumber from './app';
+
+it('return 1 when min is 1 and max is 1', () => {
+    const drawnNumber = randomNumber(1, 1);
+    expect(drawnNumber).toBe(1);
+});
