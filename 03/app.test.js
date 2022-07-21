a8d700833494087f2a7d6b44ccace5f7aa3b3ad6
Mateusz Suplewski <matx3582@gmail.com>
2022-07-21 19:51:43 +0200

    task-3 red-2-fix

--- 03/app.test.js
+++ 03/app.test.js
@@ -5,5 +5,9 @@ it('return 1 if min and max are set to 1', () => {
     expect(number).toBe(1)
 })
 it('throw exception when argument is not a number', () => {
-    expect(randomNumber(1, 'abc')).toThrow()
+    function dwarNumber() {
+        randomNumber('abc', 2)
+    }
+
+    expect(dwarNumber).toThrow()
 })
