2e2f04d8033cef9210042c42a8d3f28d7bff7c62
Mateusz Suplewski <matx3582@gmail.com>
2022-07-21 20:13:58 +0200

    Task-4 Done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,25 @@
 export default class User {
+    constructor({ email, password }) {
+        if (!email.includes('@')) {
+            throw new Error('Email is incorrect!')
+        }
+        if (password.length < 6 && [a - z] / i.test(password)) {
+            throw new Error('Password is not strong enough!')
+        }
+        this.email = email
+        this.password = password
+    }
 
+    getEmail() {
+        return this.email
+    }
+
+    getPassword() {
+        return this.password
+    }
+
+    login() {
+        if (this.email.includes('@devmentor.pl')) return true
+        return false
+    }
 }
