f6822a88c84823546843cbb9a8480036587b7ab6
Mateusz Suplewski <matx3582@gmail.com>
2022-07-21 19:52:07 +0200

    task-3 green-2

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,10 @@
 export default function randomNumber(min, max) {
+    if (typeof min !== 'number' && !Number.isNaN(min)) {
+        throw new Error('minimal value is not a number!')
+    }
+    if (typeof max !== 'number' && !Number.isNaN(max)) {
+        throw new Error('maximal value is not a number!')
+    }
     const number = Math.floor(Math.random() * (max - min) + min)
     return number
 
