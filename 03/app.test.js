c0f33df7b0508bd183b83bd2aed147be1e4080c9
Mateusz Suplewski <matx3582@gmail.com>
2022-07-21 19:38:09 +0200

    Task-3 red-2

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,6 @@ it('return 1 if min and max are set to 1', () => {
     const number = randomNumber(1, 1)
     expect(number).toBe(1)
 })
+it('throw exception when argument is not a number', () => {
+    expect(randomNumber(1, 'abc')).toThrow()
+})
