c23759cb1390b956591ba3c4cec94d53a78ceba1
Mateusz Suplewski <matx3582@gmail.com>
2022-07-22 19:18:48 +0200

    Task-5 Done

--- 05/DB.js
+++ 05/DB.js
@@ -5,18 +5,18 @@ export default class DB {
 
     insert(data) {
         return new Promise((resolve, reject) => {
-            if(data.id) {
-                if(typeof data.id !== 'number') {
-                    this.async(reject,'ID can be only number!');
+            if (data.id) {
+                if (typeof data.id !== 'number') {
+                    this.async(reject, 'ID can be only number!');
                     return null; // stop function
-                } else if(this._rows.some(item => item.id === data.id)) {
+                } else if (this._rows.some(item => item.id === data.id)) {
                     this.async(reject, 'ID can\'t be duplicated!');
                     return null; // stop function
                 }
             }
 
             this.async(() => {
-                if(!data.id) {
+                if (!data.id) {
                     data.id = this._rows.reduce((acc, item) => {
                         return acc <= item.id ? item.id + 1 : acc;
                     }, 1);
@@ -24,7 +24,7 @@ export default class DB {
 
                 this._rows.push(data);
                 resolve(data)
-            }); 
+            });
         });
     }
 
@@ -32,7 +32,7 @@ export default class DB {
         return new Promise((resolve, reject) => {
             this.async(() => {
                 const [row = null] = this._rows.filter(item => item.id === id);
-                if(row) {
+                if (row) {
                     resolve(row);
                 } else {
                     reject('ID not found');
@@ -47,11 +47,11 @@ export default class DB {
                 const lengthBeforeFilter = this._rows.length;
                 this._rows = this._rows.filter(item => item.id !== id);
                 const lengthAfterFilter = this._rows.length;
-                
-                if(lengthBeforeFilter === lengthAfterFilter) {
+
+                if (lengthBeforeFilter === lengthAfterFilter) {
                     reject('Item not exist!');
                 } else {
-                    resolve('Item was remove!');
+                    resolve('Item was removed!');
                 }
             });
         });
@@ -59,24 +59,24 @@ export default class DB {
 
     update(data) {
         return new Promise((resolve, reject) => {
-            if(!data.id) {
+            if (!data.id) {
                 this.async(reject, 'ID have to be set!');
             } else {
                 this.async(() => {
                     let updated = null;
                     this._rows = this._rows.map(item => {
-                        if(item.id === data.id) {
+                        if (item.id === data.id) {
                             updated = data
                             return updated;
                         }
-            
+
                         return item;
                     });
 
-                    if(updated) {
+                    if (updated) {
                         resolve(updated);
                     } else {
-                        reject('ID not found!');   
+                        reject('ID not found!');
                     }
                 });
             }
@@ -84,19 +84,24 @@ export default class DB {
     }
 
     truncate() {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             this.async(() => {
-                this._rows = [];
-                resolve(true);
+                if (this._rows.length > 0) {
+                    this._rows = [];
+                    resolve('Database is now clear')
+                }
+                reject('Database is already empty!')
             });
-            
         })
     }
 
     getRows() {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             this.async(() => {
-                resolve(this._rows);
+                if (this._rows.length > 0) {
+                    resolve(this._rows)
+                }
+                reject('No rows in the database!')
             });
         })
     }
--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,202 @@
 import DB from './DB';
+
+describe('insert', () => {
+    it('reject when ID is not a number', () => {
+        const db = new DB()
+        const data = { id: 'abc' }
+
+        const promise = db.insert(data)
+
+        return promise.catch(result => {
+            expect(result).toBe('ID can be only number!')
+        })
+    })
+    it('reject when ID already exists', async () => {
+        const db = new DB()
+        const data = { id: 2 }
+        const data2 = { id: 2 }
+        try {
+            await db.insert(data)
+            await db.insert(data2)
+        } catch (error) {
+            expect(error).toBe('ID can\'t be duplicated!')
+        }
+    })
+    it('insert row with id 4 when object id is set to 4', async () => {
+        const db = new DB()
+        const data = { id: 4 }
+
+        const result = await db.insert(data)
+
+        expect(result).toEqual(data)
+
+    })
+
+    it('set row id to 3 when inserted id set to 1 and 2 ', async () => {
+        const db = new DB()
+        const data = { id: 1 }
+        const data2 = { id: 2 }
+        const data3 = {}
+
+        await db.insert(data)
+        await db.insert(data2)
+        const result = await db.insert(data3)
+
+        expect(result.id).toBe(3)
+
+    })
+
+})
+
+describe('select', () => {
+    it('reject when row with id = 3 is not set', async () => {
+        const db = new DB()
+        const data = { id: 1 }
+        const data2 = { id: 2 }
+        try {
+            await db.insert(data)
+            await db.insert(data2)
+            await db.select(3)
+
+        } catch (error) {
+            expect(error).toBe('ID not found')
+        }
+    })
+    it('select row with id=15 when row with id set to 15 exist', async () => {
+        const db = new DB()
+        const data = { id: 1 }
+        const data2 = { id: 15 }
+
+        await db.insert(data)
+        await db.insert(data2)
+        const result = await db.select(15)
+
+        expect(result).toEqual(data2)
+    })
+})
+
+describe('remove', () => {
+    it('reject when row with id = 5 is not set', async () => {
+        const db = new DB()
+        const data = { id: 5 }
+        const data2 = { id: 4 }
+        try {
+            await db.insert(data)
+            await db.insert(data2)
+            await db.remove(3)
+
+        } catch (error) {
+            expect(error).toBe('Item not exist!')
+        }
+    })
+    it('remove row with id=13 when row with id set to 13 exist', async () => {
+        const db = new DB()
+        const data = { id: 13 }
+        const data2 = { id: 2 }
+
+        await db.insert(data)
+        await db.insert(data2)
+        const result = await db.remove(13)
+
+        expect(result).toBe('Item was removed!')
+    })
+})
+
+describe('update', () => {
+    it('reject when not passing id', async () => {
+        const db = new DB()
+        const data = { id: 7 }
+        const data2 = { id: 8 }
+        try {
+            await db.insert(data)
+            await db.insert(data2)
+            await db.update({})
+
+        } catch (error) {
+            expect(error).toBe('ID have to be set!')
+        }
+    })
+
+    it('update row with id=12 when row with id set to 13 exist', async () => {
+        const db = new DB()
+        const data = { id: 12 }
+        const data2 = { id: 2 }
+        const updatedData = { id: 12, name: 'Mateusz' }
+
+        await db.insert(data)
+        await db.insert(data2)
+        const result = await db.update(updatedData)
+
+        expect(result).toEqual(updatedData)
+    })
+
+    it('reject when row with id = 66 is not set', async () => {
+        const db = new DB()
+        const data = { id: 65 }
+        const data2 = { id: 67 }
+        const fakeData = { id: 66 }
+        try {
+            await db.insert(data)
+            await db.insert(data2)
+            await db.update(fakeData)
+
+        } catch (error) {
+            expect(error).toBe('ID not found!')
+        }
+    })
+
+})
+
+describe('truncate', () => {
+    it('clear database rows', async () => {
+        const db = new DB()
+        const data = { id: 11 }
+        const data2 = { id: 12 }
+
+        await db.insert(data)
+        await db.insert(data2)
+        const result = await db.truncate()
+
+        expect(result).toBe('Database is now clear')
+    })
+
+
+    it('reject when database is empty', async () => {
+        const db = new DB()
+        try {
+            await db.truncate()
+
+        } catch (error) {
+            expect(error).toBe('Database is already empty!')
+        }
+    })
+
+
+
+})
+
+describe('get rows', () => {
+    it('get rows when not empty', async () => {
+        const db = new DB()
+        const data = { id: 15, name: 'Mateusz' }
+        const data2 = { id: 17, name: 'Ania' }
+        const dataArray = [{ id: 15, name: 'Mateusz' }, { id: 17, name: 'Ania' }]
+
+        await db.insert(data)
+        await db.insert(data2)
+        const result = await db.getRows()
+
+        expect(result).toEqual(dataArray)
+    })
+
+    it('reject when rows empty', async () => {
+        const db = new DB()
+        try {
+            await db.getRows()
+
+        } catch (error) {
+            expect(error).toBe('No rows in the database!')
+        }
+    })
+
+})
