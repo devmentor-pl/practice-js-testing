be05cb30f59ddeae5d9568a85ce9ddcad3f96cb6
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-07-24 12:41:13 +0200

    Add new tests and correct some tests in exercise 05

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -2,62 +2,84 @@ import DB from './DB';
 
 describe('.insert', () => {
     it('Should check if new row contain added data', ()=> {
-        const db = new DB()     
-        db.insert({a: 1, b: 2})
-            .then (() => expect(result).toStrictEqual({"a": 1, "b": 2, "id": 1}));
+        expect.assertions(1);
+        const db = new DB();  
+        return db.insert({a: 1, b: 2})
+            .then(result => expect(result).toStrictEqual({"a": 1, "b": 2, "id": 1}));
     });
 
     it('Should check if new row was added', () => {
-        //expect.assertions(1);
-        //w tym teście, jeżeli użyję expect.assertions(1) dostaję informację: Expected one assertion to be called but received zero assertion calls.
-        //jeżeli test jest bez tej linii to przechodzi z kolei zawsze (niezależnie od tego jaką wartość wpiszę w .toBe())
+        expect.assertions(1);
         const db = new DB();
-        db.insert({a: 1, b: 2})
+        return db.insert({a: 1, b: 2})
             .then (() => db.insert({a: 1, b: 2}))
             .then(() => db.getRows())
-            .then ((rows) => expect(rows.length).toBe(7));
+            .then ((rows) => expect(rows.length).toBe(2));
     });
 
     it('Should reject when id is not a number', ()=> {
         expect.assertions(1);
         const db = new DB()
-            db.insert({a: 1, b: 2, id: "a11a"})            
-            .catch(err => {expect(err.message).toBe('ID can be only number!');
+        return db.insert({a: 1, b: 2, id: "a11a"})      
+            .catch(err => {expect(err).toBe('ID can be only number!');
         });          
     });
 
     it('Should reject when id is duplicated', ()=> {
+        expect.assertions(1);
         const db = new DB();
-            db.insert({a: 1, b: 2})
+        return db.insert({a: 1, b: 2})
+            .then (() => db.insert({a: 2, b: 3, id: 1}))
             .catch(err => {
-            expect(err.message).toBe('ID can\'t be duplicated!');
+            expect(err).toBe('ID can\'t be duplicated!');
         });          
     });
 });
 
-/*describe('.select', () => {
-
-});*/
+describe('.select', () => {
+    it('Should check if the appropriate row was selected', () => {
+        expect.assertions(1);
+        const db = new DB();
+        return db.insert({a: 1, b: 2})  
+            .then (() => db.insert({a: 3, b: 4}))
+            .then(() => db.select(2))
+            .then(() => db.getRows())
+            .then(row => expect(row[1].id).toBe(2))
+    });
+});
 
 
 describe('.remove', () => {
-    it('Row should be removed when it contains given id', ()=> {
-        //expect.assertions(1);
+    it('Should check if the row with chosed id was removed', ()=> {
+        expect.assertions(1);
         const db = new DB();
-        db.insert({a: 1, b: 2})
+        return db.insert({a: 1, b: 2})
             .then(() => db.insert({a: 3, b: 4}))
-            .then(() => db.remove(12))
-            .then(() => expect(result).toBe('Item was remove!'));
+            .then(() => db.remove(2))
+            .then(result => expect(result).toBe('Item was remove!'))
         });         
 });
 
 
 describe('.update', () => {
-    it('Should change id when the id is updated', ()=> {
+    it('Should change data in the row selected by id', ()=> {
+        expect.assertions(1);
         const db = new DB();
-        db.insert({a: 1, b: 2})
-            .then(() => db.update({id: 3}))
-            .then(()=> expect(result).toContain('id = 7'));
+        return db.insert({a: 1, b: 2})
+            .then(() => db.update({a:5, b:7, id: 1}))
+            .then(() => db.getRows())
+            .then(row => expect(row[0].a).toBe(5))
+        });
+});
+
+describe('.update', () => {
+    it('2Should change data in the row selected by id', async ()=> {
+        expect.assertions(1);
+        const db = new DB();
+        await db.insert({a: 1, b: 2})
+        const result = await db.update({a: 3, b: 8, id: 1});
+        const rows = await db.getRows()
+        expect(rows[0].a).toBe(3);
         });
 });
 
@@ -66,16 +88,23 @@ describe('.getRows', () => {
     it('Should chceck if the amount of rows is equal to added', () => {
         expect.assertions(1);
         const db = new DB();
-        db.insert({a: 1, b: 2})
+        return db.insert({a: 1, b: 2})
             .then(() => db.insert({a: 3, b: 4}))
             .then(() => db.getRows())
-            .then(rows => {expect(rows.length).toBe(7);
+            .then(rows => {expect(rows.length).toBe(2);
         });
     });
 });
 
-
-
-
-
-
+describe('.truncate', () => {
+    it('Should chceck if all the data in the DB was removed', () => {
+        expect.assertions(1);
+        const db = new DB();
+        return db.insert({a: 1, b: 2})
+            .then(() => db.insert({a: 3, b: 4}))
+            .then(() => db.truncate())
+            .then(() => db.getRows())
+            .then(rows => {expect(rows.length).toBe(0);
+        })
+    })
+})
---
+++ 05/check.js
@@ -0,0 +1,131 @@
+
+class DB {
+    constructor() {
+        this._rows = [];
+    }
+
+    insert(data) {
+        return new Promise((resolve, reject) => {
+            if(data.id) {
+                if((typeof data.id) !== 'number') {
+                    this.async(reject,'ID can be only number!');
+                    //this.async(reject(new Error('ID can be only number!')));
+                    return null; // stop function
+                } else if(this._rows.some(item => item.id === data.id)) {
+                    this.async(reject, 'ID can\'t be duplicated!');
+                    return null; // stop function
+                }
+            }
+
+            this.async(() => {
+                if(!data.id) {
+                    data.id = this._rows.reduce((acc, item) => {
+                        return acc <= item.id ? item.id + 1 : acc;
+                    }, 1);
+                }
+
+                this._rows.push(data);
+                resolve(data);
+                //resolve(this._rows.push(data));
+            }); 
+        });
+    }
+
+    select(id) {
+        return new Promise((resolve, reject) => {
+            this.async(() => {
+                const [row = null] = this._rows.filter(item => item.id === id);
+                if(row) {
+                    resolve(row);
+                } else {
+                    reject('ID not found');
+                }
+            });
+        });
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this.async(() => {
+                const lengthBeforeFilter = this._rows.length;
+                this._rows = this._rows.filter(item => item.id !== id);
+                const lengthAfterFilter = this._rows.length;
+                
+                if(lengthBeforeFilter === lengthAfterFilter) {
+                    reject('Item not exist!');
+                } else {
+                    resolve('Item was remove!');
+                }
+            });
+        });
+    }
+
+    update(data) {
+        return new Promise((resolve, reject) => {
+            if(!data.id) {
+                this.async(reject, 'ID have to be set!');
+            } else {
+                this.async(() => {
+                    let updated = null;
+                    this._rows = this._rows.map(item => {
+                        if(item.id === data.id) {
+                            updated = data
+                            return updated;
+                        }
+            
+                        return item;
+                    });
+
+                    if(updated) {
+                        resolve(updated);
+                    } else {
+                        reject('ID not found!');   
+                    }
+                });
+            }
+        });
+    }
+
+    truncate() {
+        return new Promise(resolve => {
+            this.async(() => {
+                this._rows = [];
+                resolve(true);
+            });
+            
+        })
+    }
+
+    getRows() {
+        return new Promise(resolve => {
+            this.async(() => {
+                resolve(this._rows);
+            });
+        })
+    }
+
+    async(callback, ...params) {
+        setTimeout(() => {
+            callback(...params);
+        }, Math.random() * 1000);
+    }
+}
+
+const db = new DB();
+db.insert({a: 1, b: 2})
+    .then(() => db.insert({a: 3, b: 4}))
+    //.then(() => db.remove(2))
+    //.then(() => db.update({a: 3, b:4, id: 1}))
+    //.then(result => console.log(result))
+    //.then(() => db.getRows())
+    //.then(rows => console.log(rows))
+    //.then(() => db.insert({a: 1, b: 2, id: 2}))    
+    //.then(() => db.update({a: 3, b: 4, id: 10}))
+    //.then(result => console.log(result))
+    .then(() => db.getRows())
+    .then(rows => console.log(rows))   
+    .then(() => db.truncate())
+    .then(() => db.getRows())
+    .then(rows => console.log(rows.length))      
+    
+    
