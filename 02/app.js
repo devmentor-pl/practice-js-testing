e54672434691eacc8cd443a9b07fc8d9e9809ef8
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-08-14 17:24:02 +0200

    finish 02

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,44 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+	const clickEl = document.querySelector('.error--click');
+	const enterEl = document.querySelector('.error--enter');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+	setRandomPosition(clickEl);
+	setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+	initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+	initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+	element.style.top = Math.random() * 600 + 'px';
+	element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
-    }
+	if (error) {
+		throw error;
+	}
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+	element.addEventListener(eventName, function () {
+		try {
+			setRandomPosition(this, error);
+		} catch (e) {
+			showError(e);
+		} finally {
+			const alertEl = document.querySelector('.alert');
+			alertEl.addEventListener('click', closeAlert);
+		}
+	});
+}
+
+function showError(e) {
+	const alertElText = document.querySelector('.alert__message');
+	alertElText.innerText = e;
+	alertElText.parentElement.parentElement.classList.remove('alert--hidden');
+}
+
+function closeAlert() {
+	this.classList.add('alert--hidden');
 }
