22955cfd54b15d9241e8d9e60e1426bd775e7e6a
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-15 19:35:17 +0200

    Task 03 - Test nr 2 passed

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,6 @@
 export default function randomNumber(min, max) {
+	if (typeof min !== 'number' || typeof max !== 'number') {
+		throw new Error('Min or max is not a number')
+	}
 	return Math.floor(Math.random() * (max - min) + min);
 }
