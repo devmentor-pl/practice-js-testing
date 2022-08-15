c26820ea6efbf37d9f9754b726b5beb769c3d87d
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-08-15 13:08:51 +0200

    second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,7 @@ it('return 1 when min and max are 1', () => {
 	const result = randomNumber(1, 1);
 	expect(result).toBe(1);
 });
+
+it('throws error if number is Nan', () => {
+	expect(randomNumber('1', 1).toThrow());
+});
