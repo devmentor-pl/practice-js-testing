2cc126bb111a9eb43b8bf8ebeb538da5422caa1e
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-08-15 13:17:22 +0200

    second implementation

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,7 @@
 export default function randomNumber(min, max) {
+    if(typeof min !== 'number') {
+        throw new Error('Arg must be a number!')
+    }
     return Math.random() * (max - min) + min;
 }
 
-randomNumber(1, 1);
