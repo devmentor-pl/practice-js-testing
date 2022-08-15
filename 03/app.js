e5ac8a94d3f64cc91b96bde8211b54d88a03baad
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-15 19:51:57 +0200

    Task 03 - Test nr 3 refactoring

--- 03/app.js
+++ 03/app.js
@@ -1,12 +1,15 @@
 export default function randomNumber(min, max) {
 	checkIfIsANumber(min, max);
-	if (min > max) {
-		throw new Error("Min is greater than max");
-	}
+	checkLowerAndUpperLimit(min, max);
 	return Math.floor(Math.random() * (max - min) + min);
 }
 function checkIfIsANumber(min, max) {
 	if (typeof min !== "number" || typeof max !== "number") {
 		throw new Error("Min or max is not a number");
+	}
+}
+function checkLowerAndUpperLimit(min, max) {
+	if (min > max) {
+		throw new Error("Min is greater than max");
 	}
 }
