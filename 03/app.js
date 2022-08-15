0efa8a3f940796a96dfddfc724477cf11b8c8c9a
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-08-15 13:22:40 +0200

    3rd implementation

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,5 @@
 export default function randomNumber(min, max) {
-    if(typeof min !== 'number') {
+    if(typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('Arg must be a number!')
     }
     return Math.random() * (max - min) + min;
