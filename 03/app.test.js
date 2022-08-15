3c0c77f86c07a0ac9675ec4c24bbb756eac07cc3
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-15 19:28:30 +0200

    Task 03 - Test nr 2

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ it("return 1 when min is 1 and max is 1", () => {
 	const number = randomNumber(1, 1);
 	expect(number).toBe(1);
 });
+it('return error when min or max is not a number', ()=>{
+    const number = randomNumber('1', '1')
+    expect(number).toThrow()
+})
