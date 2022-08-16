175abeb79f45be5e2ecb9905608d1965b63b8c8e
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-16 13:00:12 +0200

    Task 05 not finished

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,21 @@
-import DB from './DB';
+import DB from "./DB";
+describe("DB", () => {
+	describe("Method insert()", () => {
+		it("return error when is not a number", async () => {
+			const db = new DB();
+			const data = { id: "A" };
+			const result = db.insert(data);
+			await result.catch((err) =>
+				expect(err).toBe("ID can be only number!")
+			);
+		});
+		it("return error when ID's are duplicated", async () => {
+			const db = new DB();
+			const data = { id: 1, id: 1 };
+			const result = db.insert(data);
+			await result.catch((err) => {
+				expect(err).toBe("ID can't be duplicated!");
+			});
+		});
+	});
+});
