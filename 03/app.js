87cd8f096cf35c28a565c5f2b501f4184ea70bf6
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-17 13:56:45 +0200

    Finished task 05

--- 03/app.js
+++ 03/app.js
@@ -4,8 +4,11 @@ export default function randomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 function checkIfIsANumber(min, max) {
-	if (typeof min !== "number" || typeof max !== "number") {
-		throw new Error("Min or max is not a number");
+	if (typeof min !== "number") {
+		throw new Error("Min limit is not a number");
+	}
+	if (typeof max !== "number") {
+		throw new Error("Max limit is not a number");
 	}
 }
 function checkLowerAndUpperLimit(min, max) {
