9dd2578b9b6f3c365f18dfdc4b3d56d94e2f3451
Ada <adriannazalewska1@gmail.com>
2022-08-27 21:33:40 +0200

    01, 02 done

--- 02/app.js
+++ 02/app.js
@@ -9,16 +9,25 @@ function init() {
 
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
 }
 
 function setRandomPosition(element, error = null) {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
 
+    try {
     if(error) {
         throw error;
     }
+    } catch (e) {
+        if (e instanceof TypeError) {
+        document.querySelector('.alert').classList.remove('alert--hidden')
+        document.querySelector('.alert__message').innerText = 'Błąd typu!'
+    } 
+      else if (e instanceof RangeError) {
+        document.querySelector('.alert').classList.remove('alert--hidden')
+        document.querySelector('.alert__message').innerText = 'Błąd zakresu!'
+    }} 
 }
 
 function initEventWithError(element, eventName, error) {
