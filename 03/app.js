839b05f8f2a89bb04ec02f33555405da592c4585
Ada <adriannazalewska1@gmail.com>
2022-08-28 19:57:54 +0200

    50% green

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 export default function randomNumber(min, max) {
+    if(typeof min === 'undefined' || typeof max === 'undefined') {
+        throw new Error('Property have to be a number')
+    }
 
+    if(max > min) {
+        throw new Error('Max cannot be bigger than min')
+    }
+    
+    return Math.round(Math.random() * (max-min) + min )
 }
