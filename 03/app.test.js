413ef97784d59de1c23dd11caa5ac87fc9755f08
Ada <adriannazalewska1@gmail.com>
2022-08-28 19:56:45 +0200

    red

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,17 @@
 import randomNumber from './app';
+
+it('throw error if any of parameters in not a number', ()=>{
+    function searchForRanomNumber() {
+        new randomNumber(undefined, 10)
+    }
+
+    expect(searchForRanomNumber).toThrow();
+});
+
+it('throw error if min is bigger than max', () => {
+    function checkMinMax() {
+        new randomNumber(5, 2)
+    }
+
+    expect(checkMinMax).toThrow();
+});
