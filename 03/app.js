e29bfe4a258d4b3f5441fc92f2e2abbfcbc616c7
Ada <adriannazalewska1@gmail.com>
2022-08-30 20:24:05 +0200

    03 done

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,10 @@
 export default function randomNumber(min, max) {
-    if(typeof min === 'undefined' || typeof max === 'undefined') {
-        throw new Error('Property have to be a number')
+    if(typeof min === 'undefined') {
+        throw new Error('Min have to be a number')
+    }
+
+    if(typeof max === 'undefined') {
+        throw new Error('Max have to be a number')
     }
 
     if(max > min) {
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,7 +1,11 @@
 import randomNumber from './app';
 
-it('throw error if any of parameters in not a number', ()=>{
+it('throw error if min in not a number', ()=>{
     expect( () => randomNumber(undefined, 10).toThrow('Property have to be a number'))
+});
+
+it('throw error if max in not a number', ()=>{
+    expect( () => randomNumber(10, undefined).toThrow('Property have to be a number'))
 });
 
 it('throw error if min is bigger than max', () => {
