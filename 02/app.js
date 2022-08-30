4a588dafce8200d1a26ad21edc21d5230190b5d5
Ada <adriannazalewska1@gmail.com>
2022-08-30 20:46:54 +0200

    02 done

--- 02/app.js
+++ 02/app.js
@@ -15,23 +15,23 @@ function setRandomPosition(element, error = null) {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
 
-    try {
     if(error) {
         throw error;
     }
-    } catch (e) {
-        if (e instanceof TypeError) {
-        document.querySelector('.alert').classList.remove('alert--hidden')
-        document.querySelector('.alert__message').innerText = 'Błąd typu!'
-    } 
-      else if (e instanceof RangeError) {
-        document.querySelector('.alert').classList.remove('alert--hidden')
-        document.querySelector('.alert__message').innerText = 'Błąd zakresu!'
-    }} 
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
+    element.addEventListener(eventName, function() { 
+        try {
         setRandomPosition(this, error);
+        } catch (e) {
+            if (e instanceof TypeError) {
+            document.querySelector('.alert').classList.remove('alert--hidden')
+            document.querySelector('.alert__message').innerText = 'Błąd typu!'
+        } 
+          else if (e instanceof RangeError) {
+            document.querySelector('.alert').classList.remove('alert--hidden')
+            document.querySelector('.alert__message').innerText = 'Błąd zakresu!'
+        }} 
     })
 }
