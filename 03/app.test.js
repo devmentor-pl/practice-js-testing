2083a9dbac5bc739f4b9043ef85172697544dc7e
Marta Stelmach <marta.ms1961@gmail.com>
2022-09-01 18:29:11 +0200

    Second red test added

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,9 @@
 import randomNumber from './app';
 
 it('should return 1 if min=1 & max=1', () => {
-    expect(randomNumber(1, 1)).toBe(1)
-})
+    expect(randomNumber(1, 1)).toBe(1);
+});
+
+it('should throw error if min is not a number', () => {
+    expect(() => randomNumber('1', 2)).toThrow('first argument is not a number');
+});
