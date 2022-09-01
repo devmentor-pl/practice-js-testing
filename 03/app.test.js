073f1d22550edbd8a0343357a54230c658d22789
Marta Stelmach <marta.ms1961@gmail.com>
2022-09-01 18:35:04 +0200

    Third red test added

--- 03/app.test.js
+++ 03/app.test.js
@@ -7,3 +7,7 @@ it('should return 1 if min=1 & max=1', () => {
 it('should throw error if min is not a number', () => {
     expect(() => randomNumber('1', 2)).toThrow('first argument is not a number');
 });
+
+it('should throw error if max is not a number', () => {
+    expect(() => randomNumber(1, '2')).toThrow('second argument is not a number');
+});
