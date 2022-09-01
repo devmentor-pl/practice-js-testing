4d67cbc81eae87336fa98fd8c12fe83df3412e55
Marta Stelmach <marta.ms1961@gmail.com>
2022-09-01 18:42:05 +0200

    Second & third test refactoring

--- 03/app.js
+++ 03/app.js
@@ -1,10 +1,14 @@
 export default function randomNumber(min, max) {
-    if (typeof min !== 'number') {
+    if (!isANumber(min)) {
         throw new Error('first argument is not a number');
     };
-    if (typeof max !== 'number') {
+    if (!isANumber(max)) {
         throw new Error('second argument is not a number');
     };
 
     return Math.random() * (max - min) + min;
+};
+
+function isANumber(value) {
+    return typeof value === 'number' && !Number.isNaN(value)
 };
