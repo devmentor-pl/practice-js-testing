c3bd4eebd9b2451ddeea45d1c0ac2d953b055491
Marta Stelmach <marta.ms1961@gmail.com>
2022-09-01 18:48:31 +0200

    Fourth red test added

--- 03/app.test.js
+++ 03/app.test.js
@@ -11,3 +11,7 @@ it('should throw error if min is not a number', () => {
 it('should throw error if max is not a number', () => {
     expect(() => randomNumber(1, '2')).toThrow('second argument is not a number');
 });
+
+it('should throw error if first argument is grater than second argument', () => {
+    expect(() => randomNumber(4, 3)).toThrow('first argument cannot be grater than second argument');
+});
