34e080d2a5902ed8202f59281432bc6c45da87fd
Marta Stelmach <marta.ms1961@gmail.com>
2022-09-01 19:09:12 +0200

    Finish task-4

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,27 @@
 export default class User {
+    constructor(user) {
+        const { email, password } = user;
 
-}
+        if (!email.includes('@')) {
+            throw new Error('email is incorrect');
+        };
+        if (password.length < 8) {
+            throw new Error('password is too short');
+        };
+
+        this.email = email;
+        this.password = password;
+    };
+
+    getEmail() {
+        return this.email;
+    };
+
+    getPassword() {
+        return this.password;
+    };
+
+    login() {
+        return this.email.includes('devmentor.pl');
+    };
+};
