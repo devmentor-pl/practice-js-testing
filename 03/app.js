b35ecc3558e8677cb51fab41adb1e6ac170cb49c
Marta Stelmach <marta.ms1961@gmail.com>
2022-09-01 18:49:02 +0200

    Fourth green test added

--- 03/app.js
+++ 03/app.js
@@ -5,6 +5,10 @@ export default function randomNumber(min, max) {
     if (!isANumber(max)) {
         throw new Error('second argument is not a number');
     };
+    if (min > max) {
+        throw new Error('first argument cannot be grater than second argument');
+    };
+
 
     return Math.random() * (max - min) + min;
 };
