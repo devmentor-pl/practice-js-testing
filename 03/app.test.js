1f3bafc26c23f852921e5ce6062b6a8bd58e797f
Zuza Ganobis <zuzaganobis@gmail.com>
2022-09-28 10:35:41 +0200

    refactor

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,26 +1,32 @@
 import randomNumber from "./app";
 
-it("return 1 when draw numbers from 1 to 1", () => {
-  expect(randomNumber(1, 1)).toBe(1);
+describe("Tests for min and max", () => {
+  it("return 1 when draw numbers from 1 to 1", () => {
+    expect(randomNumber(1, 1)).toBe(1);
+  });
+
+  it("throw exception when max is smaller than min", () => {
+    function useRandomNumber() {
+      randomNumber(2, 1);
+    }
+    expect(useRandomNumber).toThrow();
+  });
 });
 
-it("throw exception when min is not a number", () => {
-  function useRandomNumber() {
-    randomNumber("string", 1);
-  }
-  expect(useRandomNumber).toThrow();
+describe("Test for min", () => {
+  it("throw exception when min is not a number", () => {
+    function useRandomNumber() {
+      randomNumber("string", 1);
+    }
+    expect(useRandomNumber).toThrow();
+  });
 });
 
-it("throw exception when max is not a number", () => {
-  function useRandomNumber() {
-    randomNumber(1, "string");
-  }
-  expect(useRandomNumber).toThrow();
-});
-
-it("throw exception when if max will be less than min", () => {
-  function useRandomNumber() {
-    randomNumber(2, 1);
-  }
-  expect(useRandomNumber).toThrow();
+describe("Test for max", () => {
+  it("throw exception when max is not a number", () => {
+    function useRandomNumber() {
+      randomNumber(1, "string");
+    }
+    expect(useRandomNumber).toThrow();
+  });
 });
