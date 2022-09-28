cbf2dc976253a28493324cdbda7ea63cce9dc3e3
Zuza Ganobis <zuzaganobis@gmail.com>
2022-09-28 10:08:51 +0200

    Task 3: Created second test (RED)

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,10 @@ import randomNumber from "./app";
 it("return 1 when draw numbers from 1 to 1", () => {
   expect(randomNumber(1, 1)).toBe(1);
 });
+
+it("throw exception when min is not a number", () => {
+  function useRandomNumber() {
+    randomNumber("string", 1);
+  }
+  expect(useRandomNumber).toThrow();
+});
