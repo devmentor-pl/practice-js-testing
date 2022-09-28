23e24e45083c4c2186d9de21100ccaf608f4426c
Zuza Ganobis <zuzaganobis@gmail.com>
2022-09-28 10:09:36 +0200

    Task 3: Scond test pass (GREEN)

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,7 @@
 export default function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (typeof min !== "number") {
+    throw new Error("Property min have to be a number");
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
