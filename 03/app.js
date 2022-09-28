a0d9d18ec3ceb04cc51f5795da24d420b3678bdb
Zuza Ganobis <zuzaganobis@gmail.com>
2022-09-28 10:30:14 +0200

    Task 3: Third test pass (GREEN)

--- 03/app.js
+++ 03/app.js
@@ -4,6 +4,9 @@ export default function randomNumber(min, max) {
   }
   if (typeof max !== "number") {
     throw new Error("Property min have to be a number");
+  }
+  if (max < min) {
+    throw new Error("Property min is higher than propety max");
   } else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
