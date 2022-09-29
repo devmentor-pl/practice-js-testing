bfdd2eb7fdd3512aada8c7b8362d92af3b2178f3
Michal Filipowski <mikefilipowski@gmail.com>
2022-09-29 17:49:03 +0200

    [refactor] min/max not a number

--- 03/app.js
+++ 03/app.js
@@ -1,18 +1,28 @@
 export default function randomNumber(min, max) {
 
 
-    if (typeof min !== 'number') {
+    if (isNumber(min)) {
 
-        throw new Error('"min " is not a number')
+        errorMessage('"min " is not a number')
 
     }
 
     if (typeof max !== 'number') {
 
-        throw new Error('"max " is not a number')
+        errorMessage('"max " is not a number')
 
     }
 
     return Math.random() * (max - min) + min
 
+}
+
+function isNumber(value) {
+    return typeof value !== 'number'
+
+}
+
+function errorMessage(message) {
+
+    throw new Error(message)
 }
