62a1b5a8419ca1ff38d734b5205c6d9d6dcf21f6
Zuza Ganobis <zuzaganobis@gmail.com>
2022-09-29 11:19:19 +0200

    Solutions to be discussed...

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,25 @@
 export default class User {
+  constructor({ email, password }) {
+    this.email = email;
+    this.password = password;
 
+    if (!this.email.includes("@")) {
+      throw new Error("Email is incorrect");
+    }
+    if (this.password.length < 4) {
+      throw new Error("Password is incorrect");
+    }
+  }
+
+  getEmail() {
+    return this.email;
+  }
+  getPassword() {
+    return this.password;
+  }
+  login() {
+    if (this.email.includes("devmentor.pl")) {
+      return true;
+    } else return false;
+  }
 }
--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,75 @@
-import DB from './DB';
+import DB from "./DB";
+
+it("check if id is a number", async () => {
+  expect.assertions(1);
+  const db = new DB();
+  const id = "string";
+
+  try {
+    await db.insert({ id: id });
+  } catch (err) {
+    expect(err).toBe("ID can be only number!");
+  }
+});
+
+it("check if id is not duplicated", async () => {
+  const db = new DB();
+  const rowsLength = db.getRows().length;
+  expect.assertions(rowsLength);
+  const id = 3;
+
+  try {
+    await db.insert({ id: id });
+  } catch (err) {
+    expect(err).toBe("ID can't be duplicated!");
+  }
+});
+
+it("check if id found", async () => {
+  expect.assertions(1);
+  const db = new DB();
+  const id = 3;
+
+  try {
+    await db.select(id);
+  } catch (err) {
+    expect(err).toBe("ID not found");
+  }
+});
+
+it("check if item exist", async () => {
+  const db = new DB();
+  const rowsLength = db.getRows().length;
+  expect.assertions(rowsLength);
+  const id = 3;
+
+  try {
+    await db.remove(id);
+  } catch (err) {
+    expect(err).toBe("Item not exist!");
+  }
+});
+
+it("check if id is set", async () => {
+  expect.assertions(1);
+  const db = new DB();
+  const data = { someData: "someData" };
+
+  try {
+    await db.update(data);
+  } catch (err) {
+    expect(err).toBe("ID have to be set!");
+  }
+});
+
+it("check if id found", async () => {
+  expect.assertions(1);
+  const db = new DB();
+  const data = { id: 3, someData: "someData" };
+
+  try {
+    await db.update(data);
+  } catch (err) {
+    expect(err).toBe("ID not found!");
+  }
+});
