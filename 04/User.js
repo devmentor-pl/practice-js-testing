a78330e65f88940d311c78b31a3eb1c8d33124f6
Michal Filipowski <mikefilipowski@gmail.com>
2022-09-29 18:38:36 +0200

    finished 04

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,43 @@
 export default class User {
 
+    constructor(object) {
+
+        const { email, password } = object
+
+        if(!email.includes('@')){
+            
+            throw new Error('email must contain "@"')
+
+        }
+
+
+        if(password.length < 6){
+            
+            throw new Error('password must be at least 6 characters long')
+
+        }
+
+
+        this.email = email
+        this.password = password
+
+    }
+    getEmail() {
+
+        return this.email
+
+    }
+
+    getPassword() {
+
+        return this.password
+
+    }
+
+    login(){
+        return this.email.includes('devmentor.pl')
+    }
+
+
+
 }
