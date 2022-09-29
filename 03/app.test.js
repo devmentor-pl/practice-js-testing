012feaa803a169f7fb59cc2ab0eb7ac96b3e12a1
Michal Filipowski <mikefilipowski@gmail.com>
2022-09-29 17:41:51 +0200

    [red]'should "min" shoud be a number otherwise will throw error'

--- 03/app.test.js
+++ 03/app.test.js
@@ -6,3 +6,10 @@ it('should print 1 when number are in range from 1 to 1', () => {
     expect(randomNumber(1, 1)).toBe(1)
 
 })
+
+it('should "min" shoud be a number otherwise will throw error', ()=>{
+
+    expect(()=> randomNumber('a', 2)).toThrow('"min " is not a number')
+
+
+})
