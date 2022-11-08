feb1edfcd3a384885aab5dbd7d5f4d7ccff95981
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-08 17:48:41 +0100

    Add test for provided types of propertys

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,16 @@
 import randomNumber from './app';
+
+describe('RandomNumber', () => {
+    it('should return value of argument when arguments are the same number', () => {
+        expect(randomNumber(5,5)).toBe(5);
+
+    });
+
+    it('should throw exception when any of args isnt a number', () => {
+        const arg1 = '';
+        const arg2 = [];
+        expect(function(){randomNumber(arg1, arg2)}).toThrow('Property has to be a number');
+    });
+
+    it
+})
