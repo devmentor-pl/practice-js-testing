368f09cf1ccf51e01e3926a0ff31f64504ce8080
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-08 17:49:00 +0100

    Add if statement

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 export default function randomNumber(min, max) {
 
-}
+    if(typeof min !== 'number' || typeof max !== 'number'){
+        throw new Error('Property has to be a number');
+        
+    }
+
+    return Math.floor(Math.random() * (max - min + 1) + min)
+} 
+
+
