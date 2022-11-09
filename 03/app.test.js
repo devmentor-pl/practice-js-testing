f7b9f627cf732168445d2f9723bce6882d896348
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-09 14:39:02 +0100

    Add test for integers

--- 03/app.test.js
+++ 03/app.test.js
@@ -14,11 +14,14 @@ describe('RandomNumber', () => {
     it('should throw exception when max range is smaller than min range', () => {
         const min = 10;
         const max = 9;
-        expect(() => randomNumber(min, max)).toThrow();
+        expect(() => randomNumber(min, max)).toThrow('Max range cannot be smaller than min range');
     });
 
-    it('should be between range of min and max', () => {
+    it('should suits provided range of min and max', () => {
         expect(randomNumber(3,10)).toBeGreaterThanOrEqual(3);
         expect(randomNumber(3,10)).toBeLessThanOrEqual(10);
+    })
+    it('should throw exception when provided argument is not an integer', () =>{
+        expect(() => randomNumber(1.2, 3)).toThrow('Provided number has to be an integer');
     })
 })
