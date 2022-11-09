9aa82b54c4419190a8ce4a80691bc01059d1d0a5
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-09 13:45:07 +0100

    Add test if the result suits the range

--- 03/app.js
+++ 03/app.js
@@ -3,11 +3,10 @@ export default function randomNumber(min, max) {
     if(typeof min !== 'number' || typeof max !== 'number'){
         throw new Error('Property has to be a number');
         
-    }
-    if(max < min){
+    }else if(max < min){
         throw new Error('Max range cannot be smaller than min range');
     }
-    
+
     return Math.floor(Math.random() * (max - min + 1) + min)
 } 
 
--- 03/app.test.js
+++ 03/app.test.js
@@ -3,18 +3,22 @@ import randomNumber from './app';
 describe('RandomNumber', () => {
     it('should return value of argument when arguments are the same number', () => {
         expect(randomNumber(5,5)).toBe(5);
-
     });
 
     it('should throw exception when any of args isnt a number', () => {
         const min = '';
         const max = 6;
-        expect(function(){randomNumber(min, max)}).toThrow('Property has to be a number');
+        expect(() => randomNumber(min, max)).toThrow('Property has to be a number');
     });
 
     it('should throw exception when max range is smaller than min range', () => {
         const min = 10;
         const max = 9;
-        expect(function(){randomNumber(min, max)}).toThrow();
+        expect(() => randomNumber(min, max)).toThrow();
+    });
+
+    it('should be between range of min and max', () => {
+        expect(randomNumber(3,10)).toBeGreaterThanOrEqual(3);
+        expect(randomNumber(3,10)).toBeLessThanOrEqual(10);
     })
 })
