a98c637489384b22c191808f7c8a057a41846f1c
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-10 14:04:37 +0100

    Change error message

--- 03/app.js
+++ 03/app.js
@@ -1,13 +1,16 @@
 export default function randomNumber(min, max) {
 
     if(typeof min !== 'number' || typeof max !== 'number'){
-        throw new Error('Property has to be a number');
         
+        throw new Error('Incorrect type of property.');
     }else if(max < min){
-        throw new Error('Max range cannot be smaller than min range');
+        
+        throw new Error('Incorrect range of numbers.');
     }else if(!Number.isInteger(min) || !Number.isInteger(max)){
-        throw new Error('Provided number has to be an integer');
+       
+        throw new Error('Incorrect number provided.');
     }
+
     return Math.floor(Math.random() * (max - min + 1) + min)
 } 
 
