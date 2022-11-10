d31ebfc93b2ccfce04309c9c293c386b58a5b7bb
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-10 14:05:00 +0100

    Change error message

--- 03/app.test.js
+++ 03/app.test.js
@@ -8,20 +8,21 @@ describe('RandomNumber', () => {
     it('should throw exception when any of args isnt a number', () => {
         const min = '';
         const max = 6;
-        expect(() => randomNumber(min, max)).toThrow('Property has to be a number');
+        expect(() => randomNumber(min, max)).toThrow('Incorrect type of property.');
     });
 
     it('should throw exception when max range is smaller than min range', () => {
         const min = 10;
         const max = 9;
-        expect(() => randomNumber(min, max)).toThrow('Max range cannot be smaller than min range');
+        expect(() => randomNumber(min, max)).toThrow('Incorrect range of numbers.');
     });
 
     it('should suits provided range of min and max', () => {
         expect(randomNumber(3,10)).toBeGreaterThanOrEqual(3);
         expect(randomNumber(3,10)).toBeLessThanOrEqual(10);
-    })
+    });
+
     it('should throw exception when provided argument is not an integer', () =>{
-        expect(() => randomNumber(1.2, 3)).toThrow('Provided number has to be an integer');
-    })
+        expect(() => randomNumber(1.2, 3)).toThrow('Incorrect number provided.');
+    });
 })
