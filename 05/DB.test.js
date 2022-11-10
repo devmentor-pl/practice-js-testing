919706a9578bc5f33cf571f8d959f9f2b9fc7fe4
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-10 15:56:00 +0100

    Add test for insert, remove and update methods

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -2,36 +2,93 @@ import DB from './DB';
 
 describe('DB', () => {
     describe('insert', () => {
-        it('should add data object to the array', () => {
-           async() => {
+        it('should add data when it has no id ', async () => {
+            const subject = new DB;
+            const data = {a: 12, b: 13};
+            const promise = subject.insert(data);
 
-               const subject = new DB;
-               const data = {a: 3, b: 4}
-               // when 
-                const result = await subject.insert(data)
-   
-                expect(subject._rows).toContain(result);
-           }
+            await expect(promise).resolves.toBe(data);
         });
-        it('should return null if data id is not a number',()  => {
-            async() => {
-
-                const subject = new DB;
-                const data = {a: 3, b: 4, id: '2'}
-                const result = await subject.insert(data)
     
-                expect(result).toBeNull();
-            }
+        
+        it('should reject to add data when it\'s id is not a type of number', async () => {
+            const subject = new DB;
+            const data = {a: 3, b: 4, id: 's'}
+            const promise = subject.insert(data)
+
+            await expect(promise).rejects.toMatch('ID can be only number!');
+
         });
 
-        it('should return null when data is duplicated', ()  => {
-            async() => {
-                const subject = new DB;
-                const data = {a: 3, b: 4, id: '2'}
-                await subject.insert(data)
-                const result = await (subject.insert(data));
-                expect(result).toBeNull();
-            }
+        it('should rejectto add data when it\'s id is duplicated', async ()  => {
+            const subject = new DB;
+            const data = {a: 3, b: 4, id: 2}
+            await subject.insert(data)
+            
+            const promise = subject.insert(data);
+            await expect(promise).rejects.toMatch('ID can\'t be duplicated!');
+            
         });
+    })
+    describe('remove', () => {
+        it('should remove data when it exists', async () => {
+            const subject = new DB;
+            const data = {a: 2, b: 3, id:1};
+            const id = 1;             
+            await subject.insert(data);
+            
+            const promise = subject.remove(id);
+            
+            await expect(promise).resolves.toBe('Item was remove!');
+        })
+
+        it('should reject to remove data when data doesn\'t exist', async () => {
+            const subject = new DB;
+            const data = {a: 2, b: 3, id:1};
+            const id = 2;             
+            await subject.insert(data);
+            
+            const promise = subject.remove(id);
+            
+            await expect(promise).rejects.toMatch('Item not exist!');    
+        })
+    
+    })
+    describe('update', () => {
+        it('should resolve when data id matches new provided data', async () => {
+            const subject = new DB;
+            const data = {a: 2, b: 3, id:1};
+            const newData = {a:4, b: 11, id:1}
+            await subject.insert(data);
+            const promise = subject.update(newData);
+
+            await expect(promise).resolves.toBe(newData);
+
+        })
+        it('should reject when provided data has no id', async () => {
+            const subject = new DB;
+            const data = {a: 2, b: 3, id:1};
+            const newData = {a:4, b: 11}
+            await subject.insert(data);
+            const promise = subject.update(newData);
+
+            await expect(promise).rejects.toBe('ID have to be set!');
+
+        })
+        it('should reject when provided data has no match id', async () => {
+            const subject = new DB;
+            const data = {a: 2, b: 3, id:1};
+            const newData = {a:4, b: 11, id: 2}
+            await subject.insert(data);
+            const promise = subject.update(newData);
+
+            await expect(promise).rejects.toBe('ID not found!');
+
+        })
+    })
+    describe('getRows', () => {
+        it('', () => {
+
+        })
     })
 })
