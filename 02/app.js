211eb9bd07d503e759ed1faf8a4c230e810a26bb
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-11-12 19:29:14 +0100

    Add displayErrorMessage func

--- 02/app.js
+++ 02/app.js
@@ -23,16 +23,20 @@ function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
         try{
             setRandomPosition(this, error);
-
         }catch(e){
-            const displayElement = document.querySelector('.alert');
-            const message = displayElement.querySelector('.alert__message');
-            displayElement.classList.toggle('alert--hidden');
-            message.innerText = e.message;
-            displayElement.onclick = () => {
-                displayElement.classList.toggle('alert--hidden');
-            }
+            displayErrorMEssage(e.message);
         }
     })
+}
+
+function displayErrorMEssage(errorMessage){
+    const displayElement = document.querySelector('.alert');
+    const message = displayElement.querySelector('.alert__message');
+    displayElement.classList.toggle('alert--hidden');
+    message.innerText = errorMessage;
+    displayElement.onclick = () => {
+        displayElement.classList.toggle('alert--hidden');
+    }
+
 }
 
