8018dcd1b179465c95bd06a01e0d7ba53f49b4f1
Jacek Zielinski <gandi34@wp.pl>
2022-11-27 17:07:34 +0100

    Almost Done

--- 02/app.js
+++ 02/app.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', init);
 
+try {
 function init() {
     const clickEl = document.querySelector('.error--click');
     const enterEl = document.querySelector('.error--enter');
-
+ 
     setRandomPosition(clickEl);
     setRandomPosition(enterEl);
 
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 
-}
+}} catch(e) {
 
 function setRandomPosition(element, error = null) {
     element.style.top = Math.random() * 600 + 'px';
@@ -19,10 +20,16 @@ function setRandomPosition(element, error = null) {
     if(error) {
         throw error;
     }
+}} finally {
+    const alertEl = document.querySelector('alert__message');
+    
 }
 
 function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
         setRandomPosition(this, error);
     })
-}
+} 
+
+
+
