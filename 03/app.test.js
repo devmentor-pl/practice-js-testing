2f3339eeb767cead9da0cf90bcd65fc56939e904
Jacek Zielinski <gandi34@wp.pl>
2022-12-05 18:04:08 +0100

    Test not a numer

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,16 @@
+//Test 1
+
 import randomNumber from './app';
 
 it ('return 1 if min=1 and max=1', () =>{
     expect( randomNumber(1, 1)).toBe(1);
-})
+});
+
+
+
+
+//Test 2
+
+it ('show error if os not a number', () => {
+    expect ( () => randomNumber('1', 10)).toThrow('min is not a number');
+});
