d76c55bdfcb663ec490d0ef7b736c904962788a3
Jacek Zielinski <gandi34@wp.pl>
2022-12-05 21:12:26 +0100

    05 done

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,23 @@
 import DB from './DB';
+
+describe('.insert()', async () => {
+
+    it('take 1 item', async () => {
+        const dataBase =new DB();
+        dataBase.insert({id: 1, name: 'devmentor'});
+        const rows = await dataBase.getRows();
+
+        expect(rows.length).toBe(1);
+    });
+
+    it('show error when id is not a number', async () => {
+        const dataBase =new DB();
+        try {
+            await dataBase.insert({id: '1', name: 'devmentor'});
+        }catch (err) {
+        
+        expect(err).toBe('ID can be only number!');
+        
+        }
+    });
+});
