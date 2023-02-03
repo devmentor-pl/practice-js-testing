f9940a05d87ef42a9fbc78c3e5b0b928348a0492
Anna <aniamatrackasadurek@gmail.com>
2023-02-03 10:28:30 +0100

    first test created

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
 import randomNumber from './app';
+
+describe('check range', () => {
+    it('if value min & value max are equal to 1', () => {
+        expect(randomNumber(1, 1)).toBe(1);
+    });
+})
