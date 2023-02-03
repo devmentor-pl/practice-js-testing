b99618f6fa420790fbf5718a5cd8e5ce18b63586
Anna <aniamatrackasadurek@gmail.com>
2023-02-03 12:39:49 +0100

    added tests to insert method

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,21 @@
 import DB from './DB';
+
+describe('test for insert method', () => {
+    it('if ID is a number', () => {
+        const db = new DB();
+        const data = {
+            id: 'abc'
+        }
+        const promise = db.insert(data);
+        return promise.catch(err => {
+            expect(err).toBe('ID can be only number!')
+        })
+    })
+    it('if ID is not duplicated', async () => {
+        const db = new DB();
+        await db.insert({id: 8})
+        return db.insert({id: 8}).catch(err => {
+            expect(err).toBe('ID can\'t be duplicated!')
+        })
+    })
+})
