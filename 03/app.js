3d013a7a18224b5280bf7ee3b4a6094532818e04
Anna <aniamatrackasadurek@gmail.com>
2023-02-03 11:09:02 +0100

    test 4 passed

--- 03/app.js
+++ 03/app.js
@@ -11,4 +11,5 @@ export default function randomNumber(min, max) {
     if((typeof min !== 'number') && (typeof max !== 'number')) {
         throw new Error('given value is not a number')
     }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
