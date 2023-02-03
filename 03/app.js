b5230ae36dcc1db5d5bc13d780a7824cb0bbba64
Anna <aniamatrackasadurek@gmail.com>
2023-02-03 11:11:25 +0100

    exercise 3 refactor

--- 03/app.js
+++ 03/app.js
@@ -1,15 +1,21 @@
 export default function randomNumber(min, max) {
 
+    validateData(min, max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function validateData(min, max) {
     if( min === max) {
         return min
     }
-
+    
     if(min > max) {
         throw new Error('value of min cannot be greater than value of max')
     }
-
+    
     if((typeof min !== 'number') && (typeof max !== 'number')) {
         throw new Error('given value is not a number')
     }
-    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
