0672011457743fdd70afe47b94a2e66c0f82a516
Anna <aniamatrackasadurek@gmail.com>
2023-02-03 10:40:41 +0100

    second test passed

--- 03/app.js
+++ 03/app.js
@@ -3,4 +3,8 @@ export default function randomNumber(min, max) {
     if( min === max) {
         return min
     }
+
+    if(min > max) {
+        throw new Error('value of min cannot be greater than value of max')
+    }
 }
