8b72d3e43a144001bf63a0a4023144f4c8978c0e
Anna <aniamatrackasadurek@gmail.com>
2023-02-03 10:38:54 +0100

    second test created

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,13 @@ describe('check range', () => {
     it('if value min & value max are equal to 1', () => {
         expect(randomNumber(1, 1)).toBe(1);
     });
+
+    it('if min > max', () => {
+        function checkNumbers() {
+            const min = 10;
+            const max = 5;
+            randomNumber( min, max);
+        }
+        expect(checkNumbers).toThrow('value of min cannot be greater than value of max');
+    });
 })
