cce82319fc1ee648e7631d8edf427245a4bb8bdc
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-02-09 11:33:16 +0100

    Add first test, check if value is a number

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,16 @@
 import randomNumber from './app';
+
+describe('randomNumber', () => {
+	it('should throw exception if min value is not a number', () => {
+		function getRandomNumber() {
+			randomNumber('a');
+		}
+		expect(getRandomNumber).toThrow('Given number must be a number!');
+	});
+	it('should throw exception if max value is not a number', () => {
+		function getRandomNumber() {
+			randomNumber(undefined, 'a');
+		}
+		expect(getRandomNumber).toThrow('Given number must be a number!');
+	});
+});
