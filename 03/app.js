92e2a6a2a9405a64bce071b1db5fd0137cf0aa0e
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-02-09 11:44:13 +0100

    Add implementation of drawing random number from the range

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,7 @@
 export default function randomNumber(min, max) {
 	if (typeof min !== 'number' || typeof max !== 'number') {
 		throw new Error('Given number must be a number!');
+	} else {
+		return Math.random() * (max - min) + min;
 	}
 }
