a9b3620f3e18a581fb1c7cf5733c724ca63a8c23
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-02-09 11:42:44 +0100

    Add second test, check if return 1 from the renage <1,1>

--- 03/app.test.js
+++ 03/app.test.js
@@ -7,10 +7,16 @@ describe('randomNumber', () => {
 		}
 		expect(getRandomNumber).toThrow('Given number must be a number!');
 	});
+
 	it('should throw exception if max value is not a number', () => {
 		function getRandomNumber() {
 			randomNumber(undefined, 'a');
 		}
 		expect(getRandomNumber).toThrow('Given number must be a number!');
+	});
+
+	it('should return 1 if drawing number from the range <1,1>', () => {
+		const number = randomNumber(1, 1);
+		expect(number).toBe(1);
 	});
 });
