16ba633a1223d258b73f94529b462bc587ca9c41
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-02-09 10:48:39 +0100

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,48 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+	const clickEl = document.querySelector('.error--click');
+	const enterEl = document.querySelector('.error--enter');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+	setRandomPosition(clickEl);
+	setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+	initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+	initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+	element.style.top = Math.random() * 600 + 'px';
+	element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
-    }
+	if (error) {
+		throw error;
+	}
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+	element.addEventListener(eventName, function () {
+		try {
+			setRandomPosition(this, error);
+		} catch (error) {
+			const alertBox = document.querySelector('.alert');
+			showErrorAlert(alertBox, error);
+			closeErrorAlert(alertBox);
+		}
+	});
+}
+
+function showErrorAlert(alertBox, error) {
+	const alertText = document.querySelector('.alert__message');
+
+	alertBox.classList.remove('alert--hidden');
+	alertText.textContent = `Przechwycono błąd typu: ${error.name} o treści: ${error.message}`;
+}
+
+function closeErrorAlert(alertBox) {
+	alertBox.addEventListener('click', (e) => {
+		if (e.target.classList.contains('alert')) {
+			e.target.classList.add('alert--hidden');
+		}
+	});
 }
