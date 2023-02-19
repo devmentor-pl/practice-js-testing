e5959e5428eadf70ad43eccd1fb20879e57073f8
Michal <maxpoz3212@gmail.com>
2023-02-19 16:04:47 +0100

    Add implementation of throwing error

--- 03/app.js
+++ 03/app.js
@@ -1,1 +1,6 @@
-export default randomNumber = (min, max) => Math.random() * (max - min) + min;
+export default randomNumber = (min, max) => {
+    if (isNaN(min) || isNaN(max)) {
+        throw new Error('Min or max is not a number!');
+    }
+    return Math.random() * (max - min) + min;
+}
