49637b46db03156f9bc6b32c22c904a63556aecf
Michal <maxpoz3212@gmail.com>
2023-02-19 19:09:00 +0100

    Add test of db size after adding 2 items

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -2,13 +2,25 @@ import DB from './DB';
 
 describe('Database', () => {
     describe('insert', () => {
+        it('Should return 2 when insert 2 items to database',
+            async () => {
+                const db = new DB();
+                await db.insert({ a: 1, b: 2 });
+                await db.insert({ a: 3, b: 4 });
+
+                const dbSize = db._rows.length;
+
+                expect(dbSize).toBe(2);
+            });
+
         it('Should return ID: 1 when insert first database item',
             async () => {
                 const db = new DB();
-                const data = { a: 1, b: 2 };
-                const result = await db.insert(data);
+                const itemData = { a: 1, b: 2 };
+                const result = await db.insert(itemData);
 
                 expect(result.id).toBe(1);
             });
+
     })
 })
