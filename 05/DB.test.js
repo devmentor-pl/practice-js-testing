9250c18aa7858ef8be4fb26b81256fb79c786950
Michal <maxpoz3212@gmail.com>
2023-02-19 19:01:01 +0100

    Add test of creating data id

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,14 @@
 import DB from './DB';
+
+describe('Database', () => {
+    describe('insert', () => {
+        it('Should return ID: 1 when insert first database item',
+            async () => {
+                const db = new DB();
+                const data = { a: 1, b: 2 };
+                const result = await db.insert(data);
+
+                expect(result.id).toBe(1);
+            });
+    })
+})
