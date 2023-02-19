4c5917e6fe6973b1c56c2d5a3a7184856717f48f
Michal <maxpoz3212@gmail.com>
2023-02-19 14:05:39 +0100

    Add task 02 solution

--- 02/app.js
+++ 02/app.js
@@ -9,20 +9,43 @@ function init() {
 
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
 }
 
 function setRandomPosition(element, error = null) {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
+    if (error) {
         throw error;
     }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
+    element.addEventListener(eventName, function () {
+        try {
+            setRandomPosition(this, error);
+        } catch (error) {
+            showAlertElement(error);
+        }
+    })
+}
+
+function showAlertElement(error) {
+    const alertElement = document.querySelector('.alert');
+    const alertMessageElement = alertElement.querySelector('.alert__message');
+
+    alertMessageElement.innerText = error.message;
+    alertElement.classList.remove('alert--hidden');
+
+    initCloseAlertEvent(alertElement);
+}
+
+function initCloseAlertEvent(alertElement) {
+    alertElement.addEventListener('click', event => {
+        const targetElement = event.target;
+
+        if (targetElement.classList.contains('alert')) {
+            targetElement.classList.add('alert--hidden');
+        }
     })
 }
