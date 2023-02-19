48ae61932dc02a7ce480f0cb6a8c13a0f96ae834
Michal <maxpoz3212@gmail.com>
2023-02-19 16:02:34 +0100

    Add second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,10 @@ describe('RandomNumber', () => {
     it('return 1 if min and max = 1', () => {
         expect(randomNumber(1, 1)).toBe(1)
     });
+
+    it('throw exception when min is not a number', () => {
+        expect(()=> {
+            randomNumber('some string', 2);
+        }).toThrow();
+    });
 });
