66a166012d3c28684439def1d5e45c76266ad025
Michal <maxpoz3212@gmail.com>
2023-02-19 20:06:20 +0100

    Add test of id number

--- 05/DB.js
+++ 05/DB.js
@@ -5,18 +5,19 @@ export default class DB {
 
     insert(data) {
         return new Promise((resolve, reject) => {
-            if(data.id) {
-                if(typeof data.id !== 'number') {
-                    this.async(reject,'ID can be only number!');
+            if (data.id) {
+                // if(typeof data.id !== 'number') {
+                if (typeof data.id !== 'number') {
+                    this.async(reject, 'ID can be only number!');
                     return null; // stop function
-                } else if(this._rows.some(item => item.id === data.id)) {
+                } else if (this._rows.some(item => item.id === data.id)) {
                     this.async(reject, 'ID can\'t be duplicated!');
                     return null; // stop function
                 }
             }
 
             this.async(() => {
-                if(!data.id) {
+                if (!data.id) {
                     data.id = this._rows.reduce((acc, item) => {
                         return acc <= item.id ? item.id + 1 : acc;
                     }, 1);
@@ -24,7 +25,7 @@ export default class DB {
 
                 this._rows.push(data);
                 resolve(data)
-            }); 
+            });
         });
     }
 
@@ -32,7 +33,7 @@ export default class DB {
         return new Promise((resolve, reject) => {
             this.async(() => {
                 const [row = null] = this._rows.filter(item => item.id === id);
-                if(row) {
+                if (row) {
                     resolve(row);
                 } else {
                     reject('ID not found');
@@ -47,8 +48,8 @@ export default class DB {
                 const lengthBeforeFilter = this._rows.length;
                 this._rows = this._rows.filter(item => item.id !== id);
                 const lengthAfterFilter = this._rows.length;
-                
-                if(lengthBeforeFilter === lengthAfterFilter) {
+
+                if (lengthBeforeFilter === lengthAfterFilter) {
                     reject('Item not exist!');
                 } else {
                     resolve('Item was remove!');
@@ -59,24 +60,24 @@ export default class DB {
 
     update(data) {
         return new Promise((resolve, reject) => {
-            if(!data.id) {
+            if (!data.id) {
                 this.async(reject, 'ID have to be set!');
             } else {
                 this.async(() => {
                     let updated = null;
                     this._rows = this._rows.map(item => {
-                        if(item.id === data.id) {
+                        if (item.id === data.id) {
                             updated = data
                             return updated;
                         }
-            
+
                         return item;
                     });
 
-                    if(updated) {
+                    if (updated) {
                         resolve(updated);
                     } else {
-                        reject('ID not found!');   
+                        reject('ID not found!');
                     }
                 });
             }
@@ -89,7 +90,7 @@ export default class DB {
                 this._rows = [];
                 resolve(true);
             });
-            
+
         })
     }
 
--- 05/DB.test.js
+++ 05/DB.test.js
@@ -16,11 +16,18 @@ describe('Database', () => {
         it('Should return ID: 1 when insert first database item',
             async () => {
                 const db = new DB();
-                const itemData = { a: 1, b: 2 };
-                const result = await db.insert(itemData);
+                const result = await db.insert({ a: 1, b: 2 });
 
                 expect(result.id).toBe(1);
             });
 
-    })
-})
+        it('Should reject when id is not a number',
+            async () => {
+                const db = new DB();
+                const itemData = { id: 'some string', a: 1, b: 2 };
+
+                await expect(db.insert(itemData))
+                    .rejects.toBe('ID can be only number!');
+            });
+    });
+});
