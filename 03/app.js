6dae404b67b2aae9b2677b9dc5453488fb4651e4
Michal <maxpoz3212@gmail.com>
2023-02-19 16:33:32 +0100

    Add implementation of range check

--- 03/app.js
+++ 03/app.js
@@ -3,6 +3,10 @@ export default randomNumber = (min, max) => {
         throw new Error('Min or max is not a number!');
     }
 
+    if(min > max){
+        throw new Error('Min value should be smaller than max value.');
+    }
+
     return Math.random() * (max - min) + min;
 }
 
