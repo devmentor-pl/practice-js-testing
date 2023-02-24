f709e689ab91613feb6caafe1f80d29d335336dc
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-02-24 18:00:54 +0100

    04 done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,30 @@
 export default class User {
+    constructor({
+        email,
+        password
+    }) {
+
+        if (email != "koder@devmentor.pl") {
+            throw new Error('Invalid email address');
+        }
+        if (password != 'pw123456') {
+            throw new Error('Invalid password');
+        }
+
+        this.email = email;
+        this.password = password;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getPassword() {
+        return this.password;
+    }
+    login() {
+        if (this.email.includes('devmentor.pl')) {
+            return true;
+        } else return false;
+    }
+
 
 }
