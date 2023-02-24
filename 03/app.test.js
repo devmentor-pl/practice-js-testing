872e7fcd614e172c2a87f066dadc8a9cb6bf2497
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-02-24 16:37:30 +0100

    not a number test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,9 @@
 import randomNumber from './app';
 
 test('expect 1', () => {
-    expect(randomNumber(1, 1)).toBe(2);
+    expect(randomNumber(1, 1)).toBe(1);
 });
+
+test("not number", () => {
+    expect(randomNumber(1, "text")).toBe(notNumber);
+})
