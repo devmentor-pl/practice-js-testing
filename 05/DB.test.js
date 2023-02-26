1f2fb9851b5b9a4b153e6f78f855bbc76a73e4f8
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-02-26 13:40:38 +0100

    05 done

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,45 @@
 import DB from './DB';
+describe("insert", () => {
+    it("should return null when insert not a number", async () => {
+        const db = new DB();
+        await expect(db.insert({
+            id: "NAN"
+        })).rejects.toEqual("ID can be only number!")
+    });
+
+    it("should return null when id are duplicated", async () => {
+        const db = new DB();
+        await db.insert({
+            id: 1
+        });
+
+        await expect(db.insert({
+            id: 1
+        })).rejects.toEqual("ID can\'t be duplicated!")
+    });
+})
+
+
+describe("select", () => {
+    it("should return ID not found", async () => {
+        const db = new DB();
+        await expect(db.select({
+            id: 1
+        })).rejects.toEqual("ID not found")
+    });
+})
+
+describe("remove", () => {
+    it("should return Item not exist", async () => {
+        const db = new DB();
+        await expect(db.remove(1)).rejects.toEqual("Item not exist!")
+    });
+
+    it("should return Item was remove", async () => {
+        const db = new DB();
+        await db.insert({
+            id: 2
+        })
+        await expect(db.remove(2)).resolves.toEqual("Item was remove!")
+    });
+})
