d5685d27b354920ac267308c135529f9ae74f7bc
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-04-03 11:43:48 +0200

    task-01 done

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,28 @@
 function getProductList() {
-    return [
-        {name: 'JavaScript: podstawy', type: 'book', count: 3, price: 67.19, discount: 0.21},
-        {name: 'React: podstawy', type: 'book', count: 4, price: 79.17, discount: 0.27}
-    ]
+	return [
+		{
+			name: 'JavaScript: podstawy',
+			type: 'book',
+			count: 3,
+			price: 67.19,
+			discount: 0.21,
+		},
+		{
+			name: 'React: podstawy',
+			type: 'book',
+			count: 4,
+			price: 79.17,
+			discount: 0.27,
+		},
+	];
 }
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
-    });
+	return productList.reduce((acc, product) => {
+		const discountPrice = product.price * product.discount;
+		return acc + (product.price - discountPrice) * product.count;
+	}, 0);
 }
 
-const totalPrice = getTotalPrice( getProductList() );
-console.log(totalPrice); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
+const totalPrice = getTotalPrice(getProductList());
+console.log(totalPrice.toFixed(2)); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
