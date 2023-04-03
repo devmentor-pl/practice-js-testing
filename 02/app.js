8e32f1577eed3a7cf6c8011a7cc58e866e4fc1ce
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-04-03 12:53:27 +0200

    task-02 done

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,52 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+	const clickEl = document.querySelector('.error--click');
+	const enterEl = document.querySelector('.error--enter');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+	setRandomPosition(clickEl);
+	setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+	initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+	initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+	element.style.top = Math.random() * 600 + 'px';
+	element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
-    }
+	if (error) {
+		throw error;
+	}
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+	element.addEventListener(eventName, function () {
+		try {
+			setRandomPosition(this, error);
+		} catch (e) {
+			setErrorMsg(e.message);
+			showError();
+		}
+	});
+}
+
+function showError() {
+	const alertSection = document.querySelector('.alert');
+	alertSection.classList.remove('alert--hidden');
+
+	closeAlert(alertSection);
+}
+
+function setErrorMsg(errorMsg) {
+	const alertMsg = document.querySelector('.alert__message');
+	alertMsg.textContent = errorMsg;
+}
+
+function closeAlert(element) {
+	element.addEventListener('click', (e) => {
+		if (e.target.classList.contains('alert')) {
+			element.classList.add('alert--hidden');
+		}
+	});
 }
