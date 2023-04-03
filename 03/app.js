3245381fce6f09a833598ad0d4cc6f26204a1f08
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-03 13:12:31 +0200

    Add ex 3 third implementation

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,5 @@
 export default function randomNumber(min, max) {
-	if (typeof min !== "number") {
+	if (typeof min !== "number" || typeof max !== "number") {
 		throw new Error("Min and max have to be numbers");
 	}
 	return Math.floor(Math.random() * (max - min)) + min;
