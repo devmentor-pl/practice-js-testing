7ed948a1a4c1251a671883d4d245a5de09b9de3a
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-03 13:44:45 +0200

    Add ex 3 refactorization

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,10 @@
 export default function randomNumber(min, max) {
+	validateNumbers(min, max);
+
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function validateNumbers(min, max) {
 	if (min === max) {
 		return 1;
 	}
@@ -10,6 +16,4 @@ export default function randomNumber(min, max) {
 	if (min > max) {
 		throw new Error("Value of min has to be smaller than value of max");
 	}
-
-	return Math.floor(Math.random() * (max - min)) + min;
 }
