bce65608d71ffc0cb1888b7178587d85f38ca861
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-04-03 17:22:37 +0200

    test 4 implementation

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,7 @@
 export default function randomNumber(min, max) {
+	if (min > max) {
+		throw new Error('max value must be greater than min value');
+	}
 	if (typeof min !== 'number' || typeof max !== 'number') {
 		throw new Error('"min" or "max" value is not a number');
 	}
