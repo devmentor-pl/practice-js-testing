4e65cf8a26234e3530cad00fdce2363faf03b999
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-03 13:07:58 +0200

    Add ex 3 second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,9 +4,16 @@ describe("randomNumber()", () => {
 	it("return 1 if range is from 1 to 1", () => {
 		const max = 1;
 		const min = 1;
-
 		const result = randomNumber(1, 1);
-
 		expect(result).toBe(1);
 	});
+
+	it("throw exception when prop min is not a number", () => {
+		function checkNumber() {
+			randomNumber("string");
+		}
+		expect(checkNumber).toThrow();
+	});
 });
+
+// jeśli podasz jako argument "nie liczbę", to zostanie rzucony błąd,
