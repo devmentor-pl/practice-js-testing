c0a7eb01b04069fd0a2a257bd64969a51a19869f
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-03 12:54:35 +0200

    Add ex 3 first test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,12 @@
-import randomNumber from './app';
+import randomNumber from "./app";
+
+describe("randomNumber()", () => {
+	it("return 1 if range is from 1 to 1", () => {
+		const max = 1;
+		const min = 1;
+
+		const result = randomNumber(1, 1);
+
+		expect(result).toBe(1);
+	});
+});
