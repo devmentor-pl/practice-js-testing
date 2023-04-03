1fb6baa5273192c25efac47538e0132c6ec1bcaf
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-04-03 16:12:58 +0200

    add test 2

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,10 @@ import randomNumber from './app';
 it('return 1 when range is 1 to 1', () => {
 	expect(randomNumber(1, 1)).toBe(1);
 });
+
+it('throw exception when "min" is not a number', () => {
+	function isNumber() {
+		randomNumber(undefined, 1);
+	}
+	expect(isNumber).toThrow();
+});
