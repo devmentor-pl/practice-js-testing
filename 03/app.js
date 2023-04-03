30bf2a1305ecd874b9b1311a6101580fa457a338
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-04-03 16:35:22 +0200

    test 3 implementation

--- 03/app.js
+++ 03/app.js
@@ -1,7 +1,9 @@
 export default function randomNumber(min, max) {
-
 	if (typeof min !== 'number') {
 		throw new Error('Value "min" is not a number');
+	}
+	if (typeof max !== 'number') {
+		throw new Error('Value "max" is not a number');
 	}
 
 	const number = Math.floor(Math.random() * (max - min + 1) + min);
