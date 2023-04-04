3f74dace466d0d405ec1bc59d1a7f6f73184d7cd
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-04 09:24:04 +0200

    Ex 4 second test passed

--- 04/User.js
+++ 04/User.js
@@ -2,6 +2,7 @@ export default class User {
 	constructor({ email, password }) {
 		this.email = email;
 		this.password = password;
+		this.validateEmail();
 	}
 
 	getEmail() {
@@ -11,4 +12,18 @@ export default class User {
 	getPassword() {
 		return this.password;
 	}
+
+	validateEmail() {
+		if (!this.email.includes("@")) {
+			throw new Error("This email in incorrect");
+		}
+	}
 }
+
+// it("should throw exception when email is incorrect", () => {
+//     function createWrongUserData() {
+//         new User({ email: "devmentor.pl", password: "pw123456" });
+//     }
+
+//     expect(createWrongUserData).toThrow();
+// });
