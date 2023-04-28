ec52e9b14e8a9335a1d72256dd00c8c6ed0271c6
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-04-28 17:18:18 +0200

    test2

--- 03/app.test.js
+++ 03/app.test.js
@@ -5,3 +5,13 @@ it('return 1 if min is set to 1 and max is set to 1', () => {
     expect(randomNumber(1,1)).toBe(1);
 
 });
+
+it("throw error when property min is not a number", () => {
+
+    function createRandomNumber(){
+        randomNumber('4', 1)
+    }
+
+    expect(createRandomNumber).toThrow()
+
+})
