52b5d60dee00bfe832f47501bfc8d1902d8afde4
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-04-28 19:34:17 +0200

    commit task 4

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,30 @@
 export default class User {
+  constructor({ email, password }) {
+    this.email = email;
+    this.password = password;
+    this.getEmail();
+    this.getPassword();
+  }
 
+  getEmail() {
+    if (!this.email.includes("@")) {
+      throw new Error("email is incorrect");
+    }
+    return this.email;
+  }
+
+  getPassword() {
+    if (this.password.length < 8) {
+      throw new Error("password is incorrect");
+    }
+    return this.password;
+  }
+
+  login() {
+    if (this.email.includes("devmentor.pl")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
