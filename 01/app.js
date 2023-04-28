fdef55fca6a4d9f2da9556ad634a551adf9ab93b
Tomek2112 <kaminski_tomasz@interia.pl>
2023-04-28 12:26:09 +0200

    3/5

--- 01/app.js
+++ 01/app.js
@@ -7,9 +7,9 @@ function getProductList() {
 
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
-    });
+    return productList.reduce( (acc, product) => {
+        return acc + (product.price * product.count * (1 - product.discount));
+    }, 0);
 }
 
 const totalPrice = getTotalPrice( getProductList() );
