3f98e0659fd556a9f5fadbea36114d5af66ad81d
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-04-28 17:37:25 +0200

    commit for tasks  1 and 2

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,35 @@
 function getProductList() {
-    return [
-        {name: 'JavaScript: podstawy', type: 'book', count: 3, price: 67.19, discount: 0.21},
-        {name: 'React: podstawy', type: 'book', count: 4, price: 79.17, discount: 0.27}
-    ]
+  return [
+    {
+      name: "JavaScript: podstawy",
+      type: "book",
+      count: 3,
+      price: 67.19,
+      discount: 0.21,
+    },
+    {
+      name: "React: podstawy",
+      type: "book",
+      count: 4,
+      price: 79.17,
+      discount: 0.27,
+    },
+  ];
 }
+
+const productList = getProductList();
+
+console.warn(productList);
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
-    });
+  return productList.reduce((acc, product) => {
+    console.warn(acc);
+    console.warn(product.price * product.count);
+    const promoDiscount = product.price * product.discount;
+    const total = acc + (product.price - promoDiscount) * product.count;
+    return total;
+  }, 0);
 }
 
-const totalPrice = getTotalPrice( getProductList() );
-console.log(totalPrice); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
+const totalPrice = getTotalPrice(getProductList());
+console.log(totalPrice.toFixed(2)); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
