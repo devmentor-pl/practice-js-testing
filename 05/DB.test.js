3119ed974f687cf2e30d21df888c94882690fead
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-04-29 12:36:45 +0200

    próba rozwiązania zadania

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,9 @@
-import DB from './DB';
+import DB from "./DB";
+
+it("when data id is a number insert to database  ", async () => {
+  const database = new DB();
+
+  const promise = await database.insert({ id: 1 });
+
+  expect(promise).toBe(["id: 1"]);
+});
