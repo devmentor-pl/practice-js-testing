88566c5ba4db6910df1f6da89afc9865a1c7de46
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-05-02 15:30:25 +0200

    zadanie 5

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,9 +1,66 @@
 import DB from "./DB";
 
-it("when data id is a number insert to database  ", async () => {
-  const database = new DB();
+describe("Insert", () => {
+  it("check if row number is equel 1", async () => {
+    const database = new DB();
+    await database.insert({ id: 1 });
+    const rows = await database.getRows();
+    expect(rows.length).toBe(1);
+  });
 
-  const promise = await database.insert({ id: 1 });
+  it("reject if ID is a number", async () => {
+    expect.assertions(1);
+    const database = new DB();
 
-  expect(promise).toBe(["id: 1"]);
+    try {
+      await database.insert({ id: "1", name: "Ariadna" });
+    } catch (err) {
+      expect(err).toBe("ID can be only number!");
+    }
+  });
+
+  it("reject if ID is duplicated", async () => {
+    expect.assertions(1);
+    const database = new DB();
+
+    try {
+      await database.insert({ id: 1, name: "Ariadna" });
+      await database.insert({ id: 1, name: "Kasia" });
+    } catch (err) {
+      expect(err).toBe("ID can't be duplicated!");
+    }
+  });
+
+  it("insert data is correct", async () => {
+    const database = new DB();
+
+    const data = { id: 1 };
+    const insertData = await database.insert({ id: 1 });
+
+    expect(insertData).toEqual(data);
+  });
+});
+
+describe("select", () => {
+  it("check if ID exists", async () => {
+    expect.assertions(1);
+    const database = new DB();
+
+    database.insert({ id: 1, name: "Ariadna" });
+    const rows = database.getRows();
+    const id = rows.id;
+    const selectID = await database.select(1);
+
+    expect(selectID).toBe(id);
+  });
+  it(" ID does not exists", async () => {
+    expect.assertions(1);
+    const database = new DB();
+
+    try {
+      await database.select(1);
+    } catch (err) {
+      expect(err).toBe("ID not found");
+    }
+  });
 });
