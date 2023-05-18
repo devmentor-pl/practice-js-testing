9c6c77f2662818df40d8f31e058b874966aca8c3
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-05-18 20:20:43 +0200

    refactor2

--- 03/app.js
+++ 03/app.js
@@ -1,9 +1,8 @@
-export default function randomNumber(min,max) {
+export default function randomNumber(min=0,max=0) {
     if(typeof min!=='number' || typeof max!=='number') {
         throw new Error('Property have to be a number');
-    } else  {
-        const result = Math.floor(Math.random() * (max - min + 1) + min);
-        return result;
     } 
+    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    return result;
 }
 
