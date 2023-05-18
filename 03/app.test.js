46ba2fe20ec80a556f19712b7c60c9852384d5bf
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-05-18 19:56:39 +0200

    test2

--- 03/app.test.js
+++ 03/app.test.js
@@ -5,3 +5,7 @@ it('should return 1 when range is <1,1>', ()=>{
     expect(result).toBe(1);
 })
 
+it('should throw when either min or max is not a number', ()=>{
+    const result = randomNumber('a','b');
+    expect(result).toThrow();
+})
