33811b6f8ec926c884ed87da3d779ff38d76a405
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-05-18 19:31:11 +0200

    test1

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
 import randomNumber from './app';
+
+it('should return 1 when range is <1,1>', ()=>{
+    const getRandomNumber = randomNumber();
+    const result = randomNumber(1,1)
+    expect(result).toBe(1)
+})
