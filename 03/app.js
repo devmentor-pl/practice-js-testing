6f5c3a28f7fa5508d63723e6b761bf518f11f4be
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-05-27 16:08:11 +0200

    Second test and implementation added

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 export default function randomNumber(min, max) {
+    if(!isNumber(min)) {
+        throwError('"min" is not a number')
+    }
+
     return Math.random() * (max - min) + min
+}
+
+function isNumber(value){
+    return typeof value === 'number' && !Number.isNaN(value)
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,8 @@ it('Should return 1 if min = 1 and max = 1', () => {
     expect(randomNumber(1,1)).toBe(1)
 })
 
+it('Should throw error if min is not a number', () => {
+    expect( () => {
+        randomNumber('1', 2).toThrow('"min" is not a number')
+    })
+})
