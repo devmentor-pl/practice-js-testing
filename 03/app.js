bbe7b4645c0bcf07efcd2536ec1e156d1bd27eed
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-05-27 16:38:58 +0200

    Finished 04

--- 03/app.js
+++ 03/app.js
@@ -3,9 +3,17 @@ export default function randomNumber(min, max) {
         throwError('"min" is not a number')
     }
 
+    if(!isNumber(max)) {
+        throwError('"max" is not a number')
+    }
+
     return Math.random() * (max - min) + min
 }
 
 function isNumber(value){
     return typeof value === 'number' && !Number.isNaN(value)
+}
+
+function throwError(message) {
+    throw new Error(message)
 }
--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,28 @@
 export default class User {
+    constructor(data){
+        const {email, password} = data
 
+        if(!email.includes('@')){
+            throw new Error('Email is incorrect')
+        }
+
+        if(password.length < 8){
+            throw new Error('Password is incorrect')
+        }
+
+        this.email = email
+        this.password = password
+    }
+
+    login() {
+        return this.email.includes('devmentor.pl')
+    }
+
+    getEmail(){
+        return this.email
+    }
+
+    getPassword(){
+        return this.password
+    }
 }
