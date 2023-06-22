1f89a91408f9db1cbab9fb6832a734964d736a50
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-06-22 11:11:21 +0200

    t3 test 3 green

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,9 @@
 export default function randomNumber(min = 0, max = 0) {
     _isTypeNumber(min)
     _isTypeNumber(max)
+    _isRangeRight(min, max)
+
+
     return Math.floor(Math.random() * (max - min)) + min
 
 }
@@ -8,5 +11,11 @@ export default function randomNumber(min = 0, max = 0) {
 function _isTypeNumber(value) {
     if (typeof value !== 'number') {
         throw new Error('Property is not a number')
+    }
+}
+
+function _isRangeRight(min, max) {
+    if (max < min) {
+        throw new Error("Maximum mustn't be higher then minimum")
     }
 }
