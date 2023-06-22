a08838822eacb7a947bd9ab90b164de3cc04f2c3
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-06-22 10:56:17 +0200

    t3 test 2 red

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,11 @@ import randomNumber from './app';
 test('min = 1 and max = 1 return 1', () => {
     expect(randomNumber(10, 10)).toBe(10)
 })
+
+test('Throw exception when min is not a number', () => {
+    function callRandomNumber() {
+        randomNumber('1')
+    }
+
+    expect(callRandomNumber).toThrow()
+})
