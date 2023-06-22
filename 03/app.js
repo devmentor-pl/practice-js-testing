d38c6c2a34b0b386744805aba96029a2305187d5
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-06-22 11:03:11 +0200

    t3 test 2 ref

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,11 @@
 export default function randomNumber(min = 0, max = 0) {
-    if (typeof min !== 'number') {
+    _isTypeNumber(min)
+    return Math.floor(Math.random() * (max - min)) + min
+
+}
+
+function _isTypeNumber(value) {
+    if (typeof value !== 'number') {
         throw new Error('Property is not a number')
     }
-    return Math.floor(Math.random() * (max - min)) + min
 }
