c231eb5ed24d7babd0878cd355e09a7bc6e16483
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-24 17:56:47 +0200

    Implement randomNumber function to pass the first test

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,5 @@
 export default function randomNumber(min, max) {
-
+  if (min === max) {
+    return min;
+  }
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,5 @@
 import randomNumber from "./app";
 
 test("when min and max are both 1, it should return 1", () => {
-  expect(randomNumber(1, 1).toBe(1));
+  expect(randomNumber(1, 1)).toBe(1);
 });
