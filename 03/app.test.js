982706c001998cd14102063871b04cb7ed2a1c5b
Marcin Soltysik <profittax@gmail.com>
2023-06-24 14:46:12 +0200

    Refactor function, all test passed

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,15 @@ import randomNumber from './app';
 it('return 1 if min=1 and max=1', () => {
   expect(randomNumber(1, 1)).toBe(1);
 });
+
+it('if min is not a number throw error', () => {
+  expect(() => randomNumber('1', 2)).toThrow('min is not a number');
+});
+
+it('if max is not a number throw error', () => {
+  expect(() => randomNumber(1, '2')).toThrow('max is not a number');
+});
+
+it('throws an error if the range is mutually exclusive', () => {
+  expect(() => randomNumber(4, 3)).toThrow('range is mutually exclusive');
+});
