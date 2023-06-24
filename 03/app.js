a1fba06d2f5038c1f9cbb620cbf7e56364071fa8
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-24 21:07:24 +0200

    Update randomNumber function to pass the second test

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,7 @@
 export default function randomNumber(min, max) {
+  if (typeof max !== "number") {
+    throw new TypeError(`Argument max must be a number`);
+  }
   if (typeof min !== "number") {
     throw new TypeError(`Argument min must be a number`);
   }
