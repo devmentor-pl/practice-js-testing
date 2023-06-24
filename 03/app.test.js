22d7874b239d65bba4dfc4a0e3aa49c66c40bb52
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-24 21:06:25 +0200

    Add test for randomNumber with non-number argument

--- 03/app.test.js
+++ 03/app.test.js
@@ -7,3 +7,7 @@ test("when min and max are both 1, it should return 1", () => {
 test("when non-number is passed should throw an error", () => {
   expect(() => randomNumber("x", 1)).toThrow();
 });
+
+test("when non-number is passed should throw an error", () => {
+  expect(() => randomNumber(1, "x")).toThrow();
+});
