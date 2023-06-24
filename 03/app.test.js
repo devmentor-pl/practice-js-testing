0ec0f0569e5220dc2d982d706a4be18a704beb30
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-24 17:59:11 +0200

    Add test for randomNumber with non-number argument

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from "./app";
 test("when min and max are both 1, it should return 1", () => {
   expect(randomNumber(1, 1)).toBe(1);
 });
+
+test("when non-number is passed should throw an error", () => {
+  expect(randomNumber("x", 1)).toThrow();
+});
