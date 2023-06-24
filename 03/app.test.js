524d354ab93c4d0ee5876ea49ce6ca662cdb7867
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-24 21:10:58 +0200

    add test for randomNumber where min is greater than max

--- 03/app.test.js
+++ 03/app.test.js
@@ -11,3 +11,7 @@ test("when non-number is passed should throw an error", () => {
 test("when non-number is passed should throw an error", () => {
   expect(() => randomNumber(1, "x")).toThrow();
 });
+
+test("should throw an error if min is greater than max", () => {
+  expect(() => randomNumber(5, 3)).toThrow();
+});
