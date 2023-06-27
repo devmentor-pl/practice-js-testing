b92f22d80b592807258868c7152e94f3b5357d89
Ireneusz Cislo <irekc86@o2.pl>
2023-06-27 06:43:40 +0200

    3test green task 03

--- 03/app.js
+++ 03/app.js
@@ -4,7 +4,9 @@ export default function randomNumber(min, max) {
         throw new Error('min is not a number')
     }
 
-   
+    if(typeof max !== 'number') {
+        throw new Error('max is not a number')
+    }
 
     return Math.random() * (max - min) + min
 }
