dcb38a16d1c5184ae4b9f22a2b9e0d45f59c4a03
Ireneusz Cislo <irekc86@o2.pl>
2023-06-27 06:24:17 +0200

    2test red task 03

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('min=1 and max=1 returns 1', () => {
     expect( randomNumber(1,1) ).toBe(1)
 })
+
+it('if min is NaN throw error', () => {
+    expect( randomNumber('a', 4).toThrow('min is not a number'))
+}) 
