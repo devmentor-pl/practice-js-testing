5e783e4a24a140f454bc5c6684803154f78f4264
Ireneusz Cislo <irekc86@o2.pl>
2023-06-27 07:36:34 +0200

    1test and 2test green task 04

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,21 @@
 export default class User {
+    constructor(object) {
+        const {email, password} = object;
 
+        if(!email.includes('@')) {
+            throw new Error()
+        }
+
+        this.email = email;
+        this.password = password
+        
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getPassword() {
+        return this.password
+    }
 }
