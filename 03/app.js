4c26c59db98b1a6e4293217f0b82efb64df469b2
kozerka <kozerka@gmail.com>
2023-06-29 12:38:47 +0200

    Task 03 finished

--- 03/app.js
+++ 03/app.js
@@ -1,9 +1,17 @@
 export default function randomNumber(min, max) {
-	if (typeof min !== 'number') {
-		throw new Error('min must be a number');
+	if (!isNumber(min)) {
+		throwError('min must be a number');
 	}
-	if (typeof max !== 'number') {
-		throw new Error('max must be a number');
+	if (!isNumber(max)) {
+		throwError('max must be a number');
 	}
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function throwError(msg) {
+	throw new Error(msg);
+}
+
+function isNumber(n) {
+	return typeof n === 'number' && !Number.isNaN(n);
 }
