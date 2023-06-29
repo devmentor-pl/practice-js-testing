5090a6a6f6e35fbbb8b64280a32e606aaa370729
kozerka <kozerka@gmail.com>
2023-06-29 12:19:37 +0200

    Red status - test 2

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('return 1 if min=w and max=1', () => {
 	expect(randomNumber(1, 1)).toBe(1);
 });
+
+it('throw error if min is not a number', () => {
+	expect(() => randomNumber('xyz', 1)).toThrow('min must be a number');
+});
