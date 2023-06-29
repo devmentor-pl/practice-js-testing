e793f2216bc4752a1a70f82b8e76ff1fa6817502
kozerka <kozerka@gmail.com>
2023-06-29 12:25:00 +0200

    Green status - test 3

--- 03/app.js
+++ 03/app.js
@@ -2,5 +2,8 @@ export default function randomNumber(min, max) {
 	if (typeof min !== 'number') {
 		throw new Error('min must be a number');
 	}
+	if (typeof max !== 'number') {
+		throw new Error('max must be a number');
+	}
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
