68519652b862c2bc9c588fa0e41c022d227b94b1
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-07-09 22:33:47 +0200

    Task 03: create fourth test

--- 03/app.test.js
+++ 03/app.test.js
@@ -5,7 +5,7 @@ it('returns 1 when random number is in the range from 1 to 1', () => {
     expect(result).toBe(1)
 })
 
-it('throw exception when first property is not a number', () => {
+it('throw exception when min property is not a number', () => {
     function getRandomNumber() {
         const result = randomNumber('1', 1)
     }
@@ -13,10 +13,17 @@ it('throw exception when first property is not a number', () => {
     expect(getRandomNumber).toThrow()
 })
 
-it('throw exception when second property is not a number', () => {
+it('throw exception when max property is not a number', () => {
     function getRandomNumber() {
         const result = randomNumber(1, '1')
     }
 
+    expect(getRandomNumber).toThrow()
+})
+
+it('throw exception when min property is greater then max property', () => {
+    function getRandomNumber() {
+        const result = randomNumber(3, 2)
+    }
     expect(getRandomNumber).toThrow()
 })
