e38541e8f212803b8532e9fdad54c4516afcb116
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-07-09 22:26:28 +0200

    Task 03: third implementation - test passed

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,9 @@ export default function randomNumber(min, max) {
     if(typeof min !== 'number'){
         throw new Error('Property has to be a number')
     }
+    if(typeof max !== 'number'){
+        throw new Error('Property has to be a number')
+    }
     const randomNumber = Math.random()* (max - min) + min 
     return randomNumber
 }
