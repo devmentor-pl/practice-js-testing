ead4171b3a25b221b9ff0c10ed7b6ce65b80ebf0
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-07-09 22:28:44 +0200

    Task 03: code refactoring

--- 03/app.js
+++ 03/app.js
@@ -1,10 +1,8 @@
 export default function randomNumber(min, max) {
-    if(typeof min !== 'number'){
+    if (typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('Property has to be a number')
-    }
-    if(typeof max !== 'number'){
-        throw new Error('Property has to be a number')
-    }
-    const randomNumber = Math.random()* (max - min) + min 
+    } 
+
+    const randomNumber = Math.random() * (max - min) + min
     return randomNumber
 }
