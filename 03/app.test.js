656f9eed59cf29dceec5eed3eef35eebf665901c
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-07-09 22:11:56 +0200

    Task 03: create second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,11 @@ it('returns 1 when random number is in the range from 1 to 1', () => {
     const result = randomNumber(1, 1)
     expect(result).toBe(1)
 })
+
+it('throw exception when first argument is not a number', () => {
+    function getRandomNumber() {
+        const result = randomNumber('1', 1)
+    }
+
+    expect(getRandomNumber).toThrow()
+})
