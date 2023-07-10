db228d363a8f19924e6e73b008b3c3d26ae599d5
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-07-10 13:44:49 +0200

    Task 04 - with one test failed

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,26 @@
 export default class User {
+    constructor({ email, password }) {
+        this.email = this.getEmail(email)
+        this.password = this.getPassword(password)
+    }
+
+    getEmail(email) {
+        if (!email.includes('@')) {
+            throw new Error('Email must include "@" sign')
+        } else return email
+    }
+
+    getPassword(password) {
+        if (password.length < 8) {
+            throw new Error('Password length must be greater than 8')
+        } else return password
+    }
+
+    login() {
+        if (!this.email.includes('devmentor.pl')) {
+            return false
+        } else return true
+    }
+
 
 }
