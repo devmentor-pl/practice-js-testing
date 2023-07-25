ec8d4e9e57dbaef3e2f3097877af414716431349
MarcinK <marcin.kulbicki@gmail.com>
2023-07-25 13:46:50 +0200

    add validation for type number

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,13 @@
 export default function randomNumber(min, max) {
+  validateNumbers(min, max);
+
   const areSameValues = min === max;
   if (areSameValues) return min;
+}
+
+function validateNumbers(...args) {
+  args.forEach((arg) => {
+    const isNumberType = typeof arg === "number" && !isNaN(arg);
+    if (!isNumberType) throw new Error("arg is not type number");
+  });
 }
