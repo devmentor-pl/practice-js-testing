230d7b0f4ae57d3a46486f25f3fa15d8ac68f566
MarcinK <marcin.kulbicki@gmail.com>
2023-07-25 13:43:29 +0200

    add test if args are number type

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,10 @@ describe("randomNumber", () => {
   it("returns argument value when two args have same value", () => {
     expect(randomNumber(1, 1)).toBe(1);
   });
+
+  it("throws expection when one of props is not of type number", () => {
+    const arg1 = "X";
+    const arg2 = 3;
+    expect(() => randomNumber(arg1, arg2)).toThrow("arg is not type number");
+  });
 });
