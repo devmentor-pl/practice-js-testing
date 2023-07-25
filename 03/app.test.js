d1f926bad33899d31b8c4df41400a641aa996372
MarcinK <marcin.kulbicki@gmail.com>
2023-07-25 13:55:17 +0200

    add test to check if min arg is in range

--- 03/app.test.js
+++ 03/app.test.js
@@ -8,6 +8,12 @@ describe("randomNumber", () => {
   it("throws expection when one of props is not of type number", () => {
     const arg1 = "X";
     const arg2 = 3;
-    expect(() => randomNumber(arg1, arg2)).toThrow("arg is not type number");
+    expect(() => randomNumber(arg1, arg2)).toThrow();
+  });
+
+  it("throws expection when [max] arg is small than [min] arg", () => {
+    const arg1 = 5;
+    const arg2 = 3;
+    expect(() => randomNumber(arg1, arg2)).toThrow();
   });
 });
