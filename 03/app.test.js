4ff3150d30790e9e2cacfc03514fbaf5e13452b4
MarcinK <marcin.kulbicki@gmail.com>
2023-07-25 13:24:33 +0200

    add test when same args value

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
-import randomNumber from './app';
+import randomNumber from "./app";
+
+describe("randomNumber", () => {
+  it("returns argument value when two args have same value", () => {
+    expect(randomNumber(1, 1)).toBe(1);
+  });
+});
