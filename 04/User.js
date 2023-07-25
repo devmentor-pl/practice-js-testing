787fa82e68e7ae4b87a2f83dcc8128e11584b408
MarcinK <marcin.kulbicki@gmail.com>
2023-07-25 15:08:16 +0200

    implement logicfor adding a new user with pass and email

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,15 @@
 export default class User {
+  constructor(user) {
+    const { email, password } = user;
+    this.email = email;
+    this.password = password;
+  }
 
+  getEmail() {
+    return this.email;
+  }
+
+  getPassword() {
+    return this.password;
+  }
 }
