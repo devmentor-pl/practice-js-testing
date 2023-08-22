c11d974b153b20814910f3bb77bbb85dec27aa63
Marcin <xcinek1337@wp.pl>
2023-08-22 11:36:13 +0200

    init

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,24 @@
 import DB from './DB';
+
+describe('DB insert func', () => {
+	it('func insert return correctly insert data', async () => {
+		const objectTest = {
+			say: 'hello',
+			id: 2,
+		};
+		const DBClass = new DB();
+		await DBClass.insert(objectTest);
+		const rows = await DBClass.getRows();
+		expect(rows).toMatchObject([objectTest]);
+	});
+	it('func insert return false when id is a string', async () => {
+		const objectTest = {
+			say: 'hello',
+			id: 'id'
+		};
+		const DBClass = new DB();
+		const result = await DBClass.insert(objectTest);
+
+		expect(result).toThrow();
+	});
+});
