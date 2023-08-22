8179729a830c3e5b252c78386ebddcec6d56bb18
Marcin <xcinek1337@wp.pl>
2023-08-22 10:07:07 +0200

    test 2

--- 04/User.js
+++ 04/User.js
@@ -1,12 +1,20 @@
 export default class User {
 	constructor(dataUser) {
-		this.email = dataUser.email;
-		this.password = dataUser.password;
+		if (this._validation(dataUser.email)) {
+			this.email = dataUser.email;
+			this.password = dataUser.password;
+		} else {
+			throw new Error('email nieprawidlowy');
+		}
 	}
 	getEmail() {
 		return this.email;
 	}
 	getPassword() {
 		return this.password;
+	}
+	_validation(email) {
+		const EmailRegex = /^[-\w\.]+@([-\w]+\.)+[a-z]+$/i;
+		return EmailRegex.test(email);
 	}
 }
