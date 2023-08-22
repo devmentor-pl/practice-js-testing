c463417624b62647b11ebe35fb6c4c8211596b92
Marcin <xcinek1337@wp.pl>
2023-08-22 09:00:00 +0200

    second test before implementation

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,9 @@ describe('RandomNumber', () => {
 	it('return error when func has no argumnets', () => {
 		expect(() => randomNumber()).toThrow();
 	});
+	it('return error when func has no number arguments', () => {
+		const ar1 = 'x';
+		const ar2 = 'yb';
+		expect(() => randomNumber(ar1, ar2)).toThrow();
+	});
 });
