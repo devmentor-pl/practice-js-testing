8f0e6141fdd9e95c5485f5fa5867fee00f358049
Marcin <xcinek1337@wp.pl>
2023-08-22 08:53:53 +0200

    first test passed

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,6 @@
 export default function randomNumber(min, max) {
-
+	if (min !== '' && max !== '') {
+		throw new Error('no arguments in func');
+	}
+	return Math.floor(Math.random() * (max - min) + min);
 }
