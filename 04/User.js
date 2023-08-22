8f55bbf43d8740c467cd0d64bd4622a2dd89aaec
Marcin <xcinek1337@wp.pl>
2023-08-22 09:58:44 +0200

    first commit

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,12 @@
 export default class User {
-
+	constructor(dataUser) {
+		this.email = dataUser.email;
+		this.password = dataUser.password;
+	}
+	getEmail() {
+		return this.email;
+	}
+	getPassword() {
+		return this.password;
+	}
 }
