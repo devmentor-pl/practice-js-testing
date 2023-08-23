a01518787f4edb81de689c8b8049cec02c691276
Marcin <xcinek1337@wp.pl>
2023-08-23 09:49:14 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,15 @@
 export default function randomNumber(min, max) {
-	if (isNaN(min) && isNaN(max)) {
-		throw new Error('no arguments in func');
+	if (min === undefined) {
+		throw new Error('function should have two arguments');
+	}
+	if (max === undefined) {
+		throw new Error('second arg is empty');
+	}
+	if (isNaN(min)) {
+		throw new Error('First argument is not a number');
+	}
+	if (isNaN(max)) {
+		throw new Error('Second argument is not a number');
 	} else if (min > max) {
 		throw new Error('min cannot be higher than max');
 	}
--- 03/app.test.js
+++ 03/app.test.js
@@ -2,21 +2,26 @@ import randomNumber from './app';
 
 describe('RandomNumber', () => {
 	it('return error when func has no argumnets', () => {
-		expect(() => randomNumber()).toThrow();
+		expect(() => randomNumber()).toThrowError('function should have two arguments');
 	});
-	it('return error when func has no number arguments', () => {
-		const ar1 = 'x';
-		const ar2 = 'yb';
-		expect(() => randomNumber(ar1, ar2)).toThrow();
+	it('return error when second arg is missing', () => {
+		expect(() => randomNumber(1)).toThrowError('second arg is empty');
 	});
+	it('checking if the first argument is a number', () => {
+		expect(() => randomNumber('k', 1)).toThrowError('First argument is not a number');
+	});
+	it('checking if the second argument is a number', () => {
+		expect(() => randomNumber('3', 'a')).toThrowError('Second argument is not a number');
+	});
+
 	it('return 1 when arguments are 1,1', () => {
 		const result = randomNumber(1, 1);
 		expect(result).toBe(1);
 	});
-	it('the range is excluded', () => {
-		expect(() => randomNumber(5, 1)).toThrow();
+	it('the range is excluded when first number is higher than second', () => {
+		expect(() => randomNumber(5, 1)).toThrowError('min cannot be higher than max');
 	});
-	it('its wokring', () => {
+	it('number is in the range', () => {
 		const result = randomNumber(1, 100);
 
 		expect(result).toBeLessThanOrEqual(100);
