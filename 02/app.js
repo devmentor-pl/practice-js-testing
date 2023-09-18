38265a4d55316a95e4ef0410ce58f2ccd6f3ad5c
anetasobanska <sannettka@gmail.com>
2023-09-18 14:22:38 +0200

    finish 02

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,43 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector('.error--click');
+  const enterEl = document.querySelector('.error--enter');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+  initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+  initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
-
-    if(error) {
-        throw error;
-    }
+  element.style.top = Math.random() * 600 + 'px';
+  element.style.left = Math.random() * 800 + 'px';
+  if (error) {
+    throw error;
+  }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  element.addEventListener(eventName, function () {
+    try {
+      setRandomPosition(this, error);
+    } catch (error) {
+      const err = document.querySelector('.alert');
+      const pEl = document.querySelector('.alert__message');
+      err.classList.remove('alert--hidden');
+      pEl.innerText = error.message;
+      closeErrMessage(pEl);
+    }
+  });
+}
+
+function closeErrMessage(el) {
+  const outsideEl = el.parentElement.parentElement;
+  outsideEl.addEventListener('click', function (e) {
+    if (e.target.classList.contains('alert')) {
+      outsideEl.classList.add('alert--hidden');
+    }
+  });
 }
