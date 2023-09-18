ae4aa409ed2bdaafef09a8586ea7f1a99382e223
anetasobanska <sannettka@gmail.com>
2023-09-18 16:08:45 +0200

    test throw error when one of arguments is not a number pass

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,6 @@
 export default function randomNumber(min, max) {
   if (min === 1 && max === 1) return 1;
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw new TypeError('The entered data must be numbers.');
+  }
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from './app';
 it('randomizes a number from 1 to 1', () => {
   expect(randomNumber(1, 1)).toBe(1);
 });
+
+it('throw error when one of arguments is not a number', () => {
+  expect(() => randomNumber('1', 1)).toThrow();
+});
