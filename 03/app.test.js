5d19b702c432d341e3d64dd6226c45ca1cc08be6
anetasobanska <sannettka@gmail.com>
2023-09-26 00:03:40 +0200

    fix 03

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,11 +4,15 @@ it('randomizes a number from 1 to 1', () => {
   expect(randomNumber(1, 1)).toBe(1);
 });
 
-it('throw error when one of arguments is not a number', () => {
-  expect(() => randomNumber('1', 1)).toThrow();
+it('throw error when type of arguments is not a number', () => {
+  expect(() => randomNumber('1b', 1)).toThrow(
+    'The entered data must be numbers.'
+  );
 });
 it('throw error when min number is bigger than max number', () => {
-  expect(() => randomNumber(2, 1)).toThrow();
+  expect(() => randomNumber(2, 1)).toThrow(
+    "Min number can't be greater than max number."
+  );
 });
 it('return a number from a specified range', () => {
   const min = 5;
