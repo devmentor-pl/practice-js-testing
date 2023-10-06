18444c4686de73bb3de0c4c5ccc9e0fc069283b6
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-10-06 20:57:13 +0200

    check if one of the value of arguments is not a number - test

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,8 @@ export default function randomNumber(min, max) {
     if (min === max) {
         return min;
     }
+   
     return Math.floor(Math.random() * (max - min) + 1)
 }
+
 
--- 03/app.test.js
+++ 03/app.test.js
@@ -3,6 +3,12 @@ import randomNumber from './app';
 it('return 1 when min and max is equal 1', () => {
    expect(randomNumber(1, 1)).toBe(1)
 })
+it('throw exception when one of the arguments is not a number ', () => {
+   expect(() => {
+      randomNumber('2', 2).toThrow()
+   })
+})
+
 
 
 
