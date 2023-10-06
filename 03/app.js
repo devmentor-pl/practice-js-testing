3a8509c367ec42cc28b692e4681f83bd317d8c1c
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-10-06 20:58:45 +0200

    add if isNaN && throw Error to randomNumber

--- 03/app.js
+++ 03/app.js
@@ -2,7 +2,9 @@ export default function randomNumber(min, max) {
     if (min === max) {
         return min;
     }
-   
+    if (isNaN(min) || isNaN(max)) {
+        throw new Error('Property have to be number');
+    }
     return Math.floor(Math.random() * (max - min) + 1)
 }
 
