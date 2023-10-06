f1d3f58e3c8b2dfbd8a758a856120d0c7e118358
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-10-06 21:16:52 +0200

    add to randomNumber if min > max === true throw Error

--- 03/app.js
+++ 03/app.js
@@ -5,5 +5,8 @@ export default function randomNumber(min, max) {
     if (isNaN(min) || isNaN(max)) {
         throw new Error('Property have to be number');
     }
+    if (min > max) {
+        throw new Error('Wrong range');
+    }
     return Math.floor(Math.random() * (max - min) + 1)
 }
