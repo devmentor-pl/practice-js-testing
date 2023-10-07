fb6301f42ffd938a3f50f6ae7203567062eaa985
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-10-07 12:26:59 +0200

    task 4 done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,38 @@
 export default class User {
+   constructor({ email, password }) {
+     if (!this.isEmailValid(email)) {
+          throw new Error('email is incorrect!')
+     }
+     if (!this.isPassValid(password)) {
+          throw new Error ('password must include at least one letter')
+     } 
+     if (!this.isPassLengthCorrect(password)) {
+        throw new Error ('password must be longer or equal to 6')
+     }
+        this.email = email;
+        this.password = password;
+   }
+   login() {
+     if (this.email.includes('devmentor.pl')) {
+          return true
+     }
+     return false
+   }
 
-}
+   isPassValid(password) {
+     return password.match(/[a-zA-Z]/) && password.length > 3;
+   }
+   isPassLengthCorrect(password) {
+    return password.length >= 6
+   }
+   isEmailValid(email) {
+     return email.includes('@') && email.includes('.');
+   }
+   getEmail() {
+    return this.email;
+   }
+   getPassword() {
+    return this.password;
+   }
+  
+ }
