7b89180957ef4b1f262f253ee552c62bf4f1cc9c
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-10-10 22:22:22 +0200

    task 05 not finshed

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,21 +1,23 @@
 import randomNumber from './app';
+describe('randomNumber', () => {
+   it('return 1 when min and max is equal 1', () => {
+      expect(randomNumber(1, 1)).toBe(1)
+   })
+   it('throw exception when one of the arguments is not a number ', () => {
+      expect(() => {
+         randomNumber('2', 2).toThrow('Property have to be number')
+      })
+   })
+   it('throw exception when min > max', () => {
+      expect(() => {
+         randomNumber(4, 3).toThrow('Wrong range')
+      })
+   })
+   it('return valid number from range', () => {
+      const generatedNumber = randomNumber(1, 10);
+      expect(generatedNumber).toBeGreaterThanOrEqual(1);
+      expect(generatedNumber).toBeLessThanOrEqual(10);
+   })   
+})
 
-it('return 1 when min and max is equal 1', () => {
-   expect(randomNumber(1, 1)).toBe(1)
-})
-it('throw exception when one of the arguments is not a number ', () => {
-   expect(() => {
-      randomNumber('2', 2).toThrow()
-   })
-})
-it('throw exception when min > max', () => {
-   expect(() => {
-      randomNumber(4, 3).toThrow()
-   })
-})
-it('return valid number from range', () => {
-   const generatedNumber = randomNumber(1, 10);
-   expect(generatedNumber).toBeGreaterThanOrEqual(1);
-   expect(generatedNumber).toBeLessThanOrEqual(10);
-})   
 
--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,3 +1,83 @@
 import DB from './DB';
 
+let db 
+
+beforeEach(() => {
+     db =  new DB();
+})
+describe('DB class', () => {
+   describe('insert', async () => {
+      it('inserts data correctly', async () =>{
+         const data = {id: 1, name: 'John'};
+         const result = await db.insert(data);
+         expect(result.id).toBe(1);
+         expect(result.name).toBe('John')
+      })
+      it('show error when id is not a number', async () => {
+            try{
+               await db.insert({id: 'xyx'})
+            }
+            catch(e) {
+               expect(e).toBe('ID can be only number!')
+            }
+      })
+      it('show error when id is duplicated', async () => {
+         expect.assertions(1)
+         await db.insert({id: 1, name: 'John'})
+   
+         try {
+            await db.insert({id: 1, name: 'Kevin'})
+         }
+         catch (e) {
+            expect(e).toBe('ID can\'t be duplicated!')
+         }
+      })
+      it('insert data to DB and generates id if not provided', async () => {
+         const data = {name: 'Joe'};
+         const result = await db.insert(data);
+         expect(result).toHaveProperty('id');
+         expect(result.name).toEqual(data.name)
+      })
+   })
+  describe('select', async () => {
+      it('select data by id', async () => {
+         const result = await db.insert({id: 1, name: 'John'});
+
+         const selectData = await db.select(1);
+         expect(selectData).toEqual(result)
+      });
+      it('show error when id not exist', async () => {
+         expect.assertions(1)
+         const id = 123;
+         try {
+            await db.select(id)
+         }
+         catch (e) {
+            expect(e).toBe('ID not found')
+         }
+      })
+  })
+  describe('remove', async () => {
+    it('reject to remove not existing id', async () => {
+      const id = 123;
+      try {
+         await db.remove(id)
+      }
+      catch (e) {
+         expect(e).toBe('Item not exist!')
+      }
+    })
+    it('remove item from db when id exist', async () => {
+      const data = {name: 'Joe', id: 1};
+      await db.insert(data);
+      await db.remove(1);
+    })
+  })
+//   describe('update', async () => {
+//      it('reject update data without id', async () => {
+      
+//      })
+//   })
+})
+
 
