cb257ae7819c3dff45367608025d04c0afe5b8e9
Maciej Necka <macieknecka@gmail.com>
2023-10-30 14:00:02 +0100

    Task done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,30 @@
 export default class User {
+  constructor({ email, password }) {
+    if (!this.validateEmail(email) || !this.validatePassword(password)) {
+      throw new Error('Incorrect email or password');
+    }
+    this.email = email;
+    this.password = password;
+  }
 
+  getEmail() {
+    return this.email;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  login() {
+    return this.email.includes('devmentor.pl');
+  }
+
+  validateEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  }
+
+  validatePassword(password) {
+    return password.length >= 6;
+  }
 }
