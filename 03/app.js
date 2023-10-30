410d58ac492108ef56a1dcb6d6a6e7c921ab9a9b
Maciej Necka <macieknecka@gmail.com>
2023-10-30 13:33:34 +0100

    Third implementation

--- 03/app.js
+++ 03/app.js
@@ -2,5 +2,8 @@ export default function randomNumber(min, max) {
   if (typeof min !== 'number' || typeof max !== 'number') {
     throw new Error('Arguments must be a number');
   }
+  if (min > max) {
+    throw new Error('Minimum value cannot be greater than maximum value');
+  }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
