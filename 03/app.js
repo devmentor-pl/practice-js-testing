932959bb387dda855ee3d170ddc1be27ee0752ea
Maciej Necka <macieknecka@gmail.com>
2023-10-30 11:59:18 +0100

    First test in app.test.js

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,1 @@
-export default function randomNumber(min, max) {
-
-}
+export default function randomNumber(min, max) {}
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,6 @@
 import randomNumber from './app';
+
+it('Return 1 when min and max is 1', () => {
+  const calc = randomNumber(1, 1);
+  expect(calc).toBe(1);
+});
