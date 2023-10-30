dbab39629e91550ebcfdd233fe6d578b89eeca1f
Maciej Necka <macieknecka@gmail.com>
2023-10-30 13:00:59 +0100

    Second implementation

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,6 @@
 export default function randomNumber(min, max) {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw new Error('Arguments must be a number');
+  }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
