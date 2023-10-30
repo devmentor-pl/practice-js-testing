c8cb2e97acb8bf185136b31c6af59e8a3daa74ab
Maciej Necka <macieknecka@gmail.com>
2023-10-30 13:00:35 +0100

    Second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,6 +1,11 @@
 import randomNumber from './app';
 
 it('Return 1 when min and max is 1', () => {
-  const calc = randomNumber(1, 1);
-  expect(calc).toBe(1);
+  expect(randomNumber(1, 1)).toBe(1);
+});
+
+it('Throw an error when a non-number is provided', () => {
+  expect(() => {
+    randomNumber('X', 1).toThrow();
+  });
 });
