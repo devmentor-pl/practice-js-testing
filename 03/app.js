235c1f2ce179d9ef772647ab931eb2e0cb1af9cb
Maciej Necka <macieknecka@gmail.com>
2023-11-06 22:09:18 +0100

    03 - Fixed

--- 03/app.js
+++ 03/app.js
@@ -1,8 +1,12 @@
 export default function randomNumber(min, max) {
-  if (typeof min !== 'number' || typeof max !== 'number' || min > max) {
-    throw new Error(
-      'Invalid input. Min and max values must be numbers, and max have to be greater than min'
-    );
+  if (typeof min !== 'number') {
+    throw new Error('Invalid input. Min value must be a number.');
+  }
+  if (typeof max !== 'number') {
+    throw new Error('Invalid input. Max value must be a number.');
+  }
+  if (min > max) {
+    throw new Error('Invalid input. Max value must be greater than min value.');
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
