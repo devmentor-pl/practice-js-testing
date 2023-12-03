dd321633e7b1d7c655b80d599076472811b10490
Kaczupinko <kaczupinko@gmail.com>
2023-12-03 14:50:45 +0000

    Add second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,8 @@ describe('randomNumber', () => {
     test('returns 1 when the range is from 1 to 1', () => {
         expect(randomNumber(1, 1)).toBe(1);
     });
+
+    test('throws error when arguments are not numbers', () => {{
+        expect(() => randomNumber('a', 'b')).toThrow();
+    }});
 })
