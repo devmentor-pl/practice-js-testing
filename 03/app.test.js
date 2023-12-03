356b7d32e40bda1312cc393a839a12d467d89390
Kaczupinko <kaczupinko@gmail.com>
2023-12-03 14:46:21 +0000

    Add 1 test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
 import randomNumber from './app';
+
+describe('randomNumber', () => {
+    test('returns 1 when the range is from 1 to 1', () => {
+        expect(randomNumber(1, 1)).toBe(1);
+    });
+})
