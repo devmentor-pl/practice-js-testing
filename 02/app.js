afb4b2c813360f0850ec781bd8df3f3be92c379c
Kaczupinko <kaczupinko@gmail.com>
2023-12-03 17:23:10 +0000

    Finish 02 task

--- 02/app.js
+++ 02/app.js
@@ -12,7 +12,7 @@ function init() {
 
 }
 
-function setRandomPosition(element, error = null) {
+function setRandomPosition(element) {
     try {
         element.style.top = Math.random() * 600 + 'px';
         element.style.left = Math.random() * 800 + 'px';
@@ -23,6 +23,24 @@ function setRandomPosition(element, error = null) {
 
 function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+        try {
+            setRandomPosition(this);
+            throw error;
+        } catch (caughtError) {
+            displayError(caughtError);
+        }
+    });
 }
+
+function displayError(error) {
+    const alertEl = document.querySelector('.alert');
+    const alertMessageEl = document.querySelector('.alert__message');
+    alertMessageEl.textContent = error.message;
+    alertEl.classList.remove('alert--hidden');
+}
+
+document.querySelector('.alert').addEventListener('click', function(event) {
+    if (event.target === this) {
+        this.classList.add('alert--hidden');
+    }
+});
