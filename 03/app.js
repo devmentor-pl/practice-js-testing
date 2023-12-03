7cfffaf414ff17c498751293c05cd16327913d09
Kaczupinko <kaczupinko@gmail.com>
2023-12-03 14:51:24 +0000

    Implement the function to pass second test

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,8 @@
 export default function randomNumber(min, max) {
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('Arguments must be numbers');
+    }
+    
     return 1;
+
 }
