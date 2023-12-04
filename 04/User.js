e66d50afee2019bfb783165552ec38faa5c1ee3b
Kaczupinko <kaczupinko@gmail.com>
2023-12-04 07:08:13 +0000

    Finish 04 task

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,34 @@
 export default class User {
+    constructor({ email, password}) {
+        if(!this.validateEmail(email)) {
+            throw new Error('incorrect email');
+        }
+        if (!this.validatePassword(password)) {
+            throw new Error('Incorrect password');
+        }
 
+        this.email = email;
+        this.password = password;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        //metoda pomocnicza w celach testowych
+        return this.password;
+    }
+
+    login() {
+        return this.email.includes('@devmentor.pl');
+    }
+
+    validateEmail(email) {
+        return email.includes('@');
+    }
+
+    validatePassword(password) {
+        return password.length >= 6;
+    }
 }
