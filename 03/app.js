716541cb474f166c0df3f0453d0c9672c73415aa
Piotr Marczykowski <piotr.marczykowski94@gmail.com>
2023-12-12 20:53:26 +0100

    add task

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,7 @@
 export default function randomNumber(min, max) {
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('Arguments must be numbers');
+    }
 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
