4852467e35761844309d0dd9a6a2c0677a04b143
Piotr Marczykowski <piotr.marczykowski94@gmail.com>
2023-12-27 20:20:24 +0100

    add task 5

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,57 @@
 import DB from './DB';
+
+describe('insert', () => {
+    test('data is correct', async () => {
+        expect.assertions(2);
+        const db = new DB()
+        const rowsBefore = await db.getRows()
+        expect(rowsBefore.length).toBe(0);
+        db.insert({
+            name: 'Piotr'
+        })
+        const rowsAfter = await db.getRows()
+        expect(rowsAfter.length).toBe(1);
+    });
+
+    test('id is not a number', async () => {
+        expect.assertions(1);
+
+        const db = new DB()
+
+
+        try {
+            await db.insert({
+                name: 'Piotr',
+                id: 'wgawwrg'
+            })
+        } catch (e) {
+            expect(e).toMatch("ID can be only number")
+        }
+    })
+});
+
+describe('remove()', () => {
+    test('item not found', async () => {
+        expect.assertions(1);
+        const removeEl = new DB();
+        const id = 1;
+        try {
+            await removeEl.remove(id)
+
+        } catch (e) {
+            expect(e).toMatch('Item not exist!')
+        }
+
+    });
+});
+
+describe('update()', () => {
+    test('ID is not set', async () => {
+        expect.assertions(1);
+        const db = new DB();
+        const data = {
+            name: "Piotr"
+        };
+        await db.update(data).catch(e => expect(e).toBe('ID have to be set!'));
+    });
+});  
