a3b33c000271e1640901a8c939b9eb285e0f3bb5
Aleksy Zydlewski <limeakiwano33@gmail.com>
2024-01-02 17:59:43 +0100

    03 tmp 3

--- 03/app.js
+++ 03/app.js
@@ -4,5 +4,8 @@ export default function randomNumber(min, max) {
  if(typeof min !== 'number' || typeof max !== 'number') {
     throw new Error('Both min and max must be numbers.');
 }
+if( min > max){
+    throw new Error('Min must be smaller or equal to max')
+}
 
 }
