1f1b2105a6cfa88e00ee3d7e754499f35f805408
Artur Matysiak <inz.matys@gmail.com>
2024-01-05 15:43:24 +0100

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,35 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector(".error--click");
+  const enterEl = document.querySelector(".error--enter");
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+  initEventWithError(clickEl, "click", new RangeError("Błąd zakresu!"));
+  initEventWithError(enterEl, "mouseenter", new TypeError("Błąd typu!"));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
-
-    if(error) {
-        throw error;
-    }
+  let err = error;
+  try {
+    element.style.top = Math.random() * 600 + "px";
+    element.style.left = Math.random() * 800 + "px";
+  } catch {
+    myAlert(err);
+  }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  element.addEventListener(eventName, function () {
+    setRandomPosition(null, error);
+  });
 }
+
+//-----
+
+myAlert = (err) => {
+  document.querySelector(".alert--hidden").style.display = "flex";
+  document.querySelector(".alert__message").textContent = err;
+};
