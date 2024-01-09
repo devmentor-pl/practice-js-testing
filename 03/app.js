0701281c07f65b80b90b354a05774e5d4a5a570a
Kasia <k.kasia912@gmail.com>
2024-01-09 17:43:05 +0100

    third test pass

--- 03/app.js
+++ 03/app.js
@@ -2,7 +2,9 @@ export default function randomNumber(min, max) {
   if (typeof max !== 'number') {
     throw new Error('Property have to be a number');
   }
-
+  if (min > max) {
+    throw new Error('min number is grater than max');
+  }
   const randomNum = Math.floor(Math.random() * (max - min)) + 1;
 
   return randomNum;
