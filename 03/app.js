763d6c7d548f3122a2c8e02bae512dd5f7327881
Kasia <k.kasia912@gmail.com>
2024-01-09 18:38:52 +0100

    finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,12 +1,19 @@
 export default function randomNumber(min, max) {
-  if (typeof max !== 'number') {
-    throw new Error('Property have to be a number');
-  }
-  if (min > max) {
-    throw new Error('min number is grater than max');
-  }
-
+  checkIfNumber(max);
+  checkScope(min, max);
   const randomNum = Math.floor(Math.random() * (max - min)) + 1;
 
   return randomNum;
+}
+
+function checkIfNumber(max) {
+  if (typeof max !== 'number') {
+    throw new Error('Property have to be a number');
+  }
+}
+
+function checkScope(min, max) {
+  if (min > max) {
+    throw new Error('min number is grater than max');
+  }
 }
