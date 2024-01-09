b98fd74da624e0b7b0af227cde09d2ee670933b7
Kasia <k.kasia912@gmail.com>
2024-01-09 17:10:24 +0100

    add second test

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,10 @@ it('return 1 when min and max is equal to 1', () => {
   const result = randomNumber(1, 1);
   expect(result).toBe(1);
 });
+
+it('throw exception when prop max is not a number', () => {
+  function createRandomNum() {
+    randomNumber(1, 'x');
+  }
+  expect(createRandomNum).toThrow('Property have to be a number');
+});
