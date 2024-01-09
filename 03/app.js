dce31c27e642fb9a8249336162ed3d9e420bf035
Artur Matysiak <inz.matys@gmail.com>
2024-01-09 13:16:06 +0100

    add test -check type number-

--- 03/app.js
+++ 03/app.js
@@ -1,10 +1,7 @@
 export default function randomNumber(min, max) {
-  // console.log(min, max);
-  const result = Math.floor(Math.random() * (max - min));
+  // console.log(typeof min, typeof max);
+  if ((typeof min !== "number" || typeof max) !== "number") return console.log("Podałeś dane typu 'string'");
+  const result = Math.floor(Math.random() * (max - min) + min);
   if (min === 1 && max === 1) return result + 1;
   return result;
 }
-
-// for (let i = 20; i < 20; i++) {
-//   console.log(randomNumber(1, 10));
-// }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,32 +1,26 @@
 import randomNumber from "./app";
 let x;
 
-test("sprawdzenie czy jest wynik z podanego przedziału", () => {
-  x = randomNumber(1, 10);
-  console.log(x);
-  const checkRange = (x) => {
-    if (x >= 0 && x <= 10) return true;
-  };
-  expect(true).toBe(Boolean(checkRange(x)));
-});
-
-test("sprawdzenie danych wejściowych: 1,1", () => {
-  x = randomNumber(1, 1);
-  console.log(x);
-  const checkOnes = (x) => {
-    if (x === 1) return true;
-  };
-  expect(true).toBe(Boolean(checkOnes(x)));
-});
-
-// test("sprawdzenie czy podano typ number", () => {
-//   x = randomNumber("4", "p");
-//   console.log(x);
-//   const isNumber = (x) => {
-//     if (typeof x === "number") return true;
+// test("sprawdzenie czy jest wynik z podanego przedziału", () => {
+//   x = randomNumber(1, 10);
+//   const checkRange = (x) => {
+//     if (x >= 0 && x <= 10) return true;
 //   };
-//   expect(true).toBe(Boolean(isNumber(x)));
+//   expect(true).toBe(Boolean(checkRange(x)));
 // });
+
+// test("sprawdzenie danych wejściowych: 1,1", () => {
+//   x = randomNumber(1, 1);
+//   const checkOnes = (x) => {
+//     if (x === 1) return true;
+//   };
+//   expect(true).toBe(Boolean(checkOnes(x)));
+// });
+
+test("sprawdzenie czy podano typ number", () => {
+  x = randomNumber(20, 30);
+  expect(true).toBe(Boolean(x));
+});
 
 /*-----------------
 
