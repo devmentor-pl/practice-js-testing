16d67f30c5475d26bc522141a15c2d97db5f359e
Kasia <k.kasia912@gmail.com>
2024-01-12 11:05:03 +0100

    correct tests in task 03

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,17 @@
 export default function randomNumber(min, max) {
-  checkIfNumber(max);
+  checkIfNumber(min, max);
   checkScope(min, max);
   const randomNum = Math.floor(Math.random() * (max - min)) + 1;
 
   return randomNum;
 }
 
-function checkIfNumber(max) {
+function checkIfNumber(min, max) {
+  if (typeof min !== 'number') {
+    throw new Error('Property min have to be a number');
+  }
   if (typeof max !== 'number') {
-    throw new Error('Property have to be a number');
+    throw new Error('Property max have to be a number');
   }
 }
 
--- 03/app.test.js
+++ 03/app.test.js
@@ -5,11 +5,18 @@ it('return 1 when min and max is equal to 1', () => {
   expect(result).toBe(1);
 });
 
+it('throw exception when prop min is not a number', () => {
+  function createRandomNum() {
+    randomNumber('x', 1);
+  }
+  expect(createRandomNum).toThrow('Property min have to be a number');
+});
+
 it('throw exception when prop max is not a number', () => {
   function createRandomNum() {
     randomNumber(1, 'x');
   }
-  expect(createRandomNum).toThrow('Property have to be a number');
+  expect(createRandomNum).toThrow('Property max have to be a number');
 });
 
 it('throw exception when prop min is greater than max', () => {
