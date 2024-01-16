118a7c4255cd6b8153592cc4448bb30e2a13b17a
Artur Matysiak <inz.matys@gmail.com>
2024-01-16 18:57:40 +0100

    fix all test's

--- 03/app.js
+++ 03/app.js
@@ -1,8 +1,18 @@
 export default function randomNumber(min, max) {
-  // console.log(typeof min, typeof max);
-  if (min > max) return console.log("źle podałeś zakres liczb!");
-  if ((typeof min !== "number" || typeof max) !== "number") return console.log("Podałeś dane typu 'string'");
-  const result = Math.floor(Math.random() * (max - min) + min);
-  if (min === 1 && max === 1) return result + 1;
+  // console.log("min to: " + typeof min, "max to: " + typeof max);
+  let result = Math.floor(Math.random() * (max - min) + min);
+
+  // czy min > max
+  if (min > max) return false;
+
+  // czy podano typ number
+  if ((typeof min !== "number" || typeof max) !== "number") return false;
+
+  // czy dla (1,1) otrzymamy 1
+  if (min === 1 && max === 1) return result;
+
+  // czy wynik jest z podanego przedziału
+  if (result < min || result > max) return false;
+
   return result;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,44 +1,25 @@
 import randomNumber from "./app";
-let x;
 
-// test("sprawdzenie czy jest WYNIK Z podanego PRZEDZIAŁU", () => {
-//   x = randomNumber(1, 10);
-//   const checkRange = (x) => {
-//     if (x >= 0 && x <= 10) return true;
-//   };
-//   expect(true).toBe(Boolean(checkRange(x)));
-// });
+describe("My first TEST! - test randomNumber() function", () => {
+  test("sprawdzenie czy podano PRAWIDLOWY ZAKRES", () => {
+    expect(randomNumber(10, 30)).toBeTruthy();
+  });
 
-// test("sprawdzenie danych wejściowych: 1,1", () => {
-//   x = randomNumber(1, 1);
-//   const checkOnes = (x) => {
-//     if (x === 1) return true;
-//   };
-//   expect(true).toBe(Boolean(checkOnes(x)));
-// });
+  test("sprawdzenie danych wejściowych: (1, 1)", () => {
+    expect(randomNumber(1, 1)).toBe(1);
+  });
 
-// test("sprawdzenie czy podano TYP NUMBER", () => {
-//   x = randomNumber(20, 30);
-//   expect(true).toBe(Boolean(x));
-// });
+  test("sprawdzenie czy podano TYP NUMBER", () => {
+    expect(randomNumber(20, 40)).toBeTruthy();
+  });
 
-test("sprawdzenie czy podano PRAWIDLOWY ZAKRES", () => {
-  x = randomNumber(20, 30);
-  expect(true).toBe(Boolean(x));
+  test("sprawdzenie czy jest WYNIK Z podanego PRZEDZIAŁU", () => {
+    expect(randomNumber(1, 10)).not.toBe(false);
+  });
 });
 
-/*-----------------
-
-min. 3 testy!
-Funkcja ma generować losową liczbę z podanego przez usera przedziału
-
-input: 
--2 argumenty: min number, max number
-
-output:
--zwracać 1 liczbę całkowitą
-
-testy:
-- jeśli przedział odwrotny (min=5, max=2) to zwróć błąd
-
-*/
+// test("sprawdzenie czy podano 2 LICZBY", () => {
+//   a = 10;
+//   b = 30;
+//   expect(randomNumber(a, b)).toBe(true);
+// });
