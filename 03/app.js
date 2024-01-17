045a336ff86b16e3169e7147ca693496885d1d47
Artur Matysiak <inz.matys@gmail.com>
2024-01-17 12:39:23 +0100

    last fix

--- 03/app.js
+++ 03/app.js
@@ -1,12 +1,13 @@
 export default function randomNumber(min, max) {
-  // console.log("min to: " + typeof min, "max to: " + typeof max);
-  let result = Math.floor(Math.random() * (max - min) + min);
+  // czy podano typ number
+  const x = typeof min === "number";
+  const y = typeof max === "number";
+  if ((x || y) !== "number") console.log("błąd!!!");
+
+  const result = Math.floor(Math.random() * (max - min) + min);
 
   // czy min > max
   if (min > max) return false;
-
-  // czy podano typ number
-  if ((typeof min !== "number" || typeof max) !== "number") return false;
 
   // czy dla (1,1) otrzymamy 1
   if (min === 1 && max === 1) return result;
