bd8d0cca3b0856dd25c9def3a77b4c2a380a2495
Dawidtk <dawidtk@gmail.com>
2024-01-27 17:20:58 +0100

    test2

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,11 @@
 import DB from './DB';
+
+describe('.insert()', () => {
+    it('reject when id is not a number', async () => {
+        const db = new DB
+        try {
+            await db.insert({id: 'b', name: 'Diego Rodrigues'})
+        } catch(err) {
+            expect(err).toBe('ID can be only number!')
+        }
+    })})
