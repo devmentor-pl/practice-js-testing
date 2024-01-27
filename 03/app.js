bda6809d22c8e04e82d82534b062cd72b95fc540
Dawidtk <dawidtk@gmail.com>
2024-01-27 12:00:08 +0100

    t5

--- 03/app.js
+++ 03/app.js
@@ -1,13 +1,24 @@
 export default function randomNumber(min, max) {
-    if (typeof min !== 'number' || typeof max !== 'number') {
-        throw new Error('Podane argumenty nie są liczbami');
-      }
-    
-      if (min > max) {
-        throw new Error('Przedział jest nieprawidłowy');
-      }
-    
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+	validateNumbers(min, max);
 
-    
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function validateNumbers(min, max) {
+	if (min === max) {
+		return max;
+	}
+
+	if (isNaN(min) === true) {
+		throw new Error("Min has to be a number");
+	}
+
+	if (isNaN(max) === true) {
+		throw new Error("Max has to be a number");
+	}
+
+	if (min > max) {
+		throw new Error("Value of min has to be smaller than value of max");
+	}
+}
+
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,10 +1,10 @@
 import randomNumber from './app';
 
-test('Losowanie z przedziału od 1 do 1', () => {
-    expect(randomNumber(1, 1)).toBe(1);
-  });
-  
-
-  test('Przedział się wyklucza', () => {
-    expect(() => randomNumber(4, 3)).toThrow('Przedział jest nieprawidłowy');
-  });
+describe("randomNumber()", () => {
+	it("return value of argument if arguments are the same", () => {
+		// const max = 1;
+		// const min = 1;
+		const result = randomNumber(1, 1);
+		expect(result).toBe(1);
+	});
+})
