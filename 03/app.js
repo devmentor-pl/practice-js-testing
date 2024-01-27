1517ffe7c5b8dcc6ceea51aa5acea4ea6594a842
Dawidtk <dawidtk@gmail.com>
2024-01-27 11:30:24 +0100

    testNr3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 export default function randomNumber(min, max) {
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('Podane argumenty nie są liczbami');
+      }
+    
+      if (min > max) {
+        throw new Error('Przedział jest nieprawidłowy');
+      }
+    
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
-}
+    
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,13 @@
 import randomNumber from './app';
+
+test('Losowanie z przedziału od 1 do 1', () => {
+    expect(randomNumber(1, 1)).toBe(1);
+  });
+  
+  test('Podanie "nie liczby" jako argumentu', () => {
+    expect(() => randomNumber('abc', 10)).toThrow('Podane argumenty nie są liczbami');
+  });
+  
+  test('Przedział się wyklucza', () => {
+    expect(() => randomNumber(4, 3)).toThrow('Przedział jest nieprawidłowy');
+  });
