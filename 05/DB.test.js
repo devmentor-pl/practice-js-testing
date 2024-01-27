aeef71d4362fc8423d77b7076367df8ac40f2db8
Dawidtk <dawidtk@gmail.com>
2024-01-27 17:39:52 +0100

    test3

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,11 +1,28 @@
 import DB from './DB';
 
-describe('.insert()', () => {
-    it('reject when id is not a number', async () => {
-        const db = new DB
-        try {
-            await db.insert({id: 'b', name: 'Diego Rodrigues'})
-        } catch(err) {
-            expect(err).toBe('ID can be only number!')
-        }
-    })})
+describe(".insert()", () => {
+	it("should return 2 when one item is inserted", () => {
+		expect.assertions(1);
+		const db = new DB();
+		const promise = db.insert({ a: 1, b: 2 });
+
+		return promise
+			.then(() => db.getRows())
+			.then(dbRows => expect(dbRows.length).toBe(2));
+	});
+
+	it("should not add data when id is not a number", () => {
+		const db = new DB();
+		const promise = db.insert({ a: 3, b: 4, id: "string" });
+
+		return expect(promise).rejects.toMatch("ID can be only number!");
+	});
+
+	it("should not add data when id is duplicated", () => {
+		const db = new DB();
+		const promise = db.insert({ a: 5, b: 6, id: 1 });
+
+		return expect(promise).rejects.toMatch("ID can't be duplicated!");
+	});
+});
+
