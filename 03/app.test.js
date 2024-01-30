f9979cb52a68af6d58a0dafffcc7b20252d6c056
Marcin Chechelski <marcin.chechelski@op.pl>
2024-01-30 22:26:39 +0100

    third step color red

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,6 @@ import randomNumber from "./app";
 it("return 1 if min =1 and max=1", () => {
     expect(randomNumber(1, 1)).toBe(1);
 });
+it("throw error if not a number", () => {
+    expect(() => randomNumber("1", 2)).toThrow('"min" is not a number');
+});
