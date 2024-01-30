a31dde30c0691f776fd951a6b41ecc585f13e2d5
Marcin Chechelski <marcin.chechelski@op.pl>
2024-01-30 22:34:01 +0100

    seventh step color green

--- 03/app.js
+++ 03/app.js
@@ -3,5 +3,9 @@ export default function randomNumber(min, max) {
         throw new Error('"min" is not a number');
     }
 
+    if (typeof max !== "number") {
+        throw new Error('"max" is not a number');
+    }
+
     return Math.random() * (max - min) + min;
 }
