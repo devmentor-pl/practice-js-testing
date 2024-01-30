8111ace096bfad98df515d0a0262c5d6a6d5e352
Marcin Chechelski <marcin.chechelski@op.pl>
2024-01-30 23:09:44 +0100

    first test passed

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,14 @@
 export default class User {
+    constructor(obj) {
+        const { email, password } = obj;
+        this.email = email;
+        this.password = password;
+    }
+    getEmail() {
+        return this.email;
+    }
 
+    getPassword() {
+        return this.password;
+    }
 }
