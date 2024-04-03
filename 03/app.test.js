2e0175c66fbfda1137dadc8e20c6ef60013de24e
Robert Staniszewski <robstani.tech@gmail.com>
2024-04-03 16:15:23 +0200

    Test if randomNumber will throw an error if argument is not a number

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,10 @@ import randomNumber from './app';
 test('randomNumber should return a number', () => {
   expect(typeof randomNumber(1, 10)).toBe('number');
 });
+
+// Next test: If argument is not a number, it should throw an error
+test('randomNumber should throw an error if argument is not a number', () => {
+  expect(() => {
+    randomNumber('a', 10);
+  }).toThrow();
+});
