de5260d939d3b9df646b06919a1548a52955debd
Robert Staniszewski <robstani.tech@gmail.com>
2024-04-03 16:23:25 +0200

    Refactorize function to check if arguments are integers. If not, throw an error

--- 03/app.js
+++ 03/app.js
@@ -2,5 +2,8 @@ export default function randomNumber(min, max) {
     if (typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('Both arguments must be numbers');
     }
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        throw new Error('Both arguments must be integers');
+    }
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
