2fb3eabb894deb443cd3ac2d11aeab1b050793e4
Robert Staniszewski <robstani.tech@gmail.com>
2024-04-03 16:40:03 +0200

    Finish task 03

--- 03/app.test.js
+++ 03/app.test.js
@@ -11,3 +11,13 @@ test('randomNumber should throw an error if argument is not a number', () => {
 test('throws error when min is greater than max', () => {
     expect(() => randomNumber(2, 1)).toThrow();
 });
+test('random number should return a number 1', () => {
+    expect(randomNumber(1, 1)).toBe(1);
+});
+test('returns a number within the specified range', () => {
+    const min = 1;
+    const max = 10;
+    const result = randomNumber(min, max);
+    expect(result).toBeGreaterThanOrEqual(min);
+    expect(result).toBeLessThanOrEqual(max);
+});
