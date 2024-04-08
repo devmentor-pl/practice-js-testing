5244b212d20ed67a485dc37fbbabb8c4bb976bd8
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-04-08 16:09:07 +0200

    task 01 partly done

--- 01/app.js
+++ 01/app.js
@@ -1,17 +1,15 @@
+// BłĄD - pokazuje się 518.25
 function getProductList() {
     return [
         {name: 'JavaScript: podstawy', type: 'book', count: 3, price: 67.19, discount: 0.21},
         {name: 'React: podstawy', type: 'book', count: 4, price: 79.17, discount: 0.27}
-    ]
+    ];
 }
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
-    });
+    return productList.reduce((acc, product) => acc + (product.price * product.count), 0);
 }
 
-
-
 const totalPrice = getTotalPrice( getProductList() );
-console.log(totalPrice); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
+console.log(totalPrice.toFixed(2)); // prawidłowa wartość: 390.42 (należy zaokrąglić do 2 miejsc po przecinku)
+// pokazuje się 518.25
