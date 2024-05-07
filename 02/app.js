cef49278ed916686daf672c1365aef892e2beba8
Typee8 <wojciechjasiak1324@gmail.com>
2024-05-07 10:28:25 +0200

    [03] function returns 1 (test)

--- 02/app.js
+++ 02/app.js
@@ -13,12 +13,15 @@ function init() {
 }
 
 function setRandomPosition(element, error = null) {
+  try {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
-
-    if(error) {
-        throw error;
-    }
+  } catch(e) {
+    const alertHidden = document.querySelector('.alert');
+    const alertPlace = document.querySelector(".alert__message");
+    alertHidden.style.display = 'flex';
+    alertPlace.innerText = error;
+  }
 }
 
 function initEventWithError(element, eventName, error) {
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
 import randomNumber from './app';
+
+describe('randomNumber(min, max)', () => {
+  it('returns 1 if min >= 1 and max <= 1', () => {
+    expect(randomNumber).toBe(1);
+  })
+})
