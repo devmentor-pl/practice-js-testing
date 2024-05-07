d810d926f523a6f50dbccea522eb193a6a32448c
Typee8 <wojciechjasiak1324@gmail.com>
2024-05-07 11:29:36 +0200

    [03] (implemented): if min > max throw an error

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,9 @@
 export default function randomNumber(min, max) {
   if(typeof min !== 'number' || typeof max !== 'number') {
-    throw new TypeError('Arguments have to be numbers');
+    throw new TypeError('Arguments have to be numbers.');
+  }
+  if(min > max) {
+    throw new RangeError("min can't be larger than max.");
   }
 
   const number = Math.abs(Math.random() * (max - min) - min);
