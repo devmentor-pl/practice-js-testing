b1ce8eae2a05fa58a82446f2540bbe51762549d5
Typee8 <wojciechjasiak1324@gmail.com>
2024-05-10 15:24:41 +0200

    Finish 02

--- 02/app.js
+++ 02/app.js
@@ -1,31 +1,40 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector(".error--click");
+  const enterEl = document.querySelector(".error--enter");
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+  initEventWithError(clickEl, "click", new RangeError("Błąd zakresu!"));
+  initEventWithError(enterEl, "mouseenter", new TypeError("Błąd typu!"));
 }
 
 function setRandomPosition(element, error = null) {
-  try {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
-  } catch(e) {
-    const alertHidden = document.querySelector('.alert');
-    const alertPlace = document.querySelector(".alert__message");
-    alertHidden.style.display = 'flex';
-    alertPlace.innerText = error;
+  element.style.top = Math.random() * 600 + "px";
+  element.style.left = Math.random() * 800 + "px";
+
+  if (error) {
+    throw error;
   }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  element.addEventListener(eventName, function () {
+    try {
+      setRandomPosition(this, error);
+    } catch (e) {
+      const alertHidden = document.querySelector(".alert");
+      const alertPlace = document.querySelector(".alert__message");
+      alertHidden.style.display = "flex";
+      alertPlace.innerText = e;
+
+      alertHidden.addEventListener("click", (evt) => {
+        if (evt.target === evt.currentTarget) {
+          alertHidden.style.display = "none";
+        }
+      });
+    }
+  });
 }
