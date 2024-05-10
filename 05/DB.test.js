efbdf79ef8f1e365d03b1b9adcefdd1f2cea2516
Typee8 <wojciechjasiak1324@gmail.com>
2024-05-10 14:54:46 +0200

    Finish 05

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,7 +1,7 @@
 import DB from "./DB";
 
 describe("class DB", () => {
-  it(`Creates new instance of the class when the "new" is used.`, () => {
+  it(`Creates new instance of the class | WHEN: "new" operator is used.`, () => {
     const db = new DB();
 
     function createNewDB(db) {
@@ -15,9 +15,9 @@ describe("class DB", () => {
     expect(createNewDB(db)).toBe(true);
   });
 
-  describe("db.insert()", () => {
+  describe("insert(data)", () => {
     // scenario
-    it("rejects promise when data id is not a number", async () => {
+    it("REJECTS: the promise | WHEN: data ID is not a number", async () => {
       // given
       const db = new DB();
       // Czy nie brakuje tutaj when?
@@ -27,33 +27,125 @@ describe("class DB", () => {
       ).rejects.toBeTruthy();
     });
 
-    it("rejects promise when provided data id is a duplicate", async () => {
+    it("REJECTS: the promise | WHEN: data ID is a duplicate", async () => {
       const db = new DB();
 
       await db.insert({ id: 5, a: 1, b: 2 });
       await expect(db.insert({ id: 5, a: 2, b: 3 })).rejects.toBeTruthy();
     });
 
-    it("gives a new id for data when it isn't provided", async () => {
+    it("REJECTS: the promise | WHEN: there's no data", async () => {
+      const db = new DB();
+
+      await expect(db.insert()).rejects.toBeTruthy();
+    });
+
+    it("Grants new ID | WHEN: data don't have an ID", async () => {
       const db = new DB();
 
       await db.insert({ a: 1, b: 2 });
       expect(db._rows[0].id).toBeTruthy();
     });
 
-    it("pushes data to the array", async () => {
+    it("PUSHES: data to the array", async () => {
       const db = new DB();
       await db.insert({ a: 1, b: 2 });
       expect(db._rows.length > 0).toBeTruthy();
     });
   });
 
-  describe("db.remove(id)", () => {
-    it("rejects promise when provided id doesn't match any items' id", async () => {
+  describe("remove(id)", () => {
+    it("REJECTS: the promise | WHEN: provided ID doesn't match any item", async () => {
       const db = new DB();
 
       await expect(db.remove(3)).rejects.toBeTruthy();
     });
 
+    it("RESOLVES: the promise | WHEN: provided ID matches an item", async () => {
+      const db = new DB();
+
+      await db.insert({ id: 5, a: 1, b: 3 });
+      await expect(db.remove(5)).resolves.toBeTruthy();
+    });
+  });
+
+  describe("update(data)", () => {
+    it(`REJECTS: the promise | WHEN: data ID is not provided`, async () => {
+      const db = new DB();
+
+      await expect(db.update("not an ID")).rejects.toBeTruthy();
+    });
+
+    it(`REJECTS: the promise | WHEN: provided ID doesn't match any item`, async () => {
+      const db = new DB();
+
+      await expect(db.update({ id: 1, a: 3, b: 4 })).rejects.toBeTruthy();
+    });
+
+    it(`UPDATES: item`, async () => {
+      const db = new DB();
+      const newItem = {
+        id: 5,
+        a: 2,
+        b: 3,
+      };
+
+      await db.insert({ id: 3, a: 1, b: 2 });
+      await db.insert({ id: 2, a: 1, b: 2 });
+      await db.insert({ id: 5, a: 1, b: 2 });
+      await db.insert({ id: 1, a: 1, b: 2 });
+
+      await db.update(newItem);
+
+      const selectedItem = db._rows.filter((item) => {
+        return item.id === newItem.id;
+      });
+      await expect(selectedItem[0]).toBe(newItem);
+    });
+
+    it("RESOLVES: the promise", async () => {
+      const db = new DB();
+
+      await db.insert({ id: 1, a: 1, b: 2 });
+      await expect(db.update({ id: 1, a: 2, b: 3 })).resolves.toBeTruthy();
+    });
+  });
+
+  describe("getRows()", () => {
+    it(`The promise returns the array as it's value | WHEN: it exists`, async () => {
+      const db = new DB();
+      const result = await db.getRows();
+      await expect(Array.isArray(result)).toBe(true);
+    });
+  });
+
+  describe("select(id)", () => {
+    it(`REJECTS: the promise | WHEN: provided ID doesn't match any item`, async () => {
+      const db = new DB();
+
+      await expect(db.select(1)).rejects.toBeTruthy();
+    });
+
+    it(`RESOLVES: the promise with selected item as its value | WHEN: provided ID matches any item`, async () => {
+      const db = new DB();
+
+      await db.insert({ a: 1, b: 2 });
+      await expect(db.select(1)).resolves.toBeTruthy();
+    });
+  });
+
+  describe("truncate()", () => {
+    it("Assigns empty array to the array", async () => {
+      const db = new DB();
+
+      await db.insert({ a: 1, b: 2 });
+      await db.truncate();
+      await expect(db._rows.length).toBe(0);
+    });
+
+    it(`RESOLVES: the promise with "true" value`, async () => {
+      const db = new DB();
+      await expect(db.truncate()).resolves.toBe(true);
+    });
   });
 });
